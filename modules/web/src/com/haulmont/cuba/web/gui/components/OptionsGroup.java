5b1d981456ad27e3ffbe4f37ebc5e78b702d3898
Dmitry Abramov <abramov@haulmont.com>
2009-03-06 06:27:23 +0000

    add OptionsGroup

--- modules/web/src/com/haulmont/cuba/web/gui/components/OptionsGroup.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/OptionsGroup.java
@@ -17,6 +17,11 @@ import com.haulmont.cuba.core.entity.Entity;
 import com.itmill.toolkit.ui.OptionGroup;
 import com.itmill.toolkit.ui.AbstractSelect;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collections;
+
 public class OptionsGroup
     extends
         com.haulmont.cuba.web.gui.components.AbstractField<OptionGroup>
@@ -46,16 +51,49 @@ public class OptionsGroup
     public <T> T getValue() {
         if (optionsDatasource != null) {
             final Object key = super.getValue();
-            return (T) optionsDatasource.getItem(key);
+            if (key instanceof Collection) {
+                final Set set = new HashSet();
+                for (Object o : (Collection) key) {
+                    set.add(optionsDatasource.getItem(o));
+                }
+                return (T) set;
+            } else {
+                final Object o = optionsDatasource.getItem(key);
+                return wrapAsCollectionIfMultiselect(o);
+            }
         } else {
-            return super.<T>getValue();
+            return wrapAsCollectionIfMultiselect(super.getValue());
+        }
+    }
+
+    protected <T> T wrapAsCollectionIfMultiselect(Object o) {
+        if (isMultiSelect()) {
+            if (o != null) {
+                return (T) Collections.singleton(o);
+            } else {
+                return (T) Collections.emptySet();
+            }
+        } else {
+            return (T) o;
         }
     }
 
     @Override
     public void setValue(Object value) {
         // TODO (abramov) need to be changed
-        super.setValue(((Entity) value).getId());
+        if (isMultiSelect()) {
+            if (value instanceof Collection) {
+                final Set set = new HashSet();
+                for (Object o : (Collection) value) {
+                    set.add(((Entity) o).getId());
+                }
+                super.setValue(set);
+            } else {
+                super.setValue(((Entity) value).getId());
+            }
+        } else {
+            super.setValue(((Entity) value).getId());
+        }
     }
 
     public boolean isMultiSelect() {
