15af01cf37d6f43d566e643f8ca22d5a96ebe034
Konstantin Krivopustov <krivopustov@haulmont.com>
2009-03-30 14:21:12 +0000

    EntityManager javadocs added

--- modules/core/src/com/haulmont/cuba/core/EntityManager.java
+++ modules/core/src/com/haulmont/cuba/core/EntityManager.java
@@ -9,39 +9,137 @@
  */
 package com.haulmont.cuba.core;
 
-import com.haulmont.cuba.core.entity.BaseEntity;
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.View;
 
+/**
+* Interface used to interact with the persistence context.
+*/
 public interface EntityManager
 {
+    /**
+     * Make an instance managed and persistent.
+     * @param entity
+     * @throws javax.persistence.EntityExistsException if the entity already exists.
+     * (The EntityExistsException may be thrown when the persist
+     * operation is invoked, or the EntityExistsException or
+     * another PersistenceException may be thrown at flush or
+     * commit time.)
+     * @throws IllegalArgumentException if not an entity
+     */
     void persist(Entity entity);
 
+    /**
+     * Merge the state of the given entity into the
+     * current persistence context.
+     * @param entity
+     * @return the instance that the state was merged to
+     * @throws IllegalArgumentException if instance is not an
+     * entity or is a removed entity
+     */
     <T extends Entity> T merge(T entity);
 
+    /**
+     * Remove the entity instance.<br>
+     * What actually happens depends on {@link #isDeleteDeferred} flag
+     * @param entity
+     * @throws IllegalArgumentException if not an entity
+     * or if a detached entity
+     */
     void remove(Entity entity);
 
+    /**
+     * Find by primary key.
+     * @param entityClass
+     * @param primaryKey
+     * @return the found entity instance or null if the entity does not exist
+     * @throws IllegalArgumentException if the first argument does
+     * not denote an entity type or the second argument is not a valid type for that entity’s primary key
+     */
     <T extends Entity> T find(Class<T> clazz, Object key);
 
+    /**
+     * Get an instance, whose state may be lazily fetched.<br>
+     * If the requested instance does not exist in the database,
+     * the EntityNotFoundException is thrown when the instance
+     * state is first accessed.<br>
+     * The application should not expect that the instance state will
+     * be available upon detachment, unless it was accessed by the
+     * application while the entity manager was open.
+     * @param entityClass
+     * @param primaryKey
+     * @return the found entity instance
+     * @throws IllegalArgumentException if the first argument does
+     * not denote an entity type or the second argument is not a valid type for that entity’s primary key
+     * @throws javax.persistence.EntityNotFoundException if the entity state cannot be accessed
+     */
     <T extends Entity> T getReference(Class<T> clazz, Object key);
 
+    /**
+     * Create an instance of Query for executing a Java Persistence query language statement.
+     * @return the new query instance
+     */
     Query createQuery();
 
+    /**
+     * Create an instance of Query for executing a Java Persistence query language statement.
+     * @param qlString a Java Persistence query string
+     * @return the new query instance
+     */
     Query createQuery(String qlStr);
 
+    /**
+     * Create an instance of Query for executing
+     * a native SQL statement, e.g., for update or delete.
+     * @return the new query instance
+     */
     Query createNativeQuery();
 
+    /**
+     * Create an instance of Query for executing
+     * a native SQL statement, e.g., for update or delete.
+     * @param sqlString a native SQL query string
+     * @return the new query instance
+     */
     Query createNativeQuery(String sql);
 
+    /**
+     * Set View for this EntityManager instance
+     * @param view
+     */
     void setView(View view);
 
+    /**
+     * Synchronize the persistence context to the underlying database.
+     * @throws PersistenceException if the flush fails
+     */
     void flush();
 
+    /**
+     * Close an application-managed EntityManager.
+     * After the close method has been invoked, all methods
+     * on the EntityManager instance and any Query objects obtained
+     * from it will throw the IllegalStateException except
+     * for getTransaction and isOpen (which will return false).
+     * If this method is called when the EntityManager is
+     * associated with an active transaction, the persistence
+     * context remains managed until the transaction completes.
+     * @throws IllegalStateException if the EntityManager is container-managed.
+     */
     void close();
 
+    /**
+     * Check whether the EntityManager is closed
+     */
     boolean isClosed();
 
+    /**
+     * Determine whether the EntityManager is in DeleteDeferred mode
+     */
     boolean isDeleteDeferred();
 
+    /**
+     * Set DeleteDeferred mode for this EntityManager
+     */
     void setDeleteDeferred(boolean deleteDeferred);
 }
