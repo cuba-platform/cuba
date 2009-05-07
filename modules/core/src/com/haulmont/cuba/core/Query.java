a22c0f83858687e979c1c1ca981e61bce8103ec9
Konstantin Krivopustov <krivopustov@haulmont.com>
2009-05-07 10:57:24 +0000

    Some refactorings and javadocs

--- modules/core/src/com/haulmont/cuba/core/Query.java
+++ modules/core/src/com/haulmont/cuba/core/Query.java
@@ -16,29 +16,99 @@ import javax.persistence.TemporalType;
 import java.util.List;
 import java.util.Date;
 
+/**
+* Interface used to control query execution.
+*/
 public interface Query
 {
     String getQueryString();
     
     void setQueryString(String queryString);
 
+    /**
+     * Execute a SELECT query and return the query results as a List.
+     * @return a list of the results
+     * @throws IllegalStateException if called for a Java Persistence query language UPDATE or DELETE statement
+     */
     List getResultList();
 
+    /**
+     * Execute a SELECT query that returns a single result.
+     * @return the result
+     * @throws javax.persistence.NoResultException if there is no result
+     * @throws javax.persistence.NonUniqueResultException if more than one result
+     * @throws IllegalStateException if called for a Java Persistence query language UPDATE or DELETE statement
+     */
     Object getSingleResult();
 
+    /**
+     * Execute an update or delete statement.
+     * @return the number of entities updated or deleted
+     * @throws IllegalStateException if called for a Java Persistence query language SELECT statement
+     * @throws javax.persistence.TransactionRequiredException if there is no transaction
+     */
     int executeUpdate();
 
+    /**
+     * Set the maximum number of results to retrieve.
+     * @param maxResult
+     * @return the same query instance
+     * @throws IllegalArgumentException if argument is negative
+     */
     Query setMaxResults(int maxResult);
 
+    /**
+     * Set the position of the first result to retrieve.
+     * @param startPosition position of the first result, numbered from 0
+     * @return the same query instance
+     * @throws IllegalArgumentException if argument is negative
+     */
     Query setFirstResult(int startPosition);
 
+    /**
+     * Bind an argument to a named parameter.
+     * @param name the parameter name
+     * @param value
+     * @return the same query instance
+     * @throws IllegalArgumentException if parameter name does not correspond to parameter in query string
+     * or argument is of incorrect type
+     */
     Query setParameter(String name, Object value);
 
+    /**
+     * Bind an instance of java.util.Date to a named parameter.
+     * @param name
+     * @param value
+     * @param temporalType
+     * @return the same query instance
+     * @throws IllegalArgumentException if parameter name does not correspond to parameter in query string
+     */
     Query setParameter(String name, Date value, TemporalType temporalType);
 
+    /**
+     * Bind an argument to a positional parameter.
+     * @param position
+     * @param value
+     * @return the same query instance
+     * @throws IllegalArgumentException if position does not correspond to positional parameter of query
+     * or argument is of incorrect type
+     */
     Query setParameter(int position, Object value);
 
+    /**
+     * Bind an instance of java.util.Date to a positional parameter.
+     * @param position
+     * @param value
+     * @param temporalType
+     * @return the same query instance
+     * @throws IllegalArgumentException if position does not correspond to positional parameter of query
+     */
     Query setParameter(int position, Date value, TemporalType temporalType);
 
+    /**
+     * Set View for this Query instance
+     * @param view
+     * @return the same query instance
+     */
     Query setView(View view);
 }
--- modules/core/src/com/haulmont/cuba/core/sys/persistence/EntityLifecycleListener.java
+++ modules/core/src/com/haulmont/cuba/core/sys/persistence/EntityLifecycleListener.java
@@ -10,18 +10,17 @@
  */
 package com.haulmont.cuba.core.sys.persistence;
 
-import com.haulmont.cuba.core.SecurityProvider;
-import com.haulmont.cuba.core.PersistenceProvider;
 import com.haulmont.cuba.core.Locator;
+import com.haulmont.cuba.core.PersistenceProvider;
+import com.haulmont.cuba.core.SecurityProvider;
+import com.haulmont.cuba.core.entity.BaseEntity;
+import com.haulmont.cuba.core.entity.DeleteDeferred;
+import com.haulmont.cuba.core.entity.Updatable;
+import com.haulmont.cuba.core.global.TimeProvider;
 import com.haulmont.cuba.core.sys.listener.EntityListenerManager;
 import com.haulmont.cuba.core.sys.listener.EntityListenerType;
-import com.haulmont.cuba.core.entity.BaseEntity;
-import com.haulmont.cuba.core.entity.Updatable;
-import com.haulmont.cuba.core.entity.DeleteDeferred;
-import com.haulmont.cuba.core.global.TimeProvider;
-import com.haulmont.cuba.security.app.EntityLog;
-import com.haulmont.cuba.security.app.EntityLogMBean;
 import com.haulmont.cuba.security.app.EntityLogAPI;
+import com.haulmont.cuba.security.app.EntityLogMBean;
 import org.apache.openjpa.enhance.PersistenceCapable;
 import org.apache.openjpa.event.AbstractLifecycleListener;
 import org.apache.openjpa.event.LifecycleEvent;
@@ -40,39 +39,43 @@ public class EntityLifecycleListener extends AbstractLifecycleListener
     }
 
     public void beforePersist(LifecycleEvent event) {
-        if ((event.getSource() instanceof BaseEntity)) {
-            __beforePersist((BaseEntity) event.getSource());
-        }
+        if (!(event.getSource() instanceof BaseEntity))
+            return;
+
+        __beforePersist((BaseEntity) event.getSource());
     }
 
     public void beforeStore(LifecycleEvent event) {
-        PersistenceCapable pc = (PersistenceCapable) event.getSource();
-        if (pc.pcIsNew()) {
-            getEntityLog().registerCreate((BaseEntity) event.getSource(), true);
-            EntityListenerManager.getInstance().fireListener(
-                    ((BaseEntity) event.getSource()), EntityListenerType.BEFORE_INSERT);
+        if (!(event.getSource() instanceof BaseEntity))
+            return;
+
+        BaseEntity entity = (BaseEntity) event.getSource();
+
+        if (((PersistenceCapable) entity).pcIsNew()) {
+            getEntityLog().registerCreate(entity, true);
+            EntityListenerManager.getInstance().fireListener(entity, EntityListenerType.BEFORE_INSERT);
         } else {
-            if (pc instanceof Updatable) {
+            if (entity instanceof Updatable) {
                 __beforeUpdate((Updatable) event.getSource());
-                if ((pc instanceof DeleteDeferred) && justDeleted((DeleteDeferred) pc)) {
-                    getEntityLog().registerDelete((BaseEntity) event.getSource(), true);
-                    processDeletePolicy((BaseEntity) event.getSource());
-                    EntityListenerManager.getInstance().fireListener(
-                            ((BaseEntity) event.getSource()), EntityListenerType.BEFORE_DELETE);
+                if ((entity instanceof DeleteDeferred) && justDeleted((DeleteDeferred) entity)) {
+                    getEntityLog().registerDelete(entity, true);
+                    processDeletePolicy(entity);
+                    EntityListenerManager.getInstance().fireListener(entity, EntityListenerType.BEFORE_DELETE);
                 } else {
-                    getEntityLog().registerModify((BaseEntity) event.getSource(), true);
-                    EntityListenerManager.getInstance().fireListener(
-                            ((BaseEntity) event.getSource()), EntityListenerType.BEFORE_UPDATE);
+                    getEntityLog().registerModify(entity, true);
+                    EntityListenerManager.getInstance().fireListener(entity, EntityListenerType.BEFORE_UPDATE);
                 }
             } else {
-                getEntityLog().registerModify((BaseEntity) event.getSource(), true);
-                EntityListenerManager.getInstance().fireListener(
-                        ((BaseEntity) event.getSource()), EntityListenerType.BEFORE_UPDATE);
+                getEntityLog().registerModify(entity, true);
+                EntityListenerManager.getInstance().fireListener(entity, EntityListenerType.BEFORE_UPDATE);
             }
         }
     }
 
     public void beforeDelete(LifecycleEvent event) {
+        if (!(event.getSource() instanceof BaseEntity))
+            return;
+
         getEntityLog().registerDelete((BaseEntity) event.getSource(), true);
         EntityListenerManager.getInstance().fireListener(
                 ((BaseEntity) event.getSource()), EntityListenerType.BEFORE_DELETE);
