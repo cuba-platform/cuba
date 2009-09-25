0e3130852a775ef1818264c0304244c31d0a5208
Konstantin Krivopustov <krivopustov@haulmont.com>
2009-09-25 10:50:17 +0000

    Fixed NoUserSessionException handling

---
+++ modules/web/src/com/haulmont/cuba/web/exception/NoUserSessionHandler.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2009 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+
+ * Author: Konstantin Krivopustov
+ * Created: 25.09.2009 15:07:23
+ *
+ * $Id$
+ */
+package com.haulmont.cuba.web.exception;
+
+import com.haulmont.cuba.web.App;
+import com.haulmont.cuba.security.global.NoUserSessionException;
+import com.haulmont.cuba.core.global.MessageProvider;
+import com.haulmont.cuba.gui.components.IFrame;
+import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.AbstractAction;
+import com.haulmont.cuba.gui.components.Component;
+import com.vaadin.terminal.ExternalResource;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+
+import java.util.Locale;
+
+public class NoUserSessionHandler extends AbstractExceptionHandler {
+
+    private Log log = LogFactory.getLog(NoUserSessionHandler.class);
+    private Locale locale;
+
+    public NoUserSessionHandler() {
+        super(NoUserSessionException.class);
+        locale = App.getInstance().getConnection().getSession().getLocale();
+    }
+
+    protected void doHandle(App app) {
+        try {
+            App.getInstance().getWindowManager().showOptionDialog(
+                MessageProvider.getMessage(getClass(), "dialogs.Information", locale),
+                    MessageProvider.getMessage(getClass(), "noUserSession.message", locale),
+                    IFrame.MessageType.CONFIRMATION,
+                    new Action[] {new LoginAction()}
+            );
+        } catch (Throwable t) {
+            log.error(t);
+        }
+    }
+
+    private class LoginAction extends AbstractAction {
+        protected LoginAction() {
+            super("actions.Ok");
+        }
+
+        public void actionPerform(Component component) {
+            App app = App.getInstance();
+            String restartUrl = app.getURL().toString() + "?restartApplication";
+            app.getMainWindow().open(new ExternalResource(restartUrl));
+        }
+
+        @Override
+        public String getCaption() {
+            return MessageProvider.getMessage(getClass(), "actions.Ok", locale);
+        }
+    }
+}
