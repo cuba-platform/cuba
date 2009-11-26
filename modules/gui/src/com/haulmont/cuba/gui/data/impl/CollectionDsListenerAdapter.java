1be7d68eae5fa7547e2aaed33687902f92d35652
Tulupov <tulupov@haulmont.com>
2009-11-26 06:20:32 +0000

    rename adapters

---
+++ modules/gui/src/com/haulmont/cuba/gui/data/impl/CollectionDsListenerAdapter.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2008 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+
+ * Author: Maksim Tulupov
+ * Created: 24.11.2009 17:31:03
+ *
+ * $Id$
+ */
+package com.haulmont.cuba.gui.data.impl;
+
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.CollectionDatasourceListener;
+import com.haulmont.cuba.core.entity.Entity;
+
+public class CollectionDsListenerAdapter<T extends Entity> extends DsListenerAdapter<T> implements CollectionDatasourceListener<T> {
+
+    public void collectionChanged(CollectionDatasource ds, Operation operation) {
+    }
+}
