97c6827b2b71c42f06ca69c93f9b304e572d23aa
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2009-11-30 08:39:45 +0000

    added AbstractActionAdapter for groovy classes

---
+++ modules/gui/src/com/haulmont/cuba/gui/components/AbstractActionAdapter.java
@@ -0,0 +1,19 @@
+package com.haulmont.cuba.gui.components;/*
+ * Copyright (c) 2008 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+
+ * Author: degtyarjov
+ * Created: 30.11.2009 12:38:00
+ *
+ * $Id$
+ */
+
+public class AbstractActionAdapter extends AbstractAction {
+    public AbstractActionAdapter(String id) {
+        super(id);
+    }
+
+    public void actionPerform(Component component) {
+    }
+}
