4a28fe8e17abb0cb390cb760f91f8b224a946e93
Konstantin Krivopustov <krivopustov@haulmont.com>
2010-02-24 12:02:08 +0000

    Refs #3 Pessimistic locking (JavaDoc added)

--- modules/global/src/com/haulmont/cuba/core/app/LockService.java
+++ modules/global/src/com/haulmont/cuba/core/app/LockService.java
@@ -18,11 +18,35 @@ public interface LockService {
 
     String NAME = "cuba_LockService";
 
+    /**
+     * Try to lock object
+     * @param name locking object name
+     * @param id locking object ID
+     * @return <li>null in case of successful lock,
+     * <li>{@link com.haulmont.cuba.core.global.LockNotSupported} instance in case of locking is not configured for this object,
+     * <li>{@link LockInfo} instance in case of this object is already locked by someone
+     */
     LockInfo lock(String name, String id);
 
+    /**
+     * Unlock object
+     * @param name locking object name
+     * @param id locking object ID
+     */
     void unlock(String name, String id);
 
+    /**
+     * Get locking status for particular object
+     * @param name locking object name
+     * @param id locking object ID
+     * @return <li>null in case of no lock,
+     * <li>{@link com.haulmont.cuba.core.global.LockNotSupported} instance in case of locking is not configured for this object,
+     * <li>{@link LockInfo} instance in case of this object is locked by someone
+     */
     LockInfo getLockInfo(String name, String id);
 
+    /**
+     * List of current locks
+     */
     List<LockInfo> getCurrentLocks();
 }
