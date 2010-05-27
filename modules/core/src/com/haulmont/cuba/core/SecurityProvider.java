824a51f7437b56b140cac0612b2cfc952c5c0c6b
Konstantin Krivopustov <krivopustov@haulmont.com>
2010-05-27 11:19:05 +0000

    Права доступа при замещение пользователя

--- modules/core/src/com/haulmont/cuba/core/SecurityProvider.java
+++ modules/core/src/com/haulmont/cuba/core/SecurityProvider.java
@@ -105,18 +105,25 @@ public abstract class SecurityProvider
             UserSession userSession = __currentUserSession();
 
             String attrName = paramName.substring(CONSTRAINT_PARAM_SESSION_ATTR.length());
+
             if (CONSTRAINT_PARAM_USER_LOGIN.equals(attrName)) {
-                query.setParameter(paramName, userSession.getUser().getLogin());
+                String userLogin = userSession.getSubstitutedUser() != null ?
+                        userSession.getSubstitutedUser().getLogin() :
+                        userSession.getUser().getLogin();
+                query.setParameter(paramName, userLogin);
+
             } else if (CONSTRAINT_PARAM_USER_ID.equals(attrName)) {
                 UUID userId = userSession.getSubstitutedUser() != null ? 
                         userSession.getSubstitutedUser().getId() :
                         userSession.getUser().getId();
                 query.setParameter(paramName, userId);
+
             } else if (CONSTRAINT_PARAM_USER_GROUP_ID.equals(attrName)) {
                 Object groupId = userSession.getSubstitutedUser() == null ?
                         userSession.getUser().getGroup().getId() :
                         userSession.getSubstitutedUser().getGroup().getId();
                 query.setParameter(paramName, groupId);
+
             } else {
                 Serializable value = userSession.getAttribute(attrName);
                 query.setParameter(paramName, value);
