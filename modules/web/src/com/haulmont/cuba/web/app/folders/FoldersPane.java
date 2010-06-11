cfa6383fa081976868d4bad4941fb9ad4b7e2894
Nikolai Gorodnov <gorodnov@haulmont.com>
2010-06-11 08:30:58 +0000

    rollback to double click in the folders

--- modules/web/src/com/haulmont/cuba/web/app/folders/FoldersPane.java
+++ modules/web/src/com/haulmont/cuba/web/app/folders/FoldersPane.java
@@ -283,6 +283,7 @@ public class FoldersPane extends VerticalLayout {
             return null;
 
         appFoldersTree = new com.haulmont.cuba.web.toolkit.ui.Tree();
+        appFoldersTree.setDoubleClickMode(true);
 
         appFoldersRoot = MessageProvider.getMessage(messagesPack, "folders.appFoldersRoot");
         fillTree(appFoldersTree, appFolders, isNeedRootAppFolder() ? appFoldersRoot : null);
@@ -298,6 +299,7 @@ public class FoldersPane extends VerticalLayout {
 
     protected Component createSearchFoldersPane() {
         searchFoldersTree = new com.haulmont.cuba.web.toolkit.ui.Tree();
+        searchFoldersTree.setDoubleClickMode(true);
 
         FoldersService service = ServiceLocator.lookup(FoldersService.NAME);
         List<SearchFolder> searchFolders = service.loadSearchFolders();
