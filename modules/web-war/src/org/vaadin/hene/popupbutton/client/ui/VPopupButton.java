c0078ff0245006cdded3c7dd94ff5e0212ca947c
Gennady Pavlov <pavlov@haulmont.com>
2010-06-21 10:42:02 +0000

    add PopupButton

---
+++ modules/web-war/src/org/vaadin/hene/popupbutton/client/ui/VPopupButton.java
@@ -0,0 +1,395 @@
+package org.vaadin.hene.popupbutton.client.ui;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+import com.google.gwt.user.client.ui.Focusable;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.terminal.gwt.client.ApplicationConnection;
+import com.vaadin.terminal.gwt.client.Console;
+import com.vaadin.terminal.gwt.client.Container;
+import com.vaadin.terminal.gwt.client.Paintable;
+import com.vaadin.terminal.gwt.client.RenderSpace;
+import com.vaadin.terminal.gwt.client.UIDL;
+import com.vaadin.terminal.gwt.client.Util;
+import com.vaadin.terminal.gwt.client.VCaption;
+import com.vaadin.terminal.gwt.client.VCaptionWrapper;
+import com.vaadin.terminal.gwt.client.VDebugConsole;
+import com.vaadin.terminal.gwt.client.RenderInformation.Size;
+import com.vaadin.terminal.gwt.client.ui.VButton;
+import com.vaadin.terminal.gwt.client.ui.VOverlay;
+import com.vaadin.terminal.gwt.client.ui.richtextarea.VRichTextArea;
+
+// This class contains code from the VPopupView class.  
+
+public class VPopupButton extends VButton implements Container,
+		Iterable<Widget> {
+
+	/** Set the CSS class name to allow styling. */
+	public static final String CLASSNAME = "v-popupbutton";
+
+	public static final String MENU_INDICATOR_CLASSNAME = "v-menu-indicator";
+
+	private final LayoutPopup popup = new LayoutPopup();
+
+	private boolean popupVisible = false;
+
+	public VPopupButton() {
+		super();
+		DivElement e = Document.get().createDivElement();
+		e.setClassName(MENU_INDICATOR_CLASSNAME);
+		getElement().getFirstChildElement().appendChild(e);
+	}
+
+	/**
+	 * Called whenever an update is received from the server
+	 */
+	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+		super.updateFromUIDL(uidl, client);
+		addStyleName(CLASSNAME);
+
+		popupVisible = uidl.getBooleanVariable("popupVisible");
+		if (popupVisible) {
+			UIDL popupUIDL = uidl.getChildUIDL(0);
+			popup.setVisible(false);
+			popup.show();
+			popup.updateFromUIDL(popupUIDL);
+			showPopup();
+		} else {
+			hidePopup();
+		}
+	}
+
+	@Override
+	public void onBrowserEvent(Event event) {
+		int type = event.getTypeInt();
+		switch (type) {
+		case Event.ONMOUSEDOWN:
+			updateState(true, false);
+			break;
+		}
+		super.onBrowserEvent(event);
+
+	}
+
+	private void updateState(boolean visible, boolean immediate) {
+		client.updateVariable(id, "popupVisible", visible, immediate);
+	}
+
+	private void showPopup() {
+		DeferredCommand.addCommand(new Command() {
+
+			public void execute() {
+				int extra = 20;
+				int left = getAbsoluteLeft();
+				int top = getAbsoluteTop() + getOffsetHeight();
+				int browserWindowWidth = Window.getClientWidth()
+						+ Window.getScrollLeft();
+				int browserWindowHeight = Window.getClientHeight()
+						+ Window.getScrollTop();
+				if (left + popup.getOffsetWidth() > browserWindowWidth - extra) {
+					left = getAbsoluteLeft()
+							- (popup.getOffsetWidth() - getOffsetWidth());
+				}
+				if (top + popup.getOffsetHeight() > browserWindowHeight - extra) {
+					top = getAbsoluteTop() - popup.getOffsetHeight() - 2;
+				}
+				popup.setPopupPosition(left, top);
+				popup.setVisible(true);
+			}
+		});
+	}
+
+	private void hidePopup() {
+		popup.setVisible(false);
+		popup.hide();
+	}
+
+	private static native void nativeBlur(Element e)
+	/*-{
+	    if (e && e.blur) {
+	        e.blur();
+	    }
+	}-*/;
+
+	private class LayoutPopup extends VOverlay {
+
+		public static final String CLASSNAME = VPopupButton.CLASSNAME
+				+ "-popup";
+
+		private final Set<Element> activeChildren = new HashSet<Element>();
+
+		private boolean hiding = false;
+
+		public LayoutPopup() {
+			super(false, false, true);
+			setStyleName(CLASSNAME);
+		}
+
+		public void updateFromUIDL(final UIDL uidl) {
+			if (Util.isCached(uidl.getChildUIDL(0))) {
+				return;
+			}
+
+			Paintable newPopupComponent = client.getPaintable(uidl
+					.getChildUIDL(0));
+			if (!newPopupComponent.equals(getPaintable())) {
+				if (getPaintable() != null) {
+					client.unregisterPaintable(getPaintable());
+				}
+				setWidget((Widget) newPopupComponent);
+			}
+			getPaintable().updateFromUIDL(uidl.getChildUIDL(0), client);
+		}
+
+		private Paintable getPaintable() {
+			return (Paintable) getWidget();
+		}
+
+		private VCaptionWrapper getCaptionWrapper() {
+			if (getWidget() instanceof VCaptionWrapper) {
+				return (VCaptionWrapper) getWidget();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPreviewNativeEvent(NativePreviewEvent event) {
+			Element target = Element
+					.as(event.getNativeEvent().getEventTarget());
+			switch (event.getTypeInt()) {
+			case Event.ONCLICK:
+				if (isOrHasChildOfButton(target)) {
+					updateState(false, true);
+				}
+				break;
+			case Event.ONMOUSEDOWN:
+				if (!isOrHasChildOfPopup(target)
+						&& !isOrHasChildOfConsole(target)
+						&& !isOrHasChildOfButton(target)) {
+					updateState(false, true);
+				}
+				break;
+			case Event.ONKEYPRESS:
+				if (isOrHasChildOfPopup(target)) {
+					// Catch children that use keyboard, so we can unfocus them
+					// when
+					// hiding
+					activeChildren.add(target);
+				}
+			default:
+				break;
+			}
+
+			super.onPreviewNativeEvent(event);
+		}
+
+		private boolean isOrHasChildOfPopup(Element element) {
+			return getElement().isOrHasChild(element);
+		}
+
+		private boolean isOrHasChildOfButton(Element element) {
+			return VPopupButton.this.getElement().isOrHasChild(element);
+		}
+
+		private boolean isOrHasChildOfConsole(Element element) {
+			Console console = ApplicationConnection.getConsole();
+			return console instanceof VDebugConsole
+					&& ((VDebugConsole) console).getElement().isOrHasChild(
+							element);
+		}
+
+		/*
+		 *
+		 * We need a hack make popup act as a child of VPopupButton in Vaadin's
+		 * component tree, but work in default GWT manner when closing or
+		 * opening.
+		 *
+		 * (non-Javadoc)
+		 *
+		 * @see com.google.gwt.user.client.ui.Widget#getParent()
+		 */
+		@Override
+		public Widget getParent() {
+			if (!isAttached() || hiding) {
+				return super.getParent();
+			} else {
+				return VPopupButton.this;
+			}
+		}
+
+		@Override
+		protected void onDetach() {
+			super.onDetach();
+			hiding = false;
+		}
+
+		public void hide(boolean autoClosed) {
+			hiding = true;
+			syncChildren();
+			super.hide(autoClosed);
+		}
+
+		@Override
+		public void show() {
+			hiding = false;
+			super.show();
+		}
+
+		/*-
+		private void unregisterPaintables() {
+			if (getPaintable() != null) {
+				client.unregisterPaintable(getPaintable());
+			}
+		}*/
+
+		/**
+		 * Try to sync all known active child widgets to server
+		 */
+		private void syncChildren() {
+			// Notify children with focus
+			if ((getWidget() instanceof Focusable)) {
+				((Focusable) getWidget()).setFocus(false);
+			} else {
+				checkForRTE(getWidget());
+			}
+
+			// Notify children that have used the keyboard
+			for (Element e : activeChildren) {
+				try {
+					nativeBlur(e);
+				} catch (Exception ignored) {
+				}
+			}
+			activeChildren.clear();
+		}
+
+		private void checkForRTE(Widget popupComponentWidget2) {
+			if (popupComponentWidget2 instanceof VRichTextArea) {
+				((VRichTextArea) popupComponentWidget2)
+						.synchronizeContentToServer();
+			} else if (popupComponentWidget2 instanceof HasWidgets) {
+				HasWidgets hw = (HasWidgets) popupComponentWidget2;
+				Iterator<Widget> iterator = hw.iterator();
+				while (iterator.hasNext()) {
+					checkForRTE(iterator.next());
+				}
+			}
+		}
+
+		@Override
+		public com.google.gwt.user.client.Element getContainerElement() {
+			return super.getContainerElement();
+		}
+
+		@Override
+		public void updateShadowSizeAndPosition() {
+			super.updateShadowSizeAndPosition();
+		}
+	}
+
+	public RenderSpace getAllocatedSpace(Widget child) {
+		Size popupExtra = calculatePopupExtra();
+
+		return new RenderSpace(RootPanel.get().getOffsetWidth()
+				- popupExtra.getWidth(), RootPanel.get().getOffsetHeight()
+				- popupExtra.getHeight());
+	}
+
+	/**
+	 * Calculate extra space taken by the popup decorations
+	 *
+	 * @return
+	 */
+	protected Size calculatePopupExtra() {
+		Element pe = popup.getElement();
+		Element ipe = popup.getContainerElement();
+
+		// border + padding
+		int width = Util.getRequiredWidth(pe) - Util.getRequiredWidth(ipe);
+		int height = Util.getRequiredHeight(pe) - Util.getRequiredHeight(ipe);
+
+		return new Size(width, height);
+	}
+
+	public boolean hasChildComponent(Widget component) {
+		if (popup.getWidget() != null) {
+			return popup.getWidget() == component;
+		} else {
+			return false;
+		}
+	}
+
+	public void replaceChildComponent(Widget oldComponent, Widget newComponent) {
+		if (!hasChildComponent(oldComponent)) {
+			throw new IllegalArgumentException();
+		}
+		popup.setWidget(newComponent);
+	}
+
+	public boolean requestLayout(Set<Paintable> children) {
+		popup.updateShadowSizeAndPosition();
+		return true;
+	}
+
+	public void updateCaption(Paintable component, UIDL uidl) {
+		if (VCaption.isNeeded(uidl)) {
+			if (popup.getCaptionWrapper() != null) {
+				popup.getCaptionWrapper().updateCaption(uidl);
+			} else {
+				VCaptionWrapper captionWrapper = new VCaptionWrapper(component,
+						client);
+				popup.setWidget(captionWrapper);
+				captionWrapper.updateCaption(uidl);
+			}
+		} else {
+			if (popup.getCaptionWrapper() != null) {
+				popup.setWidget((Widget) popup.getCaptionWrapper()
+						.getPaintable());
+			}
+		}
+	}
+
+	public Iterator<Widget> iterator() {
+		return new Iterator<Widget>() {
+
+			int pos = 0;
+
+			public boolean hasNext() {
+				// There is a child widget only if next() has not been called.
+				return (pos == 0);
+			}
+
+			public Widget next() {
+				// Next can be called only once to return the popup.
+				if (pos != 0) {
+					throw new NoSuchElementException();
+				}
+				pos++;
+				return popup;
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+
+	@Override
+	protected void onDetach() {
+		super.onDetach();
+		hidePopup();
+	}
+}
---
+++ modules/web/src/org/vaadin/hene/popupbutton/PopupButton.java
@@ -0,0 +1,276 @@
+package org.vaadin.hene.popupbutton;
+
+import com.vaadin.terminal.PaintException;
+import com.vaadin.terminal.PaintTarget;
+import com.vaadin.ui.*;
+import org.vaadin.hene.popupbutton.client.ui.VPopupButton;
+
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Server side component for the VPopupButton widget.
+ */
+// This class contains code from AbstractComponentContainer
+@SuppressWarnings("serial")
+@ClientWidget(VPopupButton.class)
+public class PopupButton extends Button implements ComponentContainer {
+
+	private static final Method COMPONENT_ATTACHED_METHOD;
+
+	private static final Method COMPONENT_DETACHED_METHOD;
+
+	static {
+		try {
+			COMPONENT_ATTACHED_METHOD = ComponentAttachListener.class
+					.getDeclaredMethod("componentAttachedToContainer",
+							new Class[] { ComponentAttachEvent.class });
+			COMPONENT_DETACHED_METHOD = ComponentDetachListener.class
+					.getDeclaredMethod("componentDetachedFromContainer",
+							new Class[] { ComponentDetachEvent.class });
+		} catch (final NoSuchMethodException e) {
+			// This should never happen
+			throw new RuntimeException(
+					"Internal error finding methods in PopupButton");
+		}
+	}
+
+	private Component component;
+
+	private boolean popupVisible = false;
+
+	public PopupButton(String caption) {
+		super(caption);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see com.vaadin.ui.Button#paintContent(com.vaadin.terminal.PaintTarget)
+	 */
+	@Override
+	public void paintContent(PaintTarget target) throws PaintException {
+		super.paintContent(target);
+		target.addVariable(this, "popupVisible", popupVisible);
+		if (popupVisible) {
+			if (component == null) {
+				throw new IllegalStateException(
+						"component cannot be null. Use addComponent to set the component.");
+			}
+			target.startTag("component");
+			component.paint(target);
+			target.endTag("component");
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see com.vaadin.ui.Button#changeVariables(java.lang.Object,
+	 * java.util.Map)
+	 */
+	@Override
+	public void changeVariables(Object source, Map variables) {
+		super.changeVariables(source, variables);
+		if (variables.containsKey("popupVisible")) {
+			popupVisible = ((Boolean) variables.get("popupVisible"))
+					.booleanValue();
+			requestRepaint();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * com.vaadin.ui.ComponentContainer#addComponent(com.vaadin.ui.Component)
+	 */
+	public void addComponent(Component c) {
+		component = c;
+		requestRepaint();
+
+		if (c instanceof ComponentContainer) {
+			// Make sure we're not adding the component inside it's own content
+			for (Component parent = this; parent != null; parent = parent
+					.getParent()) {
+				if (parent == c) {
+					throw new IllegalArgumentException(
+							"Component cannot be added inside it's own content");
+				}
+			}
+		}
+
+		if (c.getParent() != null) {
+			// If the component already has a parent, try to remove it
+			ComponentContainer oldParent = (ComponentContainer) c.getParent();
+			oldParent.removeComponent(c);
+
+		}
+
+		c.setParent(this);
+		fireEvent(new ComponentAttachEvent(this, component));
+	}
+
+	public void addListener(ComponentAttachListener listener) {
+		addListener(ComponentAttachEvent.class, listener,
+				COMPONENT_ATTACHED_METHOD);
+
+	}
+
+	public void addListener(ComponentDetachListener listener) {
+		addListener(ComponentDetachEvent.class, listener,
+				COMPONENT_DETACHED_METHOD);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see com.vaadin.ui.ComponentContainer#getComponentIterator()
+	 */
+	public Iterator<Component> getComponentIterator() {
+		return new Iterator<Component>() {
+
+			private boolean first = component == null;
+
+			public boolean hasNext() {
+				return !first;
+			}
+
+			public Component next() {
+				if (!first) {
+					first = true;
+					return component;
+				} else {
+					return null;
+				}
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+
+	/**
+	 * Not supported in this implementation.
+	 *
+	 * @see com.vaadin.ui.ComponentContainer#moveComponentsFrom(com.vaadin.ui.
+	 *      ComponentContainer)
+	 */
+	public void moveComponentsFrom(ComponentContainer source) {
+		throw new UnsupportedOperationException();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see com.vaadin.ui.ComponentContainer#removeAllComponents()
+	 */
+	public void removeAllComponents() {
+		if (component != null) {
+			removeComponent(component);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * com.vaadin.ui.ComponentContainer#removeComponent(com.vaadin.ui.Component)
+	 */
+	public void removeComponent(Component c) {
+		if (c.getParent() == this) {
+			c.setParent(null);
+			fireEvent(new ComponentDetachEvent(this, c));
+		}
+		component = null;
+		requestRepaint();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @seecom.vaadin.ui.ComponentContainer#removeListener(com.vaadin.ui.
+	 * ComponentContainer.ComponentAttachListener)
+	 */
+	public void removeListener(ComponentAttachListener listener) {
+		removeListener(ComponentAttachEvent.class, listener,
+				COMPONENT_ATTACHED_METHOD);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @seecom.vaadin.ui.ComponentContainer#removeListener(com.vaadin.ui.
+	 * ComponentContainer.ComponentDetachListener)
+	 */
+	public void removeListener(ComponentDetachListener listener) {
+		removeListener(ComponentDetachEvent.class, listener,
+				COMPONENT_DETACHED_METHOD);
+	}
+
+	/**
+	 *
+	 * Not supported in this implementation.
+	 *
+	 * @see com.vaadin.ui.ComponentContainer#replaceComponent(com.vaadin.ui.Component,
+	 *      com.vaadin.ui.Component)
+	 */
+	public void replaceComponent(Component oldComponent, Component newComponent) {
+		throw new UnsupportedOperationException();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see com.vaadin.ui.ComponentContainer#requestRepaintAll()
+	 */
+	public void requestRepaintAll() {
+		requestRepaint();
+		if (component != null) {
+			if (component instanceof Form) {
+				// Form has children in layout, but is not ComponentContainer
+				component.requestRepaint();
+				((Form) component).getLayout().requestRepaintAll();
+			} else if (component instanceof Table) {
+				((Table) component).requestRepaintAll();
+			} else if (component instanceof ComponentContainer) {
+				((ComponentContainer) component).requestRepaintAll();
+			} else {
+				component.requestRepaint();
+			}
+		}
+	}
+
+	/**
+	 * Shows or hides popup.
+	 *
+	 * @param popupVisible
+	 *            if true, popup is set to visible, otherwise popup is hidden.
+	 */
+	public void setPopupVisible(boolean popupVisible) {
+		this.popupVisible = popupVisible;
+		requestRepaint();
+	}
+
+	/**
+	 * Checks if the popup is visible.
+	 *
+	 * @return true, if popup is visible, false otherwise.
+	 */
+	public boolean isPopupVisible() {
+		return popupVisible;
+	}
+
+	/**
+	 * Set the content component of the popup.
+	 *
+	 * @param component
+	 *            the component to be displayed in the popup.
+	 */
+	public void setComponent(Component component) {
+		addComponent(component);
+	}
+
+}
