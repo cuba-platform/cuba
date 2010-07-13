409b07b499b2b2862e6269ccae9539d756310ae7
Valeriy Novikov <novikov@haulmont.com>
2010-07-13 05:16:11 +0000

    Fixes #371 (Произвольные оповещения по электронной почте)

---
+++ modules/web/src/com/haulmont/cuba/web/app/ui/security/user/browse/UserLookup.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2008 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+
+ * Author: Valery Novikov
+ * Created: 01.07.2010 15:35:43
+ *
+ * $Id$
+ */
+
+package com.haulmont.cuba.web.app.ui.security.user.browse;
+
+import com.haulmont.cuba.gui.components.*;
+
+import java.util.Map;
+
+public class UserLookup extends AbstractLookup {
+    public UserLookup(Window frame) {
+        super(frame);
+    }
+
+    protected void init(Map<String, Object> params) {
+        final Table table  = getComponent("users");
+
+        String multiSelect = (String) params.get("param$multiSelect");
+        if ("true".equals(multiSelect)) {
+            table.setMultiSelect(true);
+        }
+    }
+}
+
