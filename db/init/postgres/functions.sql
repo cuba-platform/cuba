077eaf7decc8daf30af1d42094e82ed3597b1635
Nikolai Krokhin <krokhin@haulmont.com>
2010-09-03 08:34:13 +0000

    Special Functions (PostgreSQL)

---
+++ db/init/postgres/functions.sql
@@ -0,0 +1,20 @@
+-- $Id$
+-- Useful SQL functions
+------------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION FirstDateOfMonth(date) RETURNS date AS '
+SELECT CAST(date_trunc(''month'', $1) as date);
+' LANGUAGE sql^
+
+CREATE OR REPLACE FUNCTION LastDateOfMonth(date) RETURNS date AS '
+SELECT CAST(date_trunc(''month'', $1) + interval ''1 month''
+- interval ''1 day'' as date);
+' LANGUAGE sql^
+
+--This function allows to concat text columns
+CREATE AGGREGATE concat (
+    BASETYPE = text,
+    SFUNC = textcat,
+    STYPE = text,
+    INITCOND = ''
+)^
+------------------------------------------------------------------------------------------------------------------------
