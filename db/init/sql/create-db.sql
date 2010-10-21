66a2e2d7eefcfb8c0e0dbb48488888ab7b851153
Yuriy Artamonov <artamonov@haulmont.com>
2010-10-21 07:26:01 +0000

    Fix report section in create-db.sql

--- db/init/sql/create-db.sql
+++ db/init/sql/create-db.sql
@@ -470,3 +470,159 @@ values ('c838be0a-96d0-4ef4-a7c0-dff348347f93', current_timestamp, 0, '60885987-
 
 INSERT INTO sec_filter (id,create_ts,created_by,version,update_ts,updated_by,delete_ts,deleted_by,component,name,xml,user_id) VALUES ('b61d18cb-e79a-46f3-b16d-eaf4aebb10dd',{ts '2010-03-01 11:14:06.830'},'admin',2,{ts '2010-03-01 11:52:53.170'},'admin',null,null,'[sec$User.browse].genericFilter','Search by role','<?xml version="1.0" encoding="UTF-8"?>\n
 <filter>\n  <and>\n    <c name="UrMxpkfMGn" class="com.haulmont.cuba.security.entity.Role" type="CUSTOM" locCaption="Role" entityAlias="u" join="join u.userRoles ur">ur.role.id = :component$genericFilter.UrMxpkfMGn32565\n      <param name="component$genericFilter.UrMxpkfMGn32565">NULL</param>\n    </c>\n  </and>\n</filter>\n','60885987-1b61-4247-94c7-dff348347f93');
+
+--------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE report_band_definition
+(
+  id varchar(36),
+  create_ts timestamp without time zone,
+  created_by character varying(50),
+  "version" integer,
+  update_ts timestamp without time zone,
+  updated_by character varying(50),
+  delete_ts timestamp without time zone,
+  deleted_by character varying(50),
+  query character varying(255),
+  parent_definition_id uuid,
+  "name" character varying(255),
+  orientation integer DEFAULT 0,
+  "position" integer DEFAULT 0,
+  CONSTRAINT report_band_definition_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_report_band_definition_to_report_band_definition FOREIGN KEY (parent_definition_id)
+      REFERENCES report_band_definition (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+--------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE report_report
+(
+  id varchar(36),
+  create_ts timestamp without time zone,
+  created_by character varying(50),
+  "version" integer,
+  update_ts timestamp without time zone,
+  updated_by character varying(50),
+  delete_ts timestamp without time zone,
+  deleted_by character varying(50),
+  "name" character varying(255),
+  root_definition_id uuid,
+  template_path text,
+  report_output_type integer DEFAULT 0,
+  is_custom boolean DEFAULT false,
+  custom_class character varying,
+  linked_entity character varying,
+  template_file_id uuid,
+  report_type integer,
+  CONSTRAINT report_report_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_report_report_to_report_band_definition FOREIGN KEY (root_definition_id)
+      REFERENCES report_band_definition (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+--------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE report_input_parameter
+(
+  id varchar(36),
+  create_ts timestamp without time zone,
+  created_by character varying(50),
+  "version" integer,
+  update_ts timestamp without time zone,
+  updated_by character varying(50),
+  delete_ts timestamp without time zone,
+  deleted_by character varying(50),
+  report_id uuid,
+  "type" integer,
+  "name" character varying(255),
+  alias character varying(100),
+  screen character varying(255),
+  class_name character varying,
+  from_browser boolean,
+  required boolean DEFAULT false,
+  position integer default 0,
+  meta_class varchar(255),
+  CONSTRAINT repor_input_parameter_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_repor_input_parameter_to_report_report FOREIGN KEY (report_id)
+      REFERENCES report_report (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+--------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE report_data_set
+(
+  id varchar(36),
+  create_ts timestamp without time zone,
+  created_by character varying(50),
+  "version" integer,
+  update_ts timestamp without time zone,
+  updated_by character varying(50),
+  delete_ts timestamp without time zone,
+  deleted_by character varying(50),
+  "name" character varying(255),
+  "text" text,
+  "type" integer,
+  band_definition uuid,
+  CONSTRAINT report_data_set_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_report_data_set_to_report_band_definition FOREIGN KEY (band_definition)
+      REFERENCES report_band_definition (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+--------------------------------------------------------------------------------------------------------------
+
+create table REPORT_REPORTS_ROLES (
+REPORT_ID varchar(36),
+ROLE_ID varchar(36)
+);
+
+--------------------------------------------------------------------------------------------------------------
+
+alter table REPORT_REPORTS_ROLES add constraint FK_REPORT_REPORTS_ROLES_TO_REPORT
+foreign key (REPORT_ID) references REPORT_REPORT(ID);
+
+alter table REPORT_REPORTS_ROLES add constraint FK_REPORT_REPORTS_ROLES_TO_ROLE
+foreign key (ROLE_ID) references SEC_ROLE(ID);
+
+--------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE report_report_screen
+(
+  id varchar(36),
+  create_ts timestamp without time zone,
+  created_by character varying(50),
+  "version" integer,
+  update_ts timestamp without time zone,
+  updated_by character varying(50),
+  delete_ts timestamp without time zone,
+  deleted_by character varying(50),
+  report_id uuid,
+  screen_id character varying(255),
+  CONSTRAINT report_report_screen_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_report_report_screen_to_report_report FOREIGN KEY (report_id)
+      REFERENCES report_report (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+--------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE report_value_format
+(
+  id varchar(36),
+  create_ts timestamp without time zone,
+  created_by character varying(50),
+  "version" integer,
+  update_ts timestamp without time zone,
+  updated_by character varying(50),
+  delete_ts timestamp without time zone,
+  deleted_by character varying(50),
+  report_id uuid,
+  "name" character varying(255),
+  format character varying(255),
+  CONSTRAINT report_value_format_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_report_value_format_to_report_report FOREIGN KEY (report_id)
+      REFERENCES report_report (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
