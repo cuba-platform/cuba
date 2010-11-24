e773122b7ce56333436fc1f8646dee29bb076e9e
Konstantin Krivopustov <krivopustov@haulmont.com>
2010-11-24 14:36:03 +0000

    Groovy patched to fix http://jira.codehaus.org/browse/GROOVY-4492

---
+++ modules/core/test/com/haulmont/cuba/core/sys/GseTest.groovy
@@ -0,0 +1,14 @@
+package com.haulmont.cuba.core.sys
+/*
+ * Copyright (c) 2010 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+
+ * Author: Konstantin Krivopustov
+ * Created: 24.11.2010 16:47:00
+ *
+ * $Id$
+ */
+
+
+println "GSE Test"
