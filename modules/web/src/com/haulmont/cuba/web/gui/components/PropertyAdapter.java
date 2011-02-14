7c80d47c744518bd1645d2dee561e85008c44595
Nikolai Gorodnov <gorodnov@haulmont.com>
2011-02-14 14:47:15 +0000

    Fixes #524 (View of fieldgroup field (combobox) is not automatically updated when item changed via screen controller)

--- modules/web/src/com/haulmont/cuba/web/gui/components/PropertyAdapter.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/PropertyAdapter.java
@@ -11,7 +11,7 @@ package com.haulmont.cuba.web.gui.components;
 
 import com.vaadin.data.Property;
 
-public abstract class PropertyAdapter implements Property {
+public abstract class PropertyAdapter implements Property, Property.ValueChangeNotifier {
     protected final Property itemProperty;
 
     public PropertyAdapter(Property itemProperty) {
@@ -29,5 +29,17 @@ public abstract class PropertyAdapter implements Property {
 
     public void setReadOnly(boolean newStatus) {
         itemProperty.setReadOnly(newStatus);
+    }
+
+    public void addListener(ValueChangeListener listener) {
+        if (itemProperty instanceof ValueChangeNotifier) {
+            ((ValueChangeNotifier) itemProperty).addListener(listener);
+        }
+    }
+
+    public void removeListener(ValueChangeListener listener) {
+        if (itemProperty instanceof ValueChangeNotifier) {
+            ((ValueChangeNotifier) itemProperty).removeListener(listener);
+        }
     }
 }
