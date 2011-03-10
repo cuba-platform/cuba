67509efa3a4883af292cd36df6dc0e353567d9e7
Nikolai Gorodnov <gorodnov@haulmont.com>
2011-03-10 10:59:10 +0000

    Custom collection datasource for GroupTable

---
+++ modules/gui/src/com/haulmont/cuba/gui/data/impl/CustomGroupDatasource.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2011 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+ *
+ * Author: Nikolay Gorodnov
+ * Created: 02.03.2011 19:58:42
+ *
+ * $Id$
+ */
+package com.haulmont.cuba.gui.data.impl;
+
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.chile.core.model.MetaPropertyPath;
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.gui.data.DataService;
+import com.haulmont.cuba.gui.data.DsContext;
+import com.haulmont.cuba.gui.data.GroupDatasource;
+import com.haulmont.cuba.gui.data.GroupInfo;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomGroupDatasource<T extends Entity<K>, K> 
+        extends CustomCollectionDatasource<T, K>
+        implements GroupDatasource<T, K>
+{
+    protected GroupDelegate<T,K> groupDelegate = new GroupDelegate<T, K>(this) {
+        protected void doSort(SortInfo<MetaPropertyPath>[] sortInfo) {
+            CustomGroupDatasource.super.doSort();
+        }
+    };
+
+    private static final long serialVersionUID = 2997943125043253322L;
+
+    public CustomGroupDatasource(
+            DsContext context, DataService dataservice,
+            String id, MetaClass metaClass, String viewName
+    ) {
+        super(context, dataservice, id, metaClass, viewName);
+    }
+
+    public void groupBy(Object[] properties) {
+        groupDelegate.groupBy(properties, sortInfos);
+    }
+
+    @Override
+    protected void doSort() {
+        if (hasGroups()) {
+            groupDelegate.doGroupSort(sortInfos);
+        } else {
+            super.doSort();
+        }
+    }
+
+    public List<GroupInfo> rootGroups() {
+        return groupDelegate.rootGroups();
+    }
+
+    public boolean hasChildren(GroupInfo groupId) {
+        return groupDelegate.hasChildren(groupId);
+    }
+
+    public List<GroupInfo> getChildren(GroupInfo groupId) {
+        return groupDelegate.getChildren(groupId);
+    }
+
+    public Object getGroupProperty(GroupInfo groupId) {
+        return groupDelegate.getGroupProperty(groupId);
+    }
+
+    public Object getGroupPropertyValue(GroupInfo groupId) {
+        return groupDelegate.getGroupPropertyValue(groupId);
+    }
+
+    public Collection<K> getGroupItemIds(GroupInfo groupId) {
+        return groupDelegate.getGroupItemIds(groupId);
+    }
+
+    public int getGroupItemsCount(GroupInfo groupId) {
+        return groupDelegate.getGroupItemsCount(groupId);
+    }
+
+    public boolean hasGroups() {
+        return groupDelegate.hasGroups();
+    }
+
+    public Collection<?> getGroupProperties() {
+        return groupDelegate.getGroupProperties();
+    }
+
+    public boolean containsGroup(GroupInfo groupId) {
+        return groupDelegate.containsGroup(groupId);
+    }
+}
