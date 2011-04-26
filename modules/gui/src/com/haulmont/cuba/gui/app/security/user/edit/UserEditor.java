74f59402e9be89ac721ddc07a41c0713d5c762e9
Maxim Chernov <chernov@haulmont.com>
2011-04-26 14:06:51 +0000

    В редактировании пользователя запретить выбирать пустую строку в выпадающем списке выбора языка (тикет 981)

--- modules/gui/src/com/haulmont/cuba/gui/app/security/user/edit/UserEditor.java
+++ modules/gui/src/com/haulmont/cuba/gui/app/security/user/edit/UserEditor.java
@@ -175,6 +175,8 @@ public class UserEditor extends AbstractEditor {
                     options.put(entry.getKey(), entry.getValue().getLanguage());
                 }
                 languageLookup.setOptionsMap(options);
+                com.vaadin.ui.Select languageLookupUnwrap = (com.vaadin.ui.Select) WebComponentsHelper.unwrap(languageLookup);
+                languageLookupUnwrap.setNullSelectionAllowed(false);
 
                 return languageLookup;
             }
