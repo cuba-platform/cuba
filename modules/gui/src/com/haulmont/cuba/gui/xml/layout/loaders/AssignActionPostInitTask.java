b401703f95b64da4fd75d9c00787183b77020383
Konstantin Krivopustov <krivopustov@haulmont.com>
2011-05-19 11:09:07 +0000

    Refs #644 (Generic filter doesn't apply to table if they're inside iframe)

--- modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/AssignActionPostInitTask.java
+++ modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/AssignActionPostInitTask.java
@@ -51,7 +51,7 @@ public class AssignActionPostInitTask implements ComponentLoader.PostInitTask {
             }
         } else if (elements.length == 1) {
             final String id = elements[0];
-            final Action action = ((Window) frame).getAction(id);
+            final Action action = ((Window) window).getAction(id);
 
             if (action != null) {
                 this.component.setAction(action);
