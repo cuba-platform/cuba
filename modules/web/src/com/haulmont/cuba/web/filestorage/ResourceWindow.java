028a1109fe8041023ed2dc6402bcf323b3758b4a
Yuriy Artamonov <artamonov@haulmont.com>
2011-07-05 06:57:25 +0000

    Fixes #821 Проблема с кодировкой при открытии вложений в Mozilla Firefox

--- modules/web/src/com/haulmont/cuba/web/filestorage/ResourceWindow.java
+++ modules/web/src/com/haulmont/cuba/web/filestorage/ResourceWindow.java
@@ -8,8 +8,11 @@ package com.haulmont.cuba.web.filestorage;
 
 import com.haulmont.cuba.gui.export.ExportDataProvider;
 import com.haulmont.cuba.gui.export.ExportFormat;
+import com.haulmont.cuba.web.App;
 import com.haulmont.cuba.web.app.FileDownloadHelper;
+import com.vaadin.service.ApplicationContext;
 import com.vaadin.terminal.DownloadStream;
+import com.vaadin.terminal.gwt.server.WebApplicationContext;
 import com.vaadin.ui.Window;
 import org.apache.commons.lang.StringUtils;
 
@@ -36,6 +39,7 @@ public class ResourceWindow extends Window {
      * @param dataProvider ExportDataprovider
      * @param resourceName Resource name for client side
      * @param format       Resource type, can be null
+     * @param attachment   If true then download file else view
      */
     public ResourceWindow(ExportDataProvider dataProvider, String resourceName,
                           ExportFormat format, boolean attachment) {
@@ -49,6 +53,14 @@ public class ResourceWindow extends Window {
 
     @Override
     public DownloadStream handleURI(URL context, String relativeUri) {
+
+        // Firefox trick
+        boolean isFirefox = false;
+        ApplicationContext appContext = App.getInstance().getContext();
+        if (appContext instanceof WebApplicationContext) {
+            isFirefox = ((WebApplicationContext)appContext).getBrowser().isFirefox();
+        }
+
         String[] strings = resourceName.split("[/\\\\]");
         String fileName = strings[strings.length - 1];
 
@@ -70,10 +82,19 @@ public class ResourceWindow extends Window {
         }
         DownloadStream downloadStream = new CloseableDownloadStream(dataProvider, contentType, fileName);
 
-        if (isAttachment)
-            downloadStream.setParameter("Content-Disposition", "attachment; filename=\"" + fileName + "\";");
-        else
-            downloadStream.setParameter("Content-Disposition", "inline; filename=\"" + fileName + "\";");
+        if (isFirefox)    {
+            // Firefox
+            if (isAttachment)
+                downloadStream.setParameter("Content-Disposition", "attachment; filename*=UTF-8''" + fileName + ";");
+            else
+                downloadStream.setParameter("Content-Disposition", "inline; filename*=UTF-8''" + fileName + ";");
+        } else {
+            // Other browsers
+            if (isAttachment)
+                downloadStream.setParameter("Content-Disposition", "attachment; filename=\"" + fileName + "\";");
+            else
+                downloadStream.setParameter("Content-Disposition", "inline; filename=\"" + fileName + "\";");
+        }
 
         return downloadStream;
     }
