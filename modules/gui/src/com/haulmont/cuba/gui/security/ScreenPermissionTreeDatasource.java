d9ee1ede55606225e6df42970baf8703733b877d
Yuriy Artamonov <artamonov@haulmont.com>
2011-07-07 06:24:00 +0000

    Refs #742 Small fixes

--- modules/gui/src/com/haulmont/cuba/gui/security/ScreenPermissionTreeDatasource.java
+++ modules/gui/src/com/haulmont/cuba/gui/security/ScreenPermissionTreeDatasource.java
@@ -18,6 +18,7 @@ import com.haulmont.cuba.gui.data.DsContext;
 import com.haulmont.cuba.security.entity.Permission;
 import org.apache.commons.lang.ObjectUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -40,13 +41,33 @@ public class ScreenPermissionTreeDatasource extends ScreenPermissionTargetsDatas
     protected Tree<PermissionConfig.Target> loadTree(Map params) {
         if (screensTree == null) {
             List<Node<PermissionConfig.Target>> nodes = super.loadTree(params).getRootNode().getChildren();
-            screensTree = new Tree<PermissionConfig.Target>(nodes);
+
+            List<Node<PermissionConfig.Target>> clonedNodes = new ArrayList<Node<PermissionConfig.Target>>();
+            for (Node<PermissionConfig.Target> node : nodes)
+                clonedNodes.add(cloneNode(node));
+
+            screensTree = new Tree<PermissionConfig.Target>(clonedNodes);
         }
         if (permissionDs != null)
             for (Node<PermissionConfig.Target> node : screensTree.getRootNodes())
                 applyPermissions(node);
         // Set permission variants for targets
         return screensTree;
+    }
+
+    private Node<PermissionConfig.Target> cloneNode(Node<PermissionConfig.Target> node) {
+        Node<PermissionConfig.Target> clone = new Node<PermissionConfig.Target>();
+        clone.setData(cloneTarget(node.data));
+        for (Node<PermissionConfig.Target> childNode : node.getChildren()) {
+            clone.addChild(cloneNode(childNode));
+        }
+        return clone;
+    }
+
+    private PermissionConfig.Target cloneTarget(PermissionConfig.Target target) {
+        return new PermissionConfig.Target(
+                target.getId(), target.getCaption(),
+                target.getPermissionValue(), target.getPermissionVariant());
     }
 
     private void applyPermissions(Node<PermissionConfig.Target> node) {
