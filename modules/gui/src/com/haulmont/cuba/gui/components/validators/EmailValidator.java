6d14beeb18b775c1b90a29b44edbc0f51682cfc4
Gennady Pavlov <pavlov@haulmont.com>
2012-03-15 07:00:04 +0000

    email validator multiemail support

--- modules/gui/src/com/haulmont/cuba/gui/components/validators/EmailValidator.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/validators/EmailValidator.java
@@ -15,16 +15,18 @@ public class EmailValidator extends PatternValidator {
 
     static private String sDomen = "[a-zA-Z[0-9]][a-zA-Z[0-9]\u005F\u002E\u002D]*[a-z||A-Z||0-9]";
     static private String sDomen2 = "([a-zA-Z]){2,4}";
+    public static String EMAIL_PATTERN = sDomen + "@" + sDomen + "\u002E" + sDomen2;
+    public static String MULTI_EMAIL_PATTERN = EMAIL_PATTERN + "([;,]+[\\s]*" + EMAIL_PATTERN + ")*";
 
     private static final long serialVersionUID = -4082731133028995753L;
 
     public EmailValidator(Element element, String messagesPack) {
-        super(sDomen + "@" + sDomen + "\u002E" + sDomen2);
+        super(MULTI_EMAIL_PATTERN);
         message = element.attributeValue("message");
         this.messagesPack = messagesPack;
     }
 
     public EmailValidator() {
-        super(sDomen + "@" + sDomen + "\u002E" + sDomen2);
+        super(MULTI_EMAIL_PATTERN);
     }
 }
