75307112938e254c3c0aad1348afc3dbdf056820
Konstantin Krivopustov <krivopustov@haulmont.com>
2012-03-27 13:10:08 +0000

    Remove "not null" declaration from new column

--- modules/core/db/update/mssql/02/02-100-entitySnapshotAuthor.sql
+++ modules/core/db/update/mssql/02/02-100-entitySnapshotAuthor.sql
@@ -1,4 +1,7 @@
-alter table SYS_ENTITY_SNAPSHOT add AUTHOR_ID uniqueidentifier not null^
+-- $Id$
+-- Description: core$EntitySnapshot.author attribute added
+
+alter table SYS_ENTITY_SNAPSHOT add AUTHOR_ID uniqueidentifier^
 
 alter table SYS_ENTITY_SNAPSHOT
 add constraint FK_SYS_ENTITY_SNAPSHOT_AUTHOR_ID foreign key (AUTHOR_ID) references SEC_USER(ID)^
