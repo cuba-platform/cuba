833df57c45fc09ca7321115998b54949f8d91af6
Yuriy Artamonov <artamonov@haulmont.com>
2012-03-30 13:01:31 +0000

    Fixes default selection in Group lookup

--- modules/gui/src/com/haulmont/cuba/gui/app/security/group/lookup/GroupLookup.java
+++ modules/gui/src/com/haulmont/cuba/gui/app/security/group/lookup/GroupLookup.java
@@ -7,18 +7,36 @@ package com.haulmont.cuba.gui.app.security.group.lookup;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
 import com.haulmont.cuba.gui.components.Tree;
+import com.haulmont.cuba.gui.data.HierarchicalDatasource;
+import com.haulmont.cuba.security.entity.Group;
 
 import javax.inject.Inject;
+import java.util.Collection;
 import java.util.Map;
+import java.util.UUID;
 
+/**
+ * @author abramov
+ * @version $Id$
+ */
 public class GroupLookup extends AbstractLookup {
 
     @Inject
     protected Tree groups;
 
+    @Inject
+    protected HierarchicalDatasource<Group, UUID> groupsDs;
+
     @Override
     public void init(Map<String, Object> params) {
-        groups.getDatasource().refresh();
+        groupsDs.refresh();
         groups.expandTree();
+
+        Collection<UUID> rootItemIds = groupsDs.getRootItemIds();
+        if ((rootItemIds != null) && (!rootItemIds.isEmpty())) {
+            UUID firstId = rootItemIds.iterator().next();
+            Group item = groupsDs.getItem(firstId);
+            groups.setSelected(item);
+        }
     }
 }
