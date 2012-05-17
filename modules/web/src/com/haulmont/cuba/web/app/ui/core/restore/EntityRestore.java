321e73cc5b615d75be0ea01147d4e8a77658fbcf
Yuriy Artamonov <artamonov@haulmont.com>
2012-05-17 13:34:34 +0000

    EntityRestore refactor

--- modules/web/src/com/haulmont/cuba/web/app/ui/core/restore/EntityRestore.java
+++ modules/web/src/com/haulmont/cuba/web/app/ui/core/restore/EntityRestore.java
@@ -2,11 +2,6 @@
  * Copyright (c) 2008 Haulmont Technology Ltd. All Rights Reserved.
  * Haulmont Technology proprietary and confidential.
  * Use is subject to license terms.
-
- * Author: Valery Novikov
- * Created: 27.08.2010 9:22:20
- *
- * $Id$
  */
 package com.haulmont.cuba.web.app.ui.core.restore;
 
@@ -15,27 +10,47 @@ import com.haulmont.chile.core.model.MetaProperty;
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.entity.SoftDelete;
 import com.haulmont.cuba.core.entity.annotation.EnableRestore;
-import com.haulmont.cuba.core.global.*;
+import com.haulmont.cuba.core.global.ConfigProvider;
+import com.haulmont.cuba.core.global.MessageProvider;
+import com.haulmont.cuba.core.global.MessageUtils;
+import com.haulmont.cuba.core.global.MetadataHelper;
+import com.haulmont.cuba.gui.AppConfig;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.impl.GenericDataService;
 import com.haulmont.cuba.gui.data.impl.GroupDatasourceImpl;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 import com.haulmont.cuba.web.WebConfig;
-import com.haulmont.cuba.web.gui.components.*;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 
+import javax.inject.Inject;
 import java.util.*;
 
+/**
+ * @author novikov
+ * @version $Id$
+ */
 public class EntityRestore extends AbstractWindow {
 
+    @Inject
     protected LookupField entities;
-    protected GroupDatasourceImpl entitiesDs;
-    protected Table entitiesTable;
-    protected Filter filter;
-    protected Filter primaryFilter;
-    protected WebVBoxLayout tablePanel;
+
+    @Inject
     protected Button refreshButton;
-    protected Button restore;
+
+    @Inject
+    protected Filter primaryFilter;
+
+    @Inject
+    protected BoxLayout tablePanel;
+
+    protected GroupDatasourceImpl entitiesDs;
+
+    protected Table entitiesTable;
+
+    protected Filter filter;
+
+    protected Button restoreButton;
 
     public EntityRestore(IFrame frame) {
         super(frame);
@@ -44,113 +59,163 @@ public class EntityRestore extends AbstractWindow {
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
-        entities = getComponent("entities");
-        primaryFilter = getComponent("genericFilter");
-        refreshButton = getComponent("refresh");
-        refreshButton.setAction(new AbstractAction("refresh"){
-            public void actionPerform(Component component) {
-                Object value = entities.getValue();
-                if (value != null) {
-                    MetaClass metaClass = (MetaClass) value;
-                    MetaProperty metaProperty = metaClass.getProperty("deleteTs");
-                    if (metaProperty != null) {
-                        if (entitiesTable != null)
-                            tablePanel.remove(entitiesTable);
-                        if (filter != null)
-                            tablePanel.remove(filter);
-                        entitiesTable = new WebTable();
-                        entitiesTable.setFrame(frame);
 
-                        restore = new WebButton();
-                        restore.setId("restore");
-                        restore.setCaption(getMessage("entityRestore.restore"));
-
-                        entitiesTable.setButtonsPanel(new WebButtonsPanel());
-                        entitiesTable.getButtonsPanel().addButton(restore);
-
-                        entitiesTable.setRowsCount(new WebRowsCount());
-
-                        for (MetaProperty mp : metaClass.getProperties()) {
-                            if (MetaProperty.Type.DATATYPE == mp.getType()) {
-                                Table.Column column = new Table.Column(metaClass.getPropertyPath(mp.getName()));
-                                Class classJava = metaClass.getJavaClass();
-                                column.setCaption(MessageProvider.getMessage(classJava, classJava.getSimpleName() + "." + mp.getName()));
-                                entitiesTable.addColumn(column);
-                            }
-                        }
-                        entitiesDs = new GroupDatasourceImpl(getDsContext(), new GenericDataService(), "entitiesDs", metaClass, "_local");
-                        entitiesDs.setQuery("select e from " + metaClass.getName() + " e where e.deleteTs is not null order by e.deleteTs");
-                        entitiesDs.setSoftDeletion(false);
-                        entitiesDs.refresh();
-                        entitiesTable.setDatasource(entitiesDs);
-
-                        filter = new WebFilter();
-                        filter.setId("genericFilter");
-                        filter.setFrame(getFrame());
-                        filter.setStyleName(primaryFilter.getStyleName());
-                        filter.setXmlDescriptor(primaryFilter.getXmlDescriptor());
-                        filter.setUseMaxResults(true);
-                        filter.setDatasource(entitiesDs);
-
-                        entitiesTable.setWidth("100%");
-                        entitiesTable.setHeight("100%");
-                        entitiesTable.setMultiSelect(true);
-                        entitiesTable.addAction(new AbstractAction("restore") {
-                            public void actionPerform(Component component) {
-                                final Set<Entity> listEntity = entitiesTable.getSelected();
-                                Entity entity = entitiesDs.getItem();
-                                if (listEntity != null && entity != null && listEntity.size() > 0) {
-                                    if (entity instanceof SoftDelete) {
-                                        showOptionDialog(
-                                                getMessage("dialogs.Confirmation"),
-                                                getMessage("dialogs.Message"),
-                                                IFrame.MessageType.CONFIRMATION,
-                                                new com.haulmont.cuba.gui.components.Action[]{
-                                                        new DialogAction(DialogAction.Type.OK) {
-                                                            @Override
-                                                            public void actionPerform(com.haulmont.cuba.gui.components.Component component) {
-                                                                for (Entity ent : listEntity) {
-                                                                    SoftDelete d = (SoftDelete) ent;
-                                                                    d.setDeleteTs(null);
-                                                                    d.setDeletedBy(null);
-                                                                }
-                                                                entitiesDs.commit();
-                                                                entitiesTable.refresh();
-                                                            }
-                                                        },
-                                                        new DialogAction(DialogAction.Type.CANCEL)
-                                                }
-                                        );
-                                    }
-                                } else {
-                                    showNotification(getMessage("entityRestore.restoreMsg"), NotificationType.HUMANIZED);
-                                }
-                            }
-
-                            @Override
-                            public String getCaption() {
-                                return getMessage("entityRestore.restore");
-                            }
-                        });
-
-                        restore.setAction(entitiesTable.getAction("restore"));
-
-                        tablePanel.add(filter);
-                        tablePanel.add(entitiesTable);
-                        tablePanel.expand(entitiesTable, "100%", "100%");
-                        entitiesTable.refresh();
-                        filter.loadFiltersAndApplyDefault();
-                    }
-                }
-            }
+        refreshButton.setAction(new AbstractAction("refresh") {
             @Override
-             public String getCaption() {
+            public void actionPerform(Component component) {
+                buildLayout();
+            }
+
+            @Override
+            public String getCaption() {
                 return getMessage("actions.Refresh");
             }
         });
-        primaryFilter.setVisible(false);
-        tablePanel = getComponent("table-panel");
+
+        Window layout = getComponent();
+        layout.remove(primaryFilter);
+
         entities.setOptionsMap(getEntitiesLookupFieldOptions());
+    }
+
+    private void buildLayout() {
+        Object value = entities.getValue();
+        if (value != null) {
+            MetaClass metaClass = (MetaClass) value;
+            MetaProperty deleteTsMetaProperty = metaClass.getProperty("deleteTs");
+            if (deleteTsMetaProperty != null) {
+                if (entitiesTable != null)
+                    tablePanel.remove(entitiesTable);
+                if (filter != null)
+                    tablePanel.remove(filter);
+
+                ComponentsFactory componentsFactory = AppConfig.getFactory();
+
+                entitiesTable = componentsFactory.createComponent(Table.NAME);
+                entitiesTable.setFrame(frame);
+
+                restoreButton = componentsFactory.createComponent(Button.NAME);
+                restoreButton.setId("restore");
+                restoreButton.setCaption(getMessage("entityRestore.restore"));
+
+                ButtonsPanel buttonsPanel = componentsFactory.createComponent(ButtonsPanel.NAME);
+                buttonsPanel.addButton(restoreButton);
+                entitiesTable.setButtonsPanel(buttonsPanel);
+
+                RowsCount rowsCount = componentsFactory.createComponent(RowsCount.NAME);
+                entitiesTable.setRowsCount(rowsCount);
+
+                //collect properties in order to add non-system columns first
+                LinkedList<Table.Column> nonSystemPropertyColumns = new LinkedList<Table.Column>();
+                LinkedList<Table.Column> systemPropertyColumns = new LinkedList<Table.Column>();
+                for (MetaProperty metaProperty : metaClass.getProperties()) {
+                    //don't show embedded & multiple referred entities
+                    if (isEmbedded(metaProperty))
+                        continue;
+
+                    if (metaProperty.getRange().getCardinality().isMany())
+                        continue;
+
+                    Table.Column column = new Table.Column(metaClass.getPropertyPath(metaProperty.getName()));
+                    if (!MetadataHelper.isSystem(metaProperty)) {
+                        column.setCaption(getPropertyCaption(metaClass, metaProperty));
+                        nonSystemPropertyColumns.add(column);
+                    } else {
+                        column.setCaption(metaProperty.getName());
+                        systemPropertyColumns.add(column);
+                    }
+                }
+
+                for (Table.Column column : nonSystemPropertyColumns)
+                    entitiesTable.addColumn(column);
+
+                for (Table.Column column : systemPropertyColumns)
+                    entitiesTable.addColumn(column);
+
+                entitiesDs = new GroupDatasourceImpl(getDsContext(),
+                        new GenericDataService(), "entitiesDs", metaClass, "_local");
+
+                entitiesDs.setQuery("select e from " + metaClass.getName() + " e " +
+                        "where e.deleteTs is not null order by e.deleteTs");
+
+                entitiesDs.setSoftDeletion(false);
+                entitiesDs.refresh();
+                entitiesTable.setDatasource(entitiesDs);
+
+                filter = componentsFactory.createComponent(Filter.NAME);
+                filter.setId("primaryFilter");
+                filter.setFrame(getFrame());
+                filter.setStyleName(primaryFilter.getStyleName());
+                filter.setXmlDescriptor(primaryFilter.getXmlDescriptor());
+                filter.setUseMaxResults(true);
+                filter.setDatasource(entitiesDs);
+
+                entitiesTable.setWidth("100%");
+                entitiesTable.setHeight("100%");
+                entitiesTable.setMultiSelect(true);
+                entitiesTable.addAction(new AbstractAction("restore") {
+                    @Override
+                    public void actionPerform(Component component) {
+                        showRestoreDialog();
+                    }
+
+                    @Override
+                    public String getCaption() {
+                        return getMessage("entityRestore.restore");
+                    }
+                });
+
+                restoreButton.setAction(entitiesTable.getAction("restore"));
+
+                tablePanel.add(filter);
+                tablePanel.add(entitiesTable);
+                tablePanel.expand(entitiesTable, "100%", "100%");
+
+                entitiesTable.refresh();
+
+                filter.loadFiltersAndApplyDefault();
+            }
+        }
+    }
+
+    private void showRestoreDialog() {
+        final Set<Entity> listEntity = entitiesTable.getSelected();
+        Entity entity = entitiesDs.getItem();
+        if (listEntity != null && entity != null && listEntity.size() > 0) {
+            if (entity instanceof SoftDelete) {
+                showOptionDialog(
+                        getMessage("dialogs.Confirmation"),
+                        getMessage("dialogs.Message"),
+                        MessageType.CONFIRMATION,
+                        new Action[]{
+                                new DialogAction(DialogAction.Type.OK) {
+                                    @Override
+                                    public void actionPerform(Component component) {
+                                        for (Entity ent : listEntity) {
+                                            SoftDelete d = (SoftDelete) ent;
+                                            d.setDeleteTs(null);
+                                        }
+                                        entitiesDs.commit();
+                                        entitiesTable.refresh();
+                                    }
+                                },
+                                new DialogAction(DialogAction.Type.CANCEL)
+                        }
+                );
+            }
+        } else {
+            showNotification(getMessage("entityRestore.restoreMsg"), NotificationType.HUMANIZED);
+        }
+    }
+
+    private boolean isEmbedded(MetaProperty metaProperty) {
+        return metaProperty.getAnnotatedElement().isAnnotationPresent(javax.persistence.Embedded.class);
+    }
+
+    private String getPropertyCaption(MetaClass meta, MetaProperty metaProperty) {
+        int idx = meta.getName().indexOf('$') + 1;
+        return MessageProvider.getMessage(meta.getJavaClass(), meta.getName().substring(idx)
+                + "." + metaProperty.getFullName());
     }
 
     protected Map<String, Object> getEntitiesLookupFieldOptions() {
