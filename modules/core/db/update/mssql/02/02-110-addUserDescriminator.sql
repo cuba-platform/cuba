5206ecd880b4c0831830d2cf15970315c93053e1
Dmitry Shishov <shishov@haulmont.com>
2012-05-18 13:01:16 +0000

    user discriminator update

---
+++ modules/core/db/update/mssql/02/02-110-addUserDescriminator.sql
@@ -0,0 +1,5 @@
+--$Id$
+
+alter table SEC_USER add TYPE varchar(1)^
+
+update SEC_USER set TYPE = 'C'^
---
+++ modules/core/db/update/postgres/02/02-120-addUserDescriminator.sql
@@ -0,0 +1,5 @@
+--$Id$
+
+alter table SEC_USER add column TYPE varchar(1)^
+
+update SEC_USER set TYPE = 'C'^
