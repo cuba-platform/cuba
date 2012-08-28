ee0d75a4ac127928e5d49c6c03d0aee08d91531e
Yuriy Artamonov <artamonov@haulmont.com>
2012-08-28 06:06:27 +0000

    Refs #1468 Portal module Improvements

---
+++ modules/portal/src/com/haulmont/cuba/portal/security/PortalSessionProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2012 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+ */
+
+package com.haulmont.cuba.portal.security;
+
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.UserSessionSource;
+
+import java.util.Locale;
+
+/**
+ * Provides access to the current user session in static context.<br>
+ * <p>Injected {@link UserSessionSource} interface should be used instead of this class wherever possible.</p>
+ *
+ * @author artamonov
+ * @version $Id$
+ */
+public abstract class PortalSessionProvider {
+
+    private static UserSessionSource getSource() {
+        return AppBeans.get(UserSessionSource.NAME, UserSessionSource.class);
+    }
+
+    /**
+     * Current portal session
+     *
+     * @return current user session instance
+     */
+    public static <T extends PortalSession> T getUserSession() {
+        return (T) getSource().getUserSession();
+    }
+
+    public static Locale getLocale() {
+        return getSource().getLocale();
+    }
+}
