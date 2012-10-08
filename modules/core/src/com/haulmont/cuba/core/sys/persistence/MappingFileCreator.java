7bcd6e2061e72f447f21ebb338b5827d3f93bb76
Konstantin Krivopustov <krivopustov@haulmont.com>
2012-10-08 08:32:50 +0000

    Refs #1552 Generate orm.xml mapping overrides for associations with extended entities

--- modules/core/src/com/haulmont/cuba/core/sys/persistence/MappingFileCreator.java
+++ modules/core/src/com/haulmont/cuba/core/sys/persistence/MappingFileCreator.java
@@ -10,13 +10,14 @@ import com.haulmont.bali.util.Dom4j;
 import com.haulmont.bali.util.ReflectionHelper;
 import com.haulmont.cuba.core.entity.annotation.Extends;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.ClassUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
+import javax.annotation.Nullable;
 import javax.persistence.*;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -43,31 +44,6 @@ class MappingFileCreator {
 
     private Log log = LogFactory.getLog(getClass());
 
-    private static class Attr {
-
-        private enum Type {
-            ONE_TO_ONE("one-to-one"),
-            ONE_TO_MANY("one-to-many"),
-            MANY_TO_ONE("many-to-one"),
-            MANY_TO_MANY("many-to-many");
-
-            private String xml;
-
-            private Type(String xml) {
-                this.xml = xml;
-            }
-        }
-        private final Type type;
-        private final String name;
-        private final String targetEntity;
-
-        private Attr(Type type, String name, String targetEntity) {
-            this.type = type;
-            this.name = name;
-            this.targetEntity = targetEntity;
-        }
-    }
-
     MappingFileCreator(Collection<String> classNames, Map<String, String> properties, File dir) {
         this.classNames = classNames;
         this.properties = properties;
@@ -90,11 +66,11 @@ class MappingFileCreator {
         if (extendedClasses.isEmpty())
             return;
 
-        Map<String, List<Attr>> mappings = new LinkedHashMap<>();
+        Map<Class<?>, List<Attr>> mappings = new LinkedHashMap<>();
         for (Class aClass : persistentClasses) {
             List<Attr> attrList = processClass(aClass, extendedClasses);
             if (!attrList.isEmpty())
-                mappings.put(aClass.getName(), attrList);
+                mappings.put(aClass, attrList);
         }
 
         if (mappings.isEmpty())
@@ -117,19 +93,13 @@ class MappingFileCreator {
     private List<Attr> processClass(Class aClass, Map<Class, Class> extendedClasses) {
         List<Attr> list = new ArrayList<>();
 
-        List<Class> classes = ClassUtils.getAllSuperclasses(aClass);
-        classes.add(0, aClass);
-        for (Class<?> c : classes) {
-            if (c.getAnnotation(Entity.class) != null || c.getAnnotation(MappedSuperclass.class) != null) {
-                for (Field field : c.getDeclaredFields()) {
-                    Attr.Type type = getAttrType(field);
-                    if (type != null) {
-                        Class extClass = extendedClasses.get(field.getType());
-                        if (extClass != null) {
-                            Attr attr = new Attr(type, field.getName(), extClass.getName());
-                            list.add(attr);
-                        }
-                    }
+        for (Field field : aClass.getDeclaredFields()) {
+            Attr.Type type = getAttrType(field);
+            if (type != null) {
+                Class extClass = extendedClasses.get(field.getType());
+                if (extClass != null) {
+                    Attr attr = new Attr(type, field, extClass.getName());
+                    list.add(attr);
                 }
             }
         }
@@ -150,21 +120,41 @@ class MappingFileCreator {
             return null;
     }
 
-    private Document createDocument(Map<String, List<Attr>> mappings) {
+    private Document createDocument(Map<Class<?>, List<Attr>> mappings) {
         Document doc = DocumentHelper.createDocument();
         Element rootEl = doc.addElement("entity-mappings", XMLNS);
         rootEl.addAttribute("version", PERSISTENCE_VER);
-        for (Map.Entry<String, List<Attr>> entry : mappings.entrySet()) {
-            Element entityEl = rootEl.addElement("entity", XMLNS);
-            entityEl.addAttribute("class", entry.getKey());
-            Element attributesEl = entityEl.addElement("attributes", XMLNS);
-            for (Attr attr : entry.getValue()) {
-                Element attrEl = attributesEl.addElement(attr.type.xml, XMLNS);
-                attrEl.addAttribute("name", attr.name);
-                attrEl.addAttribute("target-entity", attr.targetEntity);
+
+        for (Map.Entry<Class<?>, List<Attr>> entry : mappings.entrySet()) {
+            if (entry.getKey().getAnnotation(MappedSuperclass.class) != null) {
+                Element entityEl = rootEl.addElement("mapped-superclass", XMLNS);
+                entityEl.addAttribute("class", entry.getKey().getName());
+                createAttributes(entry, entityEl);
             }
         }
+        for (Map.Entry<Class<?>, List<Attr>> entry : mappings.entrySet()) {
+            if (entry.getKey().getAnnotation(Entity.class) != null) {
+                Element entityEl = rootEl.addElement("entity", XMLNS);
+                entityEl.addAttribute("class", entry.getKey().getName());
+                entityEl.addAttribute("name", entry.getKey().getAnnotation(Entity.class).name());
+                createAttributes(entry, entityEl);
+            }
+        }
+
         return doc;
+    }
+
+    private void createAttributes(Map.Entry<Class<?>, List<Attr>> entry, Element entityEl) {
+        Element attributesEl = entityEl.addElement("attributes", XMLNS);
+        Collections.sort(entry.getValue(), new Comparator<Attr>() {
+            @Override
+            public int compare(Attr a1, Attr a2) {
+                return a1.type.order - a2.type.order;
+            }
+        });
+        for (Attr attr : entry.getValue()) {
+            attr.toXml(attributesEl);
+        }
     }
 
     private File writeDocument(Document doc) {
@@ -181,5 +171,202 @@ class MappingFileCreator {
             IOUtils.closeQuietly(os);
         }
         return file;
+    }
+
+    private class ClassDef {
+
+        private Class<?> entityClass;
+
+        private ClassDef(Class<?> entityClass) {
+            this.entityClass = entityClass;
+        }
+
+        @Nullable
+        private Element toXml(Element parentEl) {
+            Element el;
+            if (entityClass.getAnnotation(Entity.class) != null) {
+                el = parentEl.addElement("entity", XMLNS);
+            } else if (entityClass.getAnnotation(MappedSuperclass.class) != null) {
+                el = parentEl.addElement("mapped-superclass", XMLNS);
+            } else {
+                log.warn(entityClass + " has neither @Entity nor @MappedSuperclass annotation, ignoring it");
+                return null;
+            }
+            el.addAttribute("class", entityClass.getName());
+            return el;
+        }
+    }
+
+    private static class Attr {
+
+        private enum Type {
+            MANY_TO_ONE(1, "many-to-one") {
+                @Override
+                protected String getFetch(Field field) {
+                    return field.getAnnotation(ManyToOne.class).fetch().name();
+                }
+                @Override
+                protected String getMappedBy(Field field) {
+                    return null;
+                }
+            },
+            ONE_TO_MANY(2, "one-to-many") {
+                @Override
+                protected String getFetch(Field field) {
+                    return field.getAnnotation(OneToMany.class).fetch().name();
+                }
+                @Override
+                protected String getMappedBy(Field field) {
+                    return field.getAnnotation(OneToMany.class).mappedBy();
+                }
+            },
+            ONE_TO_ONE(3, "one-to-one") {
+                @Override
+                protected String getFetch(Field field) {
+                    return field.getAnnotation(OneToOne.class).fetch().name();
+                }
+                @Override
+                protected String getMappedBy(Field field) {
+                    return field.getAnnotation(OneToOne.class).mappedBy();
+                }
+            },
+            MANY_TO_MANY(4, "many-to-many") {
+                @Override
+                protected String getFetch(Field field) {
+                    return field.getAnnotation(ManyToMany.class).fetch().name();
+                }
+                @Override
+                protected String getMappedBy(Field field) {
+                    return field.getAnnotation(ManyToMany.class).mappedBy();
+                }
+            };
+
+            private int order;
+            private String xml;
+
+            private Type(int order, String xml) {
+                this.xml = xml;
+            }
+
+            protected abstract String getFetch(Field field);
+            protected abstract String getMappedBy(Field field);
+        }
+
+        private final Type type;
+        private final Field field;
+        private final String targetEntity;
+
+        private Attr(Type type, Field field, String targetEntity) {
+            this.type = type;
+            this.field = field;
+            this.targetEntity = targetEntity;
+        }
+
+        private Element toXml(Element parentEl) {
+            Element el = parentEl.addElement(type.xml, XMLNS);
+            el.addAttribute("name", field.getName());
+            el.addAttribute("target-entity", targetEntity);
+            el.addAttribute("fetch", type.getFetch(field));
+            String mappedBy = type.getMappedBy(field);
+            if (!StringUtils.isEmpty(mappedBy))
+                el.addAttribute("mapped-by", mappedBy);
+
+            // either
+            new JoinColumnHandler(field.getAnnotation(JoinColumn.class)).toXml(el);
+            // or
+            new OrderByHandler(field.getAnnotation(OrderBy.class)).toXml(el);
+            new JoinTableHandler(field.getAnnotation(JoinTable.class)).toXml(el);
+
+            return el;
+        }
+    }
+
+    private static class JoinColumnHandler {
+
+        private JoinColumn annotation;
+
+        private JoinColumnHandler(JoinColumn annotation) {
+            this.annotation = annotation;
+        }
+
+        protected void toXml(Element parentEl) {
+            if (annotation == null)
+                return;
+
+            Element el = parentEl.addElement(getElementName());
+            el.addAttribute("name", annotation.name());
+
+            if (!StringUtils.isEmpty(annotation.referencedColumnName()))
+                el.addAttribute("referenced-column-name", annotation.referencedColumnName());
+
+            if (annotation.unique())
+                el.addAttribute("unique", "true");
+
+            if (!annotation.nullable())
+                el.addAttribute("nullable", "false");
+
+            if (!annotation.insertable())
+                el.addAttribute("insertable", "false");
+
+            if (!annotation.updatable())
+                el.addAttribute("updatable", "false");
+        }
+
+        protected String getElementName() {
+            return "join-column";
+        }
+    }
+
+    private static class InverseJoinColumnHandler extends JoinColumnHandler {
+
+        private InverseJoinColumnHandler(JoinColumn annotation) {
+            super(annotation);
+        }
+
+        @Override
+        protected String getElementName() {
+            return "inverse-join-column";
+        }
+    }
+
+    private static class JoinTableHandler {
+
+        private JoinTable annotation;
+
+        private JoinTableHandler(JoinTable annotation) {
+            this.annotation = annotation;
+        }
+
+        private void toXml(Element parentEl) {
+            if (annotation == null)
+                return;
+
+            Element el = parentEl.addElement("join-table");
+            el.addAttribute("name", annotation.name());
+
+            for (JoinColumn joinColumnAnnot : annotation.joinColumns()) {
+                new JoinColumnHandler(joinColumnAnnot).toXml(el);
+            }
+            for (JoinColumn joinColumnAnnot : annotation.joinColumns()) {
+                new InverseJoinColumnHandler(joinColumnAnnot).toXml(el);
+            }
+        }
+    }
+
+    private static class OrderByHandler {
+
+        private OrderBy annotation;
+
+        private OrderByHandler(OrderBy annotation) {
+            this.annotation = annotation;
+        }
+
+        private void toXml(Element parentEl) {
+            if (annotation == null)
+                return;
+
+            Element el = parentEl.addElement("order-by");
+            el.setText(annotation.value());
+        }
     }
 }
