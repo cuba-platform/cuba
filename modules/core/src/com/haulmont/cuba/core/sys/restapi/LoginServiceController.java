3075fa0692f3759a765328c30a0964d0c84ff325
Yuriy Artamonov <artamonov@haulmont.com>
2012-10-29 15:27:49 +0000

    Refs #1306 Fix REST-API tests

--- modules/core/src/com/haulmont/cuba/core/sys/restapi/LoginServiceController.java
+++ modules/core/src/com/haulmont/cuba/core/sys/restapi/LoginServiceController.java
@@ -6,7 +6,8 @@
 
 package com.haulmont.cuba.core.sys.restapi;
 
-import com.haulmont.cuba.core.Locator;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Encryption;
 import com.haulmont.cuba.security.app.LoginService;
 import com.haulmont.cuba.security.global.LoginException;
 import com.haulmont.cuba.security.global.UserSession;
@@ -29,9 +30,8 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
- * Author: Alexander Chevelev
- * Date: 19.04.2011
- * Time: 22:03:25
+ * @author chevelev
+ * @version $Id$
  */
 @Controller
 @RequestMapping(value = "/login")
@@ -51,7 +51,7 @@ public class LoginServiceController {
     @RequestMapping(method = RequestMethod.POST)
     public void loginByPost(@RequestBody String requestBody,
                       @RequestHeader(value = "Content-Type") MimeType contentType,
-                      HttpServletResponse response) throws JSONException, IOException, JSONException {
+                      HttpServletResponse response) throws IOException, JSONException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         String username;
         String password;
@@ -59,11 +59,11 @@ public class LoginServiceController {
         if (contentType.match(JSONConvertor.MIME_TYPE_JSON)) {
             JSONObject json = new JSONObject(requestBody);
             username = json.getString("username");
-            password = json.getString("passwordHash");
+            password = json.getString("password");
             locale = new Locale(json.getString("locale"));
         } else if (contentType.match(FORM_TYPE)) {
             String[] pairs = requestBody.split("\\&");
-            Map<String, String> name2value = new HashMap<String, String>();
+            Map<String, String> name2value = new HashMap<>();
             for (String pair : pairs) {
                 String[] fields = pair.split("=");
                 String name = URLDecoder.decode(fields[0], "UTF-8");
@@ -71,19 +71,22 @@ public class LoginServiceController {
                 name2value.put(name, value);
             }
             username = name2value.get("username");
-            password = name2value.get("passwordHash");
+            password = name2value.get("password");
             locale = new Locale(name2value.get("locale"));
         } else {
             throw new IllegalStateException("Unsupported content type: " + contentType);
         }
 
         try {
-            LoginService svc = Locator.lookup(LoginService.NAME);
-            UserSession userSession = svc.login(username, password, locale);
+            Encryption encryption = AppBeans.get(Encryption.NAME);
+            LoginService svc = AppBeans.get(LoginService.NAME);
+            UserSession userSession = svc.login(username, encryption.getPlainHash(password), locale);
             response.setStatus(HttpServletResponse.SC_OK);
             PrintWriter writer = new PrintWriter(response.getOutputStream());
             writer.write(userSession.getId().toString());
             writer.close();
+
+            log.debug(String.format("User %s logged in with REST-API, session id: %s", username, userSession.getId()));
         } catch (LoginException e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
@@ -91,22 +94,24 @@ public class LoginServiceController {
 
     @RequestMapping(method = RequestMethod.GET)
     public void loginByGet(@RequestParam(value = "u") String username,
-                           @RequestParam(value = "p") String passwordHash,
+                           @RequestParam(value = "p") String password,
                            @RequestParam(value = "l") String localeStr,
                            HttpServletResponse response) throws IOException, JSONException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         Locale locale = StringUtils.isBlank(localeStr) ? new Locale("en") : new Locale(localeStr);
         try {
-            LoginService svc = Locator.lookup(LoginService.NAME);
-            UserSession userSession = svc.login(username, passwordHash, locale);
+            LoginService svc = AppBeans.get(LoginService.NAME);
+            Encryption encryption = AppBeans.get(Encryption.NAME);
+
+            UserSession userSession = svc.login(username, encryption.getPlainHash(password), locale);
             response.setStatus(HttpServletResponse.SC_OK);
             PrintWriter writer = new PrintWriter(response.getOutputStream());
             writer.write(userSession.getId().toString());
             writer.close();
+
+            log.debug(String.format("User %s logged in with REST-API, session id: %s", username, userSession.getId()));
         } catch (LoginException e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
-
     }
-
 }
