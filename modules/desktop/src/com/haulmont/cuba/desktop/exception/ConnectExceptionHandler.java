7107b7e00e5df9b17340a2710192f7ddb0799372
Konstantin Krivopustov <krivopustov@haulmont.com>
2012-12-18 06:15:04 +0000

    Refs #1642 [Desktop] Error shown as "Unexpected exception" instead of "Server connection error"

--- modules/desktop/src/com/haulmont/cuba/desktop/exception/ConnectExceptionHandler.java
+++ modules/desktop/src/com/haulmont/cuba/desktop/exception/ConnectExceptionHandler.java
@@ -6,32 +6,39 @@
 
 package com.haulmont.cuba.desktop.exception;
 
-import com.haulmont.cuba.core.global.MessageProvider;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.cuba.desktop.App;
 import com.haulmont.cuba.gui.components.IFrame;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.springframework.remoting.RemoteAccessException;
 
+import java.util.List;
+
 /**
- * <p>$Id$</p>
- *
  * @author krivopustov
+ * @version $Id$
  */
 public class ConnectExceptionHandler implements ExceptionHandler {
 
     @Override
     public boolean handle(Thread thread, Throwable exception) {
-        if (exception instanceof RemoteAccessException) {
-            String msg = MessageProvider.getMessage(getClass(), "connectException.message");
-            if (exception.getCause() == null) {
-                App.getInstance().getMainFrame().showNotification(msg, IFrame.NotificationType.ERROR);
-            } else {
-                String description = MessageProvider.formatMessage(getClass(), "connectException.description",
-                        exception.getCause().toString());
-                App.getInstance().getMainFrame().showNotification(msg, description, IFrame.NotificationType.ERROR);
+        @SuppressWarnings("unchecked")
+        List<Throwable> list = ExceptionUtils.getThrowableList(exception);
+        for (Throwable throwable : list) {
+            if (throwable instanceof RemoteAccessException) {
+                Messages messages = AppBeans.get(Messages.NAME);
+                String msg = messages.getMessage(getClass(), "connectException.message");
+                if (throwable.getCause() == null) {
+                    App.getInstance().getMainFrame().showNotification(msg, IFrame.NotificationType.ERROR);
+                } else {
+                    String description = messages.formatMessage(getClass(), "connectException.description",
+                            throwable.getCause().toString());
+                    App.getInstance().getMainFrame().showNotification(msg, description, IFrame.NotificationType.ERROR);
+                }
+                return true;
             }
-            return true;
         }
-        else
-            return false;
+        return false;
     }
 }
