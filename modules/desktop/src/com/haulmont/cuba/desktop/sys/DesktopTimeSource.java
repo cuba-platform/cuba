2af8f0d9a0b52e1d36f66790f2d658b95698964d
Alexander Budarov <budarov@haulmont.com>
2013-02-01 08:16:28 +0000

    synchronization documenting annotations #PL-1745

--- modules/desktop/src/com/haulmont/cuba/desktop/sys/DesktopTimeSource.java
+++ modules/desktop/src/com/haulmont/cuba/desktop/sys/DesktopTimeSource.java
@@ -8,6 +8,7 @@ package com.haulmont.cuba.desktop.sys;
 
 import com.haulmont.cuba.core.global.TimeSource;
 
+import javax.annotation.concurrent.GuardedBy;
 import java.util.Date;
 
 /**
@@ -23,7 +24,7 @@ public class DesktopTimeSource implements TimeSource {
      * Time offset (time difference between server and client machine).
      * It will be used to correct time obtained from system clock.
      */
-    // @GuardedBy(this)
+    @GuardedBy("this")
     protected long timeOffset;
 
     @Override
