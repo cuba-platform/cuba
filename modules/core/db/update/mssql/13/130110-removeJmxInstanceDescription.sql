654299a4ddb33e338da4344bcaea777a098ba784
Yuriy Artamonov <artamonov@haulmont.com>
2013-03-18 07:41:50 +0000

    Rename db script 131001-removeJmxInstanceDescription.sql

---
+++ modules/core/db/update/mssql/13/130110-removeJmxInstanceDescription.sql
@@ -0,0 +1,9 @@
+-- $Id$
+-- Description: remove column DESCRIPTION from SYS_JMX_INSTANCE table
+
+if not exists(select SCRIPT_NAME from SYS_DB_CHANGELOG where SCRIPT_NAME like '%131001-removeJmxInstanceDescription.sql')
+begin
+  alter table SYS_JMX_INSTANCE drop column DESCRIPTION;
+
+  exec sp_rename 'SYS_JMX_INSTANCE.CLUSTER_NODE_NAME', 'NODE_NAME', 'COLUMN';
+end
--- modules/core/db/update/postgres/13/131001-removeJmxInstanceDescription.sql
+++
@@ -1,6 +0,0 @@
--- $Id$
--- Description: remove column DESCRIPTION from SYS_JMX_INSTANCE table
-
-alter table SYS_JMX_INSTANCE drop column DESCRIPTION^
-
-alter table SYS_JMX_INSTANCE rename column CLUSTER_NODE_NAME to NODE_NAME^
