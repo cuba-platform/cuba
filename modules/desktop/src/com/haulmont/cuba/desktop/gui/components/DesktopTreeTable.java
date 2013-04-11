48f06334b4235ef69180d2577ac3f40490e6b7a2
Yuriy Artamonov <artamonov@haulmont.com>
2013-04-11 13:07:48 +0000

    Desktop tree table bug with Refresh action #PL-2034

--- modules/desktop/src/com/haulmont/cuba/desktop/gui/components/DesktopTreeTable.java
+++ modules/desktop/src/com/haulmont/cuba/desktop/gui/components/DesktopTreeTable.java
@@ -152,11 +152,25 @@ public class DesktopTreeTable
                 new TreeSelectionListener() {
                     @Override
                     public void valueChanged(TreeSelectionEvent e) {
-                        if (isRowsAjusting)
+                        if (isRowsAjusting || isRowSelecting)
                             return;
 
-                        Entity entity = getSingleSelected();
-                        datasource.setItem(entity);
+                        isRowSelecting = true;
+                        try {
+                            selectionBackup = getSelected();
+
+                            final Set selected = getSelected();
+                            if (selected.isEmpty()) {
+                                datasource.setItem(null);
+                            } else {
+                                // reset selection and select new item
+                                if (isMultiSelect())
+                                    datasource.setItem(null);
+                                datasource.setItem((Entity) selected.iterator().next());
+                            }
+                        } finally {
+                            isRowSelecting = false;
+                        }
                     }
                 }
         );
