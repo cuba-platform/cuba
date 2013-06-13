dccb0fc85bf542c9a130d39112a32961212d1719
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-06-13 07:36:33 +0000

    NPE occurs inside of EntityBuildFactory when retrieving property for invalid metaclass. #PL-2208

--- modules/client/src/com/haulmont/cuba/client/sys/config/EntityFactory.java
+++ modules/client/src/com/haulmont/cuba/client/sys/config/EntityFactory.java
@@ -35,6 +35,8 @@ public class EntityFactory extends TypeFactory {
         if (StringUtils.isBlank(string))
             return null;
         EntityLoadInfo info = EntityLoadInfo.parse(string);
+        if (info == null)
+            throw new IllegalArgumentException("Invalid entity info: " + string);
 
         LoadContext ctx = new LoadContext(info.getMetaClass()).setId(info.getId());
         if (info.getViewName() != null)
