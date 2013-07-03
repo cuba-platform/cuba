49294582716e6c05a4e14d54149f32e80c256f3d
Denis Hasanov <hasanov@haulmont.com>
2013-07-03 06:15:23 +0000

    EmailerMBean#sendTestEmail writes exception to log. #PL-1884

--- modules/core/src/com/haulmont/cuba/core/jmx/Emailer.java
+++ modules/core/src/com/haulmont/cuba/core/jmx/Emailer.java
@@ -71,6 +71,7 @@ public class Emailer implements EmailerMBean {
         return config.getSmtpStarttlsEnable();
     }
 
+    @Authenticated
     @Override
     public String sendTestEmail(String addresses) {
         try {
