decb42a018d913e09a73160323f6785385e4e2ad
Andrey Tsarevskiy <tsarevskiy@haulmont.com>
2013-07-19 08:35:35 +0000

    Переделан update скрипт падающий при миграции тезиса на проверку sys_db_changelog #PL-2278

--- modules/core/db/update/mssql/12/121004-addFieldsToScheduledTasks.sql
+++ modules/core/db/update/mssql/12/121004-addFieldsToScheduledTasks.sql
@@ -2,10 +2,9 @@
 -- Description: add defined by, class name and script name fields to scheduled task
 -- also update all existing tasks to beans
 
-if not exists (select * from sys.columns
-            where Name = N'DEFINED_BY' and Object_ID = Object_ID(N'SYS_SCHEDULED_TASK'))
+if not exists(select SCRIPT_NAME from SYS_DB_CHANGELOG where SCRIPT_NAME like '%02-120-alterScheduledTask.sql')
 begin
   alter table SYS_SCHEDULED_TASK add DEFINED_BY varchar(1) default 'B';
   alter table SYS_SCHEDULED_TASK add CLASS_NAME varchar(500);
   alter table SYS_SCHEDULED_TASK add SCRIPT_NAME varchar(500);
-end ;
+end
--- modules/core/db/update/postgres/12/121004-addFieldsToScheduledTasks.sql
+++ modules/core/db/update/postgres/12/121004-addFieldsToScheduledTasks.sql
@@ -2,20 +2,12 @@
 -- Description: add defined by, class name and script name fields to scheduled task
 -- also update all existing tasks to beans
 
-create or replace function addColumnsToScheduledTask() returns void as
-$$
+
+do $$
 begin
-    if (not (exists (select * from information_schema.columns
-        where table_name = 'sys_scheduled_task' and column_name = 'defined_by'))) then
-            alter table SYS_SCHEDULED_TASK add column DEFINED_BY varchar(1) default 'B';
-            alter table SYS_SCHEDULED_TASK add column CLASS_NAME varchar(500);
-            alter table SYS_SCHEDULED_TASK add column SCRIPT_NAME varchar(500);
-        end if;
-end;
-$$
-language 'plpgsql'^
-
-select addColumnsToScheduledTask()^
-
-drop function addColumnsToScheduledTask();
-
+    if not exists(select SCRIPT_NAME from SYS_DB_CHANGELOG where SCRIPT_NAME like '%02-140-alterScheduledTask.sql') then
+        alter table SYS_SCHEDULED_TASK add column DEFINED_BY varchar(1) default 'B';
+        alter table SYS_SCHEDULED_TASK add column CLASS_NAME varchar(500);
+        alter table SYS_SCHEDULED_TASK add column SCRIPT_NAME varchar(500);
+    end if;
+end $$;
