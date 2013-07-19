fab0db60fe7f4cf0e50e47e11f7d4bc3ae4786fa
Andrey Tsarevskiy <tsarevskiy@haulmont.com>
2013-07-19 07:37:42 +0000

    Поправлен update скрипт падающий при миграции тезиса #PL-2278

--- modules/core/db/update/mssql/12/121004-addFieldsToScheduledTasks.sql
+++ modules/core/db/update/mssql/12/121004-addFieldsToScheduledTasks.sql
@@ -2,7 +2,10 @@
 -- Description: add defined by, class name and script name fields to scheduled task
 -- also update all existing tasks to beans
 
-alter table SYS_SCHEDULED_TASK add column DEFINED_BY varchar(1) default 'B';
-alter table SYS_SCHEDULED_TASK add column CLASS_NAME varchar(500);
-alter table SYS_SCHEDULED_TASK add column SCRIPT_NAME varchar(500);
-
+if not exists (select * from sys.columns
+            where Name = N'DEFINED_BY' and Object_ID = Object_ID(N'SYS_SCHEDULED_TASK'))
+begin
+  alter table SYS_SCHEDULED_TASK add DEFINED_BY varchar(1) default 'B';
+  alter table SYS_SCHEDULED_TASK add CLASS_NAME varchar(500);
+  alter table SYS_SCHEDULED_TASK add SCRIPT_NAME varchar(500);
+end ;
--- modules/core/db/update/postgres/12/121004-addFieldsToScheduledTasks.sql
+++ modules/core/db/update/postgres/12/121004-addFieldsToScheduledTasks.sql
@@ -2,7 +2,20 @@
 -- Description: add defined by, class name and script name fields to scheduled task
 -- also update all existing tasks to beans
 
-alter table SYS_SCHEDULED_TASK add column DEFINED_BY varchar(1) default 'B';
-alter table SYS_SCHEDULED_TASK add column CLASS_NAME varchar(500);
-alter table SYS_SCHEDULED_TASK add column SCRIPT_NAME varchar(500);
+create or replace function addColumnsToScheduledTask() returns void as
+$$
+begin
+    if (not (exists (select * from information_schema.columns
+        where table_name = 'sys_scheduled_task' and column_name = 'defined_by'))) then
+            alter table SYS_SCHEDULED_TASK add column DEFINED_BY varchar(1) default 'B';
+            alter table SYS_SCHEDULED_TASK add column CLASS_NAME varchar(500);
+            alter table SYS_SCHEDULED_TASK add column SCRIPT_NAME varchar(500);
+        end if;
+end;
+$$
+language 'plpgsql'^
+
+select addColumnsToScheduledTask()^
+
+drop function addColumnsToScheduledTask();
 
