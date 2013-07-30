2eb99770c641014e05ee2e75f2316d6462b01b65
Vladimir Shakhov <shakhov@haulmont.com>
2013-07-30 12:34:58 +0000

    Изменена сигнатура метода isTransient(entity, property.getName()) на isTransient(property) #PL-2319

--- modules/portal/src/com/haulmont/cuba/portal/restapi/XMLConvertor.java
+++ modules/portal/src/com/haulmont/cuba/portal/restapi/XMLConvertor.java
@@ -454,7 +454,7 @@ public class XMLConvertor implements Convertor {
         List<MetaProperty> properties = ConvertorHelper.getOrderedProperties(metaClass);
         for (MetaProperty property : properties) {
             Element child;
-            if (metadataTools.isTransient(entity, property.getName()))
+            if (metadataTools.isTransient(property))
                 continue;
 
             if (!attrViewPermitted(metaClass, property.getName()))
