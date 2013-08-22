91a40efa073c91ee9704a42b2934c775c505759c
Denis Hasanov <hasanov@haulmont.com>
2013-08-22 09:26:26 +0000

    Email History screen - displaying content text. #PL-2381

--- modules/gui/src/com/haulmont/cuba/gui/app/core/sendingmessage/browse/SendingMessageBrowser.java
+++ modules/gui/src/com/haulmont/cuba/gui/app/core/sendingmessage/browse/SendingMessageBrowser.java
@@ -7,11 +7,12 @@
 package com.haulmont.cuba.gui.app.core.sendingmessage.browse;
 
 import com.haulmont.cuba.core.entity.SendingMessage;
-import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.data.DataSupplier;
 import com.haulmont.cuba.gui.data.Datasource;
 import com.haulmont.cuba.gui.data.impl.DsListenerAdapter;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 
 import javax.inject.Inject;
 import java.util.Map;
@@ -31,8 +32,25 @@ public class SendingMessageBrowser extends AbstractWindow {
     @Inject
     protected DataSupplier dataSupplier;
 
+    @Inject
+    protected FieldGroup fg;
+
+    @Inject
+    protected ComponentsFactory factory;
+
     @Override
     public void init(Map<String, Object> params) {
+        fg.addCustomField("contentText", new FieldGroup.CustomFieldGenerator() {
+            @Override
+            public Component generateField(Datasource datasource, String propertyId) {
+                final TextArea textArea = factory.createComponent(TextArea.NAME);
+                textArea.setDatasource(selectedMessageDs, "contentText");
+                textArea.setRows(20);
+                textArea.setHeight("350px");
+                textArea.setEditable(false);
+                return textArea;
+            }
+        });
         sendingMessageDs.addListener(new DsListenerAdapter<SendingMessage>() {
             @Override
             public void itemChanged(Datasource<SendingMessage> ds, SendingMessage prevItem, SendingMessage item) {
