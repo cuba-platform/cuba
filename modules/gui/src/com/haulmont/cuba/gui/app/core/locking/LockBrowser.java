711c9ed01c89a43dede5968f3920250b2f674bdc
Denis Hasanov <hasanov@haulmont.com>
2013-08-23 08:50:14 +0000

    Lock Editor - open as dialog. #PL-2414

--- modules/gui/src/com/haulmont/cuba/gui/app/core/locking/LockBrowser.java
+++ modules/gui/src/com/haulmont/cuba/gui/app/core/locking/LockBrowser.java
@@ -6,13 +6,11 @@
 package com.haulmont.cuba.gui.app.core.locking;
 
 import com.haulmont.cuba.core.app.LockService;
-import com.haulmont.cuba.core.entity.Entity;
-import com.haulmont.cuba.core.entity.LockDescriptor;
-import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.LockInfo;
-import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.cuba.gui.WindowManager;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.CreateAction;
+import com.haulmont.cuba.gui.components.actions.EditAction;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -26,48 +24,18 @@ public class LockBrowser extends AbstractWindow {
     @Named("locks")
     protected Table table;
 
+    @Named("setupTable.create")
+    protected CreateAction createAction;
+
+    @Named("setupTable.edit")
+    protected EditAction editAction;
+
     @Inject
     protected Table setupTable;
 
     public void init(Map<String, Object> params) {
-        setupTable.addAction(new AbstractAction("create") {
-            @Override
-            public void actionPerform(Component component) {
-                Window window = openEditor("sys$LockDescriptor.edit", new LockDescriptor(), WindowManager.OpenType.DIALOG);
-                window.addListener(new CloseListener() {
-                    @Override
-                    public void windowClosed(String actionId) {
-                        getDsContext().get("lockDescriptorDs").refresh();
-                    }
-                });
-            }
-
-            @Override
-            public String getCaption() {
-                return AppBeans.get(Messages.class).getMainMessage("actions.Create");
-            }
-        });
-
-        setupTable.addAction(new AbstractAction("edit") {
-            @Override
-            public void actionPerform(Component component) {
-                Entity entity = getDsContext().get("lockDescriptorDs").getItem();
-                if (entity != null) {
-                    Window window = openEditor("sys$LockDescriptor.edit", entity, WindowManager.OpenType.DIALOG);
-                    window.addListener(new CloseListener() {
-                        @Override
-                        public void windowClosed(String actionId) {
-                            getDsContext().get("lockDescriptorDs").refresh();
-                        }
-                    });
-                }
-            }
-
-            @Override
-            public String getCaption() {
-                return AppBeans.get(Messages.class).getMainMessage("actions.Edit");
-            }
-        });
+        createAction.setOpenType(WindowManager.OpenType.DIALOG);
+        editAction.setOpenType(WindowManager.OpenType.DIALOG);
         table.refresh();
     }
 
