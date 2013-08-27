c98863cf21808e7f248ff661fd7ffd0e3d8b5808
Yuriy Artamonov <artamonov@haulmont.com>
2013-08-27 14:40:38 +0000

    Wrong date in DateField #PL-2354

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/datefield/CubaDateFieldWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/datefield/CubaDateFieldWidget.java
@@ -25,6 +25,21 @@ public class CubaDateFieldWidget extends VPopupCalendar {
     }
 
     @Override
+    public void setTextFieldEnabled(boolean textFieldEnabled) {
+        super.setTextFieldEnabled(textFieldEnabled);
+
+        calendarToggle.getElement().setTabIndex(-1);
+    }
+
+    @Override
+    protected void onAttach() {
+        super.onAttach();
+
+        // Always set -1 tab index for calendarToggle
+        calendarToggle.setTabIndex(-1);
+    }
+
+    @Override
     public CubaMaskedFieldWidget getImpl() {
         return (CubaMaskedFieldWidget) super.getImpl();
     }
@@ -32,6 +47,26 @@ public class CubaDateFieldWidget extends VPopupCalendar {
     @Override
     protected CubaMaskedFieldWidget createImpl() {
         return new CubaMaskedFieldWidget() {
+
+            @Override
+            protected boolean validateText(String text) {
+                if (text.equals(nullRepresentation)) {
+                    return true;
+                }
+
+                boolean validMask = super.validateText(text);
+                if (!validMask)
+                    return false;
+
+                try {
+                    getDateTimeService().parseDate(getText(), getFormatString(), lenient);
+                } catch (Exception e) {
+                    return false;
+                }
+
+                return validMask;
+            }
+
             public void valueChange(boolean blurred) {
                 String newText = getText();
                 if (!prompting && newText != null
@@ -41,6 +76,7 @@ public class CubaDateFieldWidget extends VPopupCalendar {
                             getElement().removeClassName(CubaDateFieldWidget.EMPTY_FIELD_CLASS);
                         }
                         CubaDateFieldWidget.this.onChange(null);
+
                         valueBeforeEdit = newText;
                     } else {
                         setText(valueBeforeEdit);
