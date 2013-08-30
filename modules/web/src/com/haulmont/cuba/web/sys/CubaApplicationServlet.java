e1216fadfdbea25269c0e53cf7e50dc8fafef308
Yuriy Artamonov <artamonov@haulmont.com>
2013-08-30 13:16:45 +0000

    Remove ?restartApp from URI after redirect #PL-2476

--- modules/web/src/com/haulmont/cuba/web/sys/CubaApplicationServlet.java
+++ modules/web/src/com/haulmont/cuba/web/sys/CubaApplicationServlet.java
@@ -14,6 +14,7 @@ import com.vaadin.server.DeploymentConfiguration;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinServletService;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -65,6 +66,9 @@ public class CubaApplicationServlet extends VaadinServlet {
 
         if (request.getParameter("restartApp") != null) {
             request.getSession().invalidate();
+            // remove parameters
+            requestURI = StringUtils.substring(requestURI, 0, StringUtils.indexOf(requestURI, '?'));
+
             response.sendRedirect(requestURI);
             return;
         }
