d00b2439103dd32c515c4da4847e667514fac650
Aleksey Kozyaikin <kozyaikin@haulmont.com>
2013-09-03 09:12:23 +0000

    Folders refactoring. #PL-2511

--- modules/web-auth/src/com/haulmont/cuba/web/auth/WebAuthConfig.java
+++ modules/web-auth/src/com/haulmont/cuba/web/auth/WebAuthConfig.java
@@ -4,7 +4,6 @@ import com.haulmont.cuba.core.config.Config;
 import com.haulmont.cuba.core.config.Property;
 import com.haulmont.cuba.core.config.Source;
 import com.haulmont.cuba.core.config.SourceType;
-import com.haulmont.cuba.core.config.defaults.Default;
 import com.haulmont.cuba.core.config.defaults.DefaultBoolean;
 import com.haulmont.cuba.core.config.defaults.DefaultString;
 
