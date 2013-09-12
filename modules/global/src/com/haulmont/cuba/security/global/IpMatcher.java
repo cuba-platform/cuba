33dcec0c3820cf9b5d594362dd8e932999ded9a2
Dmitry Kozlov <kozlov@haulmont.com>
2013-09-12 13:24:05 +0000

    IpMatcher improvements and IPv6 masks support #PL-1245 fixed

--- modules/global/src/com/haulmont/cuba/security/global/IpMatcher.java
+++ modules/global/src/com/haulmont/cuba/security/global/IpMatcher.java
@@ -10,58 +10,91 @@
  */
 package com.haulmont.cuba.security.global;
 
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * @version $Id$
+ */
 public class IpMatcher {
 
-    private List<String> masks = new ArrayList<String>();
+    private static Set<String> whiteListedIPs = ImmutableSet.of("127.0.0.1", "0:0:0:0:0:0:0:1");
+    private List<String[]> masks = new ArrayList<>();
 
     private Log log = LogFactory.getLog(IpMatcher.class);
 
-    public IpMatcher(String mask) {
-        String[] strings = mask.split("[,;]");
-        for (String string : strings) {
-            masks.add(string.trim());
+    public IpMatcher(String source) {
+        String[] parts = source.split("[,;]");
+        for (String part : parts) {
+            String m = part.trim();
+            String[] mask = ipv4(m);
+            if (mask == null) {
+                mask = ipv6(m);
+                if (mask == null) {
+                    log.warn("Invalid IP mask: '" + m + "'");
+                    continue;
+                }
+            }
+            masks.add(mask);
         }
     }
 
     public boolean match(String ip) {
-        if (ip == null || ip.equals("") || ip.equals("127.0.0.1"))
+        if (StringUtils.isBlank(ip) || whiteListedIPs.contains(ip)) {
             return true;
+        }
+        if (masks.isEmpty()) {
+            return true;
+        }
 
-        for (String mask : masks) {
-            if (matchMask(mask, ip))
+        String[] ipv = ipv4(ip);
+        if (ipv == null) {
+            ipv = ipv6(ip);
+            if (ipv == null) {
+                log.warn("IP format not supported: '" + ip + "'");
+                return true;
+            }
+        }
+
+        for (String[] mask : masks) {
+            if (match(mask, ipv))
                 return true;
         }
         return false;
     }
 
-    private boolean matchMask(String mask, String ip) {
-        if (mask.equals(ip))
-            return true;
-
-        String[] maskParts = mask.split("\\.");
-        if (maskParts.length != 4) {
-            log.warn("Invalid IP mask: " + mask);
-            return true;
+    private boolean match(String[] mask, String[] ip) {
+        if (mask.length != ip.length) {
+            return false;
         }
-
-        String[] ipParts = ip.split("\\.");
-        if (ipParts.length != 4) {
-            log.warn("IP format not supported: " + ip);
-            return true;
-        }
-
-        for (int i = 0; i < maskParts.length; i++) {
-            String maskPart = maskParts[i];
-            if (!maskPart.equals("*") && !maskPart.equals(ipParts[i])) {
+        for (int j = 0; j < mask.length; j++) {
+            String mp = mask[j];
+            if (!mp.equals("*") && !ip[j].equals(mp)) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static String[] ipv4(String ip) {
+        String[] ipp = ip.split("\\.");
+        if (ipp.length != 4) {
+            return null;
+        }
+        return ipp;
+    }
+
+    private static String[] ipv6(String ip) {
+        String[] ipp = ip.split(":");
+        if (ipp.length != 8) {
+            return null;
+        }
+        return ipp;
     }
 }
