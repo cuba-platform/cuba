378c48eb6a2b3c5a2810f79dbaa9aeed6190d531
Sergey Saiyan <saiyan@haulmont.com>
2013-09-22 08:06:22 +0000

    fix typo in ms sql update script #PL-2368

--- modules/core/db/update/mssql/13/130920-email-file-storage.sql
+++ modules/core/db/update/mssql/13/130920-email-file-storage.sql
@@ -1,10 +1,10 @@
 -- $Id$ --
 -- Add linking columns from SendingMessage and SendingAttachment to sys$File
 
-alter table SYS_SENDING_MESSAGE add column CONTENT_TEXT_FILE_ID uniqueidentifier^
+alter table SYS_SENDING_MESSAGE add CONTENT_TEXT_FILE_ID uniqueidentifier^
 alter table SYS_SENDING_MESSAGE add constraint FK_SYS_SENDING_MESSAGE_CONTENT_FILE
 foreign key (CONTENT_TEXT_FILE_ID) references SYS_FILE(ID)^
 
-alter table SYS_SENDING_ATTACHMENT add column CONTENT_FILE_ID uniqueidentifier^
+alter table SYS_SENDING_ATTACHMENT add CONTENT_FILE_ID uniqueidentifier^
 alter table SYS_SENDING_ATTACHMENT add constraint FK_SYS_SENDING_ATTACHMENT_CONTENT_FILE
 foreign key (CONTENT_FILE_ID) references SYS_FILE (ID)^
