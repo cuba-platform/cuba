65f6ddc0d935a52c4b3eb21598a946073a6adc1e
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-09-23 07:54:26 +0000

    Refactor main menu and screen identifiers. #PL-2687

---
+++ modules/core/db/update/mssql/13/130923-permissionsToRenamedScreens.sql
@@ -0,0 +1,10 @@
+-- $Id$
+-- Update permissions to renamed screens
+
+update SEC_PERMISSION set TARGET = 'sys$Category.browse' where PERMISSION_TYPE = 10 and TARGET = 'sys$Categories.browse';
+
+update SEC_PERMISSION set TARGET = 'entityRestore' where PERMISSION_TYPE = 10 and TARGET = 'sys$Entity.restore';
+
+update SEC_PERMISSION set TARGET = 'jmxConsole' where PERMISSION_TYPE = 10 and TARGET = 'jmxcontrol$DisplayMbeans';
+
+update SEC_PERMISSION set TARGET = 'serverLog' where PERMISSION_TYPE = 10 and TARGET = 'logcontrol$ServerLog';
