aef4c28252d2feb9919c7ca385ed1679c7b2774a
Yuriy Artamonov <artamonov@haulmont.com>
2013-09-23 13:03:49 +0000

    Redirect to base app Url if no handlers for sub path #PL-2584

--- modules/web/src/com/haulmont/cuba/web/sys/CubaVaadinServletService.java
+++ modules/web/src/com/haulmont/cuba/web/sys/CubaVaadinServletService.java
@@ -12,10 +12,14 @@ import com.haulmont.cuba.core.global.Resources;
 import com.haulmont.cuba.core.sys.AppContext;
 import com.haulmont.cuba.web.App;
 import com.haulmont.cuba.web.WebConfig;
+import com.haulmont.cuba.web.auth.RequestContext;
 import com.vaadin.server.*;
 import com.vaadin.server.communication.PublishedFileHandler;
+import com.vaadin.server.communication.ServletBootstrapHandler;
 import org.apache.commons.lang.StringUtils;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -64,8 +68,6 @@ public class CubaVaadinServletService extends VaadinServletService {
             public SystemMessages getSystemMessages(SystemMessagesInfo systemMessagesInfo) {
                 Locale locale = systemMessagesInfo.getLocale();
 
-                String webContext = AppContext.getProperty("cuba.webContextName");
-
                 CustomizedSystemMessages msgs = new CustomizedSystemMessages();
 
                 if (AppContext.isStarted()) {
@@ -81,8 +83,16 @@ public class CubaVaadinServletService extends VaadinServletService {
                     msgs.setSessionExpiredMessage(messages.getMainMessage("sessionExpiredErrorMessage", locale));
                 }
                 msgs.setOutOfSyncNotificationEnabled(false);
-                // todo replace with normal address from request
-                msgs.setInternalErrorURL("/" + webContext + "?restartApp");
+
+                String redirectUri;
+                if (RequestContext.get() != null) {
+                    redirectUri = RequestContext.get().getRequest().getRequestURI();
+                } else {
+                    String webContext = AppContext.getProperty("cuba.webContextName");
+                    redirectUri = "/" + webContext;
+                }
+
+                msgs.setInternalErrorURL(redirectUri + "?restartApp");
 
                 return msgs;
             }
@@ -103,13 +113,17 @@ public class CubaVaadinServletService extends VaadinServletService {
     @Override
     protected List<RequestHandler> createRequestHandlers() throws ServiceException {
         List<RequestHandler> requestHandlers = super.createRequestHandlers();
-        // replace PublishedFileHandler for support resources from VAADIN directory
 
         List<RequestHandler> cubaRequestHandlers = new ArrayList<>();
+
         for (RequestHandler handler : requestHandlers) {
             if (handler instanceof PublishedFileHandler) {
                 // replace PublishedFileHandler with CubaPublishedFileHandler
+                // for support resources from VAADIN directory
                 cubaRequestHandlers.add(new CubaPublishedFileHandler());
+            } else if (handler instanceof ServletBootstrapHandler) {
+                // replace ServletBootstrapHandler with CubaApplicationBootstrapHandler
+                cubaRequestHandlers.add(new CubaApplicationBootstrapHandler());
             } else {
                 cubaRequestHandlers.add(handler);
             }
@@ -123,6 +137,26 @@ public class CubaVaadinServletService extends VaadinServletService {
         @Override
         protected InputStream getApplicationResourceAsStream(Class<?> contextClass, String fileName) {
             return VaadinServlet.getCurrent().getServletContext().getResourceAsStream("VAADIN/" + fileName);
+        }
+    }
+
+    // Add ability to redirect to base application URL if we have unparsable path tail
+    protected static class CubaApplicationBootstrapHandler extends ServletBootstrapHandler {
+
+        @Override
+        public boolean handleRequest(VaadinSession session, VaadinRequest request, VaadinResponse response)
+                throws IOException {
+            String requestPath = request.getPathInfo();
+
+            // redirect to base URL if we have unparsable path tail
+            if (!StringUtils.equals("/", requestPath)) {
+                response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+                response.setHeader("Location", request.getContextPath());
+
+                return true;
+            }
+
+            return super.handleRequest(session, request, response);
         }
     }
 }
