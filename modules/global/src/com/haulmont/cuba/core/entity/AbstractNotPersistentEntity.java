86c01d39bef0a1aae05185dd766c864780d4c760
Konstantin Devyatkin <devyatkin@haulmont.com>
2013-10-09 12:40:02 +0000

    ReportEditor - проблемы с сохранением #PL-2810

--- modules/global/src/com/haulmont/cuba/core/entity/AbstractNotPersistentEntity.java
+++ modules/global/src/com/haulmont/cuba/core/entity/AbstractNotPersistentEntity.java
@@ -74,4 +74,19 @@ public abstract class AbstractNotPersistentEntity extends AbstractInstance imple
     public String toString() {
         return getClass().getName() + "-" + uuid;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractNotPersistentEntity that = (AbstractNotPersistentEntity) o;
+
+        return !(getId() != null ? !getId().equals(that.getId()) : that.getId() != null);
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
 }
