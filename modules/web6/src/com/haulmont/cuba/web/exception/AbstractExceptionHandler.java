cf7a4f5b9dab17bb15f12a0df9901f5857c3d3db
Yuriy Artamonov <artamonov@haulmont.com>
2013-10-22 07:11:30 +0000

    In some cases RemoteException contains causes not supported by client. #PL-2678

--- modules/web6/src/com/haulmont/cuba/web/exception/AbstractExceptionHandler.java
+++ modules/web6/src/com/haulmont/cuba/web/exception/AbstractExceptionHandler.java
@@ -14,17 +14,18 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Base class for exception handlers determining their ability to handle an exception by its class name.
- *
+ * Base class for exception handlers determining their ability to handle an exception by its class name and optional
+ * implementation of {@link #canHandle(String, String, Throwable)} method.
+ * <p/>
  * <p>If you need to handle a specific exception, create a descendant of this class,
  * pass handling exception class names into constructor, implement
  * {@link #doHandle(com.haulmont.cuba.web.App, String, String, Throwable)} method
  * and register the new handler in the definition of {@link ExceptionHandlersConfiguration} bean in the client's
  * spring.xml.
- *
- * <p>$Id$</p>
+ * <p/>
  *
  * @author krivopustov
+ * @version $Id$
  */
 public abstract class AbstractExceptionHandler implements ExceptionHandler {
 
@@ -37,16 +38,19 @@ public abstract class AbstractExceptionHandler implements ExceptionHandler {
     @Override
     public boolean handle(Terminal.ErrorEvent event, App app) {
         Throwable exception = event.getThrowable();
+        //noinspection unchecked
         List<Throwable> list = ExceptionUtils.getThrowableList(exception);
         for (Throwable throwable : list) {
-            if (classNames.contains(throwable.getClass().getName())) {
+            if (classNames.contains(throwable.getClass().getName())
+                    && canHandle(throwable.getClass().getName(), throwable.getMessage(), throwable)) {
                 doHandle(app, throwable.getClass().getName(), throwable.getMessage(), throwable);
                 return true;
             }
             if (throwable instanceof RemoteException) {
                 RemoteException remoteException = (RemoteException) throwable;
                 for (RemoteException.Cause cause : remoteException.getCauses()) {
-                    if (classNames.contains(cause.getClassName())) {
+                    if (classNames.contains(cause.getClassName())
+                            && canHandle(cause.getClassName(), cause.getMessage(), cause.getThrowable())) {
                         doHandle(app, cause.getClassName(), cause.getMessage(), cause.getThrowable());
                         return true;
                     }
@@ -57,12 +61,27 @@ public abstract class AbstractExceptionHandler implements ExceptionHandler {
     }
 
     /**
+     * Should be implemented in subclasses if the exception class name is not enough to determine the ability to
+     * handle the exception.
+     *
+     * @param className exception class name
+     * @param message   exception message
+     * @param throwable exception instance. Can be null if the exception occured on the server side and this
+     *                  exception class isn't accessible by the client.
+     * @return true if the exception can be handled by this handler
+     */
+    protected boolean canHandle(String className, String message, @Nullable Throwable throwable) {
+        return true;
+    }
+
+    /**
      * Perform exception handling.
+     *
      * @param app       current {@link App} instance
      * @param className actual exception class name
      * @param message   exception message
      * @param throwable exception instance. Can be null if the exception occured on the server side and this
-     * exception class isn't accessible by the client.
+     *                  exception class isn't accessible by the client.
      */
     protected abstract void doHandle(App app, String className, String message, @Nullable Throwable throwable);
 }
--- modules/web6/src/com/haulmont/cuba/web/exception/NumericOverflowExceptionHandler.java
+++ modules/web6/src/com/haulmont/cuba/web/exception/NumericOverflowExceptionHandler.java
@@ -5,7 +5,9 @@
 
 package com.haulmont.cuba.web.exception;
 
+import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.MessageProvider;
+import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.cuba.web.App;
 import com.vaadin.ui.Window;
 import org.apache.commons.lang.StringUtils;
@@ -16,9 +18,8 @@ import javax.annotation.Nullable;
 /**
  * Handles database "numeric overflow" exception.
  *
- * <p>$Id$</p>
- *
  * @author degtyarjov
+ * @version $Id$
  */
 public class NumericOverflowExceptionHandler extends AbstractExceptionHandler {
 
@@ -26,9 +27,15 @@ public class NumericOverflowExceptionHandler extends AbstractExceptionHandler {
         super(ReportingSQLException.class.getName());
     }
 
+    @Override
+    protected boolean canHandle(String className, String message, @Nullable Throwable throwable) {
+        return StringUtils.containsIgnoreCase(message, "Numeric field overflow");
+    }
+
+    @Override
     protected void doHandle(App app, String className, String message, @Nullable Throwable throwable) {
         if (StringUtils.containsIgnoreCase(message, "Numeric field overflow")) {
-            String msg = MessageProvider.getMessage(getClass(), "numericFieldOverflow.message");
+            String msg = AppBeans.get(Messages.class).getMessage(getClass(), "numericFieldOverflow.message");
             app.getAppWindow().showNotification(msg, Window.Notification.TYPE_ERROR_MESSAGE);
         }
     }
