1b4d6e150cec154c74b8f08b98b4a676c1b58cb5
Yuriy Artamonov <artamonov@haulmont.com>
2013-12-18 13:39:38 +0000

    Inherit DesktopGroupTable from DesktopTable #PL-3315

--- modules/desktop/src/com/haulmont/cuba/desktop/gui/components/DesktopGroupTable.java
+++ modules/desktop/src/com/haulmont/cuba/desktop/gui/components/DesktopGroupTable.java
@@ -6,41 +6,14 @@
 package com.haulmont.cuba.desktop.gui.components;
 
 import com.haulmont.cuba.core.entity.Entity;
-import com.haulmont.cuba.desktop.gui.data.TableModelAdapter;
-import com.haulmont.cuba.desktop.sys.vcl.JXTableExt;
 import com.haulmont.cuba.gui.components.GroupTable;
-import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.data.GroupInfo;
-import org.jdesktop.swingx.JXTable;
-
-import java.awt.*;
 
 /**
  * @author krivopustov
  * @version $Id$
  */
-public class DesktopGroupTable extends DesktopAbstractTable<JXTable> implements GroupTable {
-
-    public DesktopGroupTable() {
-        impl = new JXTableExt() {
-
-            @Override
-            public void setFont(Font font) {
-                super.setFont(font);
-                applyFont(this, font);
-            }
-        };
-        initComponent();
-        impl.setColumnControlVisible(true);
-
-        tableSettings = new SwingXTableSettings(impl, columnsOrder);
-    }
-
-    @Override
-    protected void initTableModel(CollectionDatasource datasource) {
-        tableModel = new TableModelAdapter(datasource, columnsOrder, true);
-        impl.setModel(tableModel);
-    }
+public class DesktopGroupTable extends DesktopTable implements GroupTable {
 
     @Override
     public void groupBy(Object[] properties) {
@@ -78,17 +51,5 @@ public class DesktopGroupTable extends DesktopAbstractTable<JXTable> implements 
 
     @Override
     public void setFixedGrouping(boolean groupingByUserEnabled) {
-    }
-
-    @Override
-    public void setSortable(boolean sortable) {
-        super.setSortable(sortable);
-        impl.setSortable(sortable);
-    }
-
-    @Override
-    public void setEditable(boolean editable) {
-        super.setEditable(editable);
-        impl.setEditable(editable);
     }
 }
