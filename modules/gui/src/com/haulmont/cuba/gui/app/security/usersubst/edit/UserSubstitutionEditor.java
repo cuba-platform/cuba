728467f939f92d5190caaf88be3084dc12414d64
Yuriy Artamonov <artamonov@haulmont.com>
2013-12-26 13:34:17 +0000

    Don't allow to set till date beyond start date for substitution #PL-3371

---
+++ modules/gui/src/com/haulmont/cuba/gui/app/security/usersubst/edit/UserSubstitutionEditor.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008-2013 Haulmont. All rights reserved.
+ * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
+ */
+package com.haulmont.cuba.gui.app.security.usersubst.edit;
+
+import com.haulmont.cuba.core.global.PersistenceHelper;
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.DateField;
+import com.haulmont.cuba.gui.components.LookupField;
+import com.haulmont.cuba.gui.components.ValidationErrors;
+import com.haulmont.cuba.security.entity.UserSubstitution;
+
+import javax.inject.Inject;
+
+/**
+ * @author krivopustov
+ * @version $Id$
+ */
+public class UserSubstitutionEditor extends AbstractEditor<UserSubstitution> {
+
+    @Inject
+    protected DateField startDateField;
+
+    @Inject
+    protected LookupField substUser;
+
+    @Override
+    protected void postInit() {
+        super.postInit();
+
+        if (!PersistenceHelper.isNew(getItem())) {
+            substUser.setEditable(false);
+        }
+    }
+
+    @Override
+    protected void postValidate(ValidationErrors errors) {
+        super.postValidate(errors);
+
+        UserSubstitution substitution = getItem();
+        if (substitution.getStartDate() != null && substitution.getEndDate() != null) {
+            if (substitution.getStartDate().getTime() > substitution.getEndDate().getTime()) {
+                errors.add(startDateField, getMessage("dateOrderError"));
+            }
+        }
+    }
+}
