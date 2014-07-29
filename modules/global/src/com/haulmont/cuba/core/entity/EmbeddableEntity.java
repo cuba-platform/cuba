44bb41f2f9d0616666d3bd870555928631a278d5
Yuriy Artamonov <artamonov@haulmont.com>
2014-07-29 09:05:24 +0000

    Missing JavaDoc EmbeddableEntity

--- modules/global/src/com/haulmont/cuba/core/entity/EmbeddableEntity.java
+++ modules/global/src/com/haulmont/cuba/core/entity/EmbeddableEntity.java
@@ -6,7 +6,8 @@ package com.haulmont.cuba.core.entity;
 
 import com.haulmont.chile.core.model.MetaClass;
 import com.haulmont.chile.core.model.impl.AbstractInstance;
-import com.haulmont.cuba.core.global.MetadataProvider;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.global.UuidProvider;
 
 import javax.persistence.Id;
@@ -14,6 +15,10 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.Transient;
 import java.util.UUID;
 
+/**
+ * @author artamonov
+ * @version $Id$
+ */
 @MappedSuperclass
 public abstract class EmbeddableEntity extends AbstractInstance implements Entity<UUID> {
     private static final long serialVersionUID = 266201862280559076L;
@@ -33,7 +38,8 @@ public abstract class EmbeddableEntity extends AbstractInstance implements Entit
 
     @Override
     public MetaClass getMetaClass() {
-        return MetadataProvider.getSession().getClass(getClass());
+        Metadata metadata = AppBeans.get(Metadata.NAME);
+        return metadata.getSession().getClass(getClass());
     }
 
     @Override
