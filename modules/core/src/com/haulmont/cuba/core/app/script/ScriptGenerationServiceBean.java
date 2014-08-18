de79d0fd364dad2e4df028f566d1e02d6a45400c
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2014-08-18 12:26:22 +0000

    PL-1708 Generate INSERT SQL statements for selected entity in System Information window 1) Support Joined inheritance strategy 2) Partial support for embedded 3) Reload entities before script generation #PL-1708

--- modules/core/src/com/haulmont/cuba/core/app/script/ScriptGenerationServiceBean.java
+++ modules/core/src/com/haulmont/cuba/core/app/script/ScriptGenerationServiceBean.java
@@ -6,9 +6,22 @@
 package com.haulmont.cuba.core.app.script;
 
 import com.haulmont.bali.util.Preconditions;
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.chile.core.model.MetaProperty;
+import com.haulmont.chile.core.model.Range;
+import com.haulmont.cuba.core.Persistence;
+import com.haulmont.cuba.core.Transaction;
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.View;
+import com.haulmont.cuba.core.global.ViewRepository;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+import static com.haulmont.chile.core.model.MetaProperty.Type;
+import static com.haulmont.chile.core.model.MetaProperty.Type.*;
+import static com.haulmont.chile.core.model.Range.Cardinality.*;
 
 /**
  * @author degtyarjov
@@ -16,10 +29,18 @@ import org.springframework.stereotype.Service;
  */
 @Service(ScriptGenerationService.NAME)
 public class ScriptGenerationServiceBean implements ScriptGenerationService {
+    @Inject
+    protected Persistence persistence;
+
+    @Inject
+    protected ViewRepository viewRepository;
+
     @Override
     public String generateInsertScript(Entity entity) {
         Preconditions.checkNotNullArgument(entity);
         SqlScriptGenerator generator = AppBeans.getPrototype(SqlScriptGenerator.NAME, entity.getClass());
+        entity = reload(entity);
+
         return generator.generateInsertScript(entity);
     }
 
@@ -27,6 +48,46 @@ public class ScriptGenerationServiceBean implements ScriptGenerationService {
     public String generateUpdateScript(Entity entity) {
         Preconditions.checkNotNullArgument(entity);
         SqlScriptGenerator generator = AppBeans.getPrototype(SqlScriptGenerator.NAME, entity.getClass());
+        entity = reload(entity);
         return generator.generateUpdateScript(entity);
+    }
+
+    protected Entity reload(Entity entity) {
+        Transaction tx = persistence.createTransaction();
+        try {
+            entity = persistence.getEntityManager().find(entity.getClass(), entity.getId(), createFullView(entity));
+            tx.commit();
+        } finally {
+            tx.end();
+        }
+        return entity;
+    }
+
+    //todo use local fields only for embedded
+    protected View createFullView(Entity entity) {
+        MetaClass metaClass = entity.getMetaClass();
+        View view = new View(entity.getClass());
+        for (MetaProperty metaProperty : metaClass.getProperties()) {
+            if (isReferenceField(metaProperty)) {
+                view.addProperty(metaProperty.getName(), viewRepository.getView(metaProperty.getRange().asClass(), View.LOCAL));
+            } else if (isDataField(metaProperty)) {
+                view.addProperty(metaProperty.getName());
+            }
+        }
+
+        return view;
+    }
+
+    protected boolean isReferenceField(MetaProperty metaProperty) {
+        Type type = metaProperty.getType();
+        Range.Cardinality cardinality = metaProperty.getRange().getCardinality();
+        return (ASSOCIATION == type || COMPOSITION == type)
+                && (MANY_TO_ONE == cardinality || ONE_TO_ONE == cardinality);
+    }
+
+    protected boolean isDataField(MetaProperty metaProperty) {
+        Type type = metaProperty.getType();
+        return (DATATYPE == type || ENUM == type);
+
     }
 }
--- modules/core/src/com/haulmont/cuba/core/app/script/SqlScriptGenerator.java
+++ modules/core/src/com/haulmont/cuba/core/app/script/SqlScriptGenerator.java
@@ -7,25 +7,23 @@ package com.haulmont.cuba.core.app.script;
 
 import com.google.common.base.Preconditions;
 import com.haulmont.chile.core.datatypes.impl.EnumClass;
+import com.haulmont.chile.core.model.MetaClass;
 import com.haulmont.cuba.core.Persistence;
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.sys.persistence.PostgresUUID;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
+import javax.persistence.*;
 import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static java.lang.String.format;
 
@@ -37,14 +35,14 @@ import static java.lang.String.format;
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class SqlScriptGenerator {
     public static final String NAME = "cuba_SqlScriptGenerator";
+    public static final String ID = "id";
 
     protected SimpleDateFormat dateFormat = new SimpleDateFormat("''yyyy-MM-dd HH:mm:ss''");
     protected String insertTemplate = "insert into %s \n(%s) \nvalues (%s);";
-    protected String updateTemplate = "update %s \nset %s \nwhere id=%s;";
+    protected String updateTemplate = "update %s \nset %s \nwhere %s=%s;";
     protected Class clazz;
-    protected List<String> columnNames = new ArrayList<>();
-    protected List<String> fieldNames = new ArrayList<>();
-    protected String tableName;
+    protected List<Table> tables = new LinkedList<>();
+    protected String discriminatorValue;
 
     @Inject
     protected Metadata metadata;
@@ -57,11 +55,9 @@ public class SqlScriptGenerator {
     }
 
     @PostConstruct
-    public void init(){
-        this.tableName = metadata.getTools().getDatabaseTable(metadata.getClass(clazz));
-        Preconditions.checkNotNull(tableName,
-                format("Could not generate script for class [%s], because it's not linked with DB table using @Table annotation", clazz));
-        collectMetadata(clazz);
+    public void init() {
+        MetaClass metaClass = metadata.getClass(clazz);
+        collectTableMetadata(metaClass, new Table(null));
     }
 
     public String generateInsertScript(Entity entity) {
@@ -70,36 +66,26 @@ public class SqlScriptGenerator {
                         entity.getClass().getName(),
                         clazz.getClass()));
 
-        List<String> valuesStr = new ArrayList<>();
-        for (String fieldName : fieldNames) {
-            Object value = entity.getValue(fieldName);
-            valuesStr.add(convertValue(value));
+        StringBuilder result = new StringBuilder();
+        for (Table table : tables) {
+            result.append(table.insert(entity)).append("\n");
         }
 
-        String result = format(insertTemplate, tableName, convertList(columnNames), convertList(valuesStr));
-        return result;
+        return result.toString();
     }
 
-    //todo: do we only need loaded fields for detached?
     public String generateUpdateScript(Entity entity) {
         Preconditions.checkArgument(entity.getClass().equals(clazz),
                 format("Could not generate insert script for entity with class [%s]. This script generator is for class [%s]",
                         entity.getClass().getName(),
                         clazz.getClass()));
 
-        List<String> valuesStr = new ArrayList<>();
-        for (int i = 0; i < fieldNames.size(); i++) {
-            String fieldName = fieldNames.get(i);
-            String columnName = columnNames.get(i);
-
-            if (!fieldName.equalsIgnoreCase("id")) {
-                Object value = entity.getValue(fieldName);
-                valuesStr.add(format("%s=%s", columnName, convertValue(value)));
-            }
+        StringBuilder result = new StringBuilder();
+        for (Table table : tables) {
+            result.append(table.update(entity)).append("\n");
         }
 
-        String result = format(updateTemplate, tableName, convertList(valuesStr), convertValue(entity.getUuid()));
-        return result;
+        return result.toString();
     }
 
     protected String convertValue(Object value) {
@@ -119,7 +105,8 @@ public class SqlScriptGenerator {
                 valueStr = dateFormat.format((Date) value);
             } else if (value instanceof String
                     || value instanceof UUID
-                    || value instanceof PostgresUUID) {
+                    || value instanceof PostgresUUID
+                    || value instanceof Character) {
                 valueStr = format("'%s'", value);
             } else {
                 valueStr = value.toString();
@@ -135,24 +122,141 @@ public class SqlScriptGenerator {
         return string.substring(1, string.length() - 1);
     }
 
-    protected void collectMetadata(Class clazz) {
-        if (clazz == null) return;
+    protected void collectTableMetadata(MetaClass metaClass, Table table) {
+        Class<?> javaClass = metaClass.getJavaClass();
+        javax.persistence.Table annotation = javaClass.getAnnotation(javax.persistence.Table.class);
+        MetaClass ancestor = metaClass.getAncestor();
 
-        if (!clazz.getSuperclass().equals(Object.class)) {
-            collectMetadata(clazz.getSuperclass());
+        if (annotation != null && StringUtils.isNotEmpty(annotation.name())) {
+            if (table.name == null) {
+                table.name = annotation.name();
+                tables.add(0, table);
+            } else {
+                Table newTable = new Table(annotation.name());
+                tables.add(0, newTable);
+                table.parent = newTable;
+                table = newTable;
+            }
         }
 
-        for (Field field : clazz.getDeclaredFields()) {
-            Column columnAnnotation = field.getAnnotation(Column.class);
-            JoinColumn joinColumnAnnotation = field.getAnnotation(JoinColumn.class);
+        table.collectMetadata(javaClass);
+        if (ancestor != null) {
+            collectTableMetadata(ancestor, table);
+        }
+    }
 
-            if (columnAnnotation != null) {
-                columnNames.add(columnAnnotation.name());
-                fieldNames.add(field.getName());
-            } else if (joinColumnAnnotation != null) {
-                columnNames.add(joinColumnAnnotation.name());
-                fieldNames.add(field.getName());
+    protected class Table {
+        protected Table parent;
+        protected String name;
+        protected String discriminatorColumn;
+        protected DiscriminatorType discriminatorType;
+        protected Map<String, String> fieldToColumnMapping = new LinkedHashMap<>();
+
+        public Table(String name) {
+            this.name = name;
+        }
+
+        public String insert(Entity entity) {
+            List<String> columnNames = new ArrayList<>();
+            List<String> valuesStr = new ArrayList<>();
+
+            if (discriminatorColumn != null) {
+                String discriminatorValueStr = convertValue(discriminatorValue());
+                columnNames.add(discriminatorColumn);
+                valuesStr.add(discriminatorValueStr);
             }
+
+            for (Map.Entry<String, String> entry : fieldToColumnMapping.entrySet()) {
+                String fieldName = entry.getKey();
+                String columnName = entry.getValue();
+                Object value = entity.getValueEx(fieldName);
+                valuesStr.add(convertValue(value));
+                columnNames.add(columnName);
+            }
+
+            return format(insertTemplate, name, convertList(columnNames), convertList(valuesStr));
+        }
+
+        public String update(Entity entity) {
+            List<String> valuesStr = new ArrayList<>();
+            for (Map.Entry<String, String> entry : fieldToColumnMapping.entrySet()) {
+                String fieldName = entry.getKey();
+                String columnName = entry.getValue();
+                if (!fieldName.equalsIgnoreCase(ID)) {
+                    Object value = entity.getValue(fieldName);
+                    valuesStr.add(format("%s=%s", columnName, convertValue(value)));
+                }
+            }
+
+            return format(updateTemplate, name, convertList(valuesStr), fieldToColumnMapping.get(ID), convertValue(entity.getUuid()));
+        }
+
+
+        protected Object discriminatorValue() {
+            if (discriminatorValue == null) {
+                return null;
+            } else {
+                if (discriminatorColumn != null && discriminatorType != null) {
+                    switch (discriminatorType) {
+                        case CHAR:
+                            return discriminatorValue.charAt(0);
+                        case INTEGER:
+                            return Integer.valueOf(discriminatorValue);
+                        case STRING:
+                            return discriminatorValue;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        protected void collectMetadata(Class clazz) {
+            if (clazz == null) return;
+            PrimaryKeyJoinColumn primaryKey = (PrimaryKeyJoinColumn) clazz.getAnnotation(PrimaryKeyJoinColumn.class);
+            if (primaryKey != null) {
+                fieldToColumnMapping.put(ID, primaryKey.name());
+            }
+
+            if (discriminatorValue == null) {
+                DiscriminatorValue discriminatorValueAnnotation = (DiscriminatorValue) clazz.getAnnotation(DiscriminatorValue.class);
+                if (discriminatorValueAnnotation != null) {
+                    discriminatorValue = discriminatorValueAnnotation.value();
+                }
+            }
+
+            DiscriminatorColumn discriminatorColumn = (DiscriminatorColumn) clazz.getAnnotation(DiscriminatorColumn.class);
+            if (discriminatorColumn != null) {
+                this.discriminatorColumn = discriminatorColumn.name();
+                this.discriminatorType = discriminatorColumn.discriminatorType();
+            }
+
+            fieldToColumnMapping.putAll(collect(clazz));
+        }
+
+        private Map<String, String> collect(Class clazz) {
+            Map<String, String> result = new LinkedHashMap<>();
+            for (Field field : clazz.getDeclaredFields()) {
+
+                Embedded embedded = field.getAnnotation(Embedded.class);
+                Column columnAnnotation = field.getAnnotation(Column.class);
+                JoinColumn joinColumnAnnotation = field.getAnnotation(JoinColumn.class);
+
+                if (embedded != null) {
+                    Class<?> embeddedObjectType = field.getType();
+                    Map<String, String> embeddedFields = collect(embeddedObjectType);
+                    //todo attributes override
+                    for (Map.Entry<String, String> entry : embeddedFields.entrySet()) {
+                        result.put(field.getName() + "." + entry.getKey(), entry.getValue());
+                    }
+                } else if (columnAnnotation != null) {
+                    result.put(field.getName(), columnAnnotation.name());
+                } else if (joinColumnAnnotation != null) {
+                    result.put(field.getName(), joinColumnAnnotation.name());
+                }
+            }
+
+            return result;
         }
     }
 }
