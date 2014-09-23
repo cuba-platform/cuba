bc13022a51b4236c3164a075d70d7321fdaa7cfb
Yuriy Artamonov <artamonov@haulmont.com>
2014-09-23 07:42:59 +0000

    CubaLogWrapper does not check log level before getInfo #PL-4339

--- modules/global/src/com/haulmont/cuba/core/sys/logging/AbstractLogWrapper.java
+++ modules/global/src/com/haulmont/cuba/core/sys/logging/AbstractLogWrapper.java
@@ -9,23 +9,23 @@ import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.logging.Log;
 
 /**
- * <p>$Id$</p>
- *
  * @author krivopustov
+ * @version $id$
  */
 public abstract class AbstractLogWrapper implements Log {
 
     protected final Log delegate;
 
-    protected ThreadLocal<Boolean> inLogging = new ThreadLocal<Boolean>();
+    protected ThreadLocal<Boolean> inLogging = new ThreadLocal<>();
 
     public AbstractLogWrapper(Log delegate) {
         this.delegate = delegate;
     }
 
     protected String getInfo() {
-        if (BooleanUtils.isTrue(inLogging.get()))
+        if (BooleanUtils.isTrue(inLogging.get())) {
             return "";
+        }
 
         inLogging.set(true);
         try {
@@ -52,75 +52,117 @@ public abstract class AbstractLogWrapper implements Log {
 
     protected abstract String getUserInfo();
 
+    @Override
     public boolean isDebugEnabled() {
         return delegate.isDebugEnabled();
     }
 
+    @Override
     public boolean isErrorEnabled() {
         return delegate.isErrorEnabled();
     }
 
+    @Override
     public boolean isFatalEnabled() {
         return delegate.isFatalEnabled();
     }
 
+    @Override
     public boolean isInfoEnabled() {
         return delegate.isInfoEnabled();
     }
 
+    @Override
     public boolean isTraceEnabled() {
         return delegate.isTraceEnabled();
     }
 
+    @Override
     public boolean isWarnEnabled() {
         return delegate.isWarnEnabled();
     }
 
+    @Override
     public void trace(Object o) {
-        delegate.trace(getInfo() + o);
+        if (delegate.isTraceEnabled()) {
+            delegate.trace(getInfo() + o);
+        }
     }
 
+    @Override
     public void trace(Object o, Throwable throwable) {
-        delegate.trace(getInfo() + o, throwable);
+        if (delegate.isTraceEnabled()) {
+            delegate.trace(getInfo() + o, throwable);
+        }
     }
 
+    @Override
     public void debug(Object o) {
-        delegate.debug(getInfo() + o);
+        if (delegate.isDebugEnabled()) {
+            delegate.debug(getInfo() + o);
+        }
     }
 
+    @Override
     public void debug(Object o, Throwable throwable) {
-        delegate.debug(getInfo() + o, throwable);
+        if (delegate.isDebugEnabled()) {
+            delegate.debug(getInfo() + o, throwable);
+        }
     }
 
+    @Override
     public void info(Object o) {
-        delegate.info(getInfo() + o);
+        if (delegate.isInfoEnabled()) {
+            delegate.info(getInfo() + o);
+        }
     }
 
+    @Override
     public void info(Object o, Throwable throwable) {
-        delegate.info(getInfo() + o, throwable);
+        if (delegate.isInfoEnabled()) {
+            delegate.info(getInfo() + o, throwable);
+        }
     }
 
+    @Override
     public void warn(Object o) {
-        delegate.warn(getInfo() + o);
+        if (delegate.isWarnEnabled()) {
+            delegate.warn(getInfo() + o);
+        }
     }
 
+    @Override
     public void warn(Object o, Throwable throwable) {
-        delegate.warn(getInfo() + o, throwable);
+        if (delegate.isWarnEnabled()) {
+            delegate.warn(getInfo() + o, throwable);
+        }
     }
 
+    @Override
     public void error(Object o) {
-        delegate.error(getInfo() + o);
+        if (delegate.isErrorEnabled()) {
+            delegate.error(getInfo() + o);
+        }
     }
 
+    @Override
     public void error(Object o, Throwable throwable) {
-        delegate.error(getInfo() + o, throwable);
+        if (delegate.isErrorEnabled()) {
+            delegate.error(getInfo() + o, throwable);
+        }
     }
 
+    @Override
     public void fatal(Object o) {
-        delegate.fatal(getInfo() + o);
+        if (delegate.isFatalEnabled()) {
+            delegate.fatal(getInfo() + o);
+        }
     }
 
+    @Override
     public void fatal(Object o, Throwable throwable) {
-        delegate.fatal(getInfo() + o, throwable);
+        if (delegate.isFatalEnabled()) {
+            delegate.fatal(getInfo() + o, throwable);
+        }
     }
 }
