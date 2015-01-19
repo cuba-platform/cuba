692327afd0ce249be9fda3ba15c5dea5b8f9c1e4
Yuriy Artamonov <artamonov@haulmont.com>
2015-01-19 14:58:36 +0000

    ExportFormat is not configurable #PL-4536

--- modules/gui/src/com/haulmont/cuba/gui/export/ExportFormat.java
+++ modules/gui/src/com/haulmont/cuba/gui/export/ExportFormat.java
@@ -4,52 +4,57 @@
  */
 package com.haulmont.cuba.gui.export;
 
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Format of data exported by {@link ExportDisplay}.
  *
  * @author krivopustov
  * @version $Id$
  */
-public enum ExportFormat {
+public final class ExportFormat implements Serializable {
 
-    HTML("text/html", "html"),
-    HTM("text/html", "htm"),
+    private static final long serialVersionUID = -8448531804422711852L;
 
-    PDF("application/pdf", "pdf"),
-    XLS("application/vnd.ms-excel", "xls"),
-    XLSX("application/vnd.ms-excel", "xlsx"),
-    RTF("application/rtf", "rtf"),
-    DOC("application/doc", "doc"),
-    DOCX("application/docx", "docx"),
-    XML("text/xml", "xml"),
-    CSV("application/csv", "csv"),
+    public static final ExportFormat HTML = new ExportFormat("text/html", "html");
+    public static final ExportFormat HTM = new ExportFormat("text/html", "htm");
+    public static final ExportFormat PDF = new ExportFormat("application/pdf", "pdf");
+    public static final ExportFormat XLS = new ExportFormat("application/vnd.ms-excel", "xls");
+    public static final ExportFormat XLSX = new ExportFormat("application/vnd.ms-excel", "xlsx");
+    public static final ExportFormat RTF = new ExportFormat("application/rtf", "rtf");
+    public static final ExportFormat DOC = new ExportFormat("application/doc", "doc");
+    public static final ExportFormat DOCX = new ExportFormat("application/docx", "docx");
+    public static final ExportFormat XML = new ExportFormat("text/xml", "xml");
+    public static final ExportFormat CSV = new ExportFormat("application/csv", "csv");
+    public static final ExportFormat JPEG = new ExportFormat("image/jpeg", "jpeg");
+    public static final ExportFormat JPG = new ExportFormat("image/jpeg", "jpg");
+    public static final ExportFormat PNG = new ExportFormat("image/png", "png");
+    public static final ExportFormat RAR = new ExportFormat("application/x-rar-compressed", "rar");
+    public static final ExportFormat ZIP = new ExportFormat("application/zip", "zip");
+    public static final ExportFormat GZ = new ExportFormat(" application/x-gzip", "gz");
+    public static final ExportFormat OCTET_STREAM = new ExportFormat("application/octet-stream", "");
 
-    JPEG("image/jpeg", "jpeg"),
-    JPG("image/jpeg", "jpg"),
-    PNG("image/png", "png"),
+    public static final List<ExportFormat> DEFAULT_FORMATS = Collections.unmodifiableList(
+            Arrays.asList(HTML, HTM, PDF, XLS, XLSX, RTF, DOC, DOCX, XML, CSV, JPEG, JPG, PNG, RAR, GZ, ZIP, OCTET_STREAM));
 
-    RAR("application/x-rar-compressed", "rar"),
-    ZIP("application/zip", "zip"),
-    OCTET_STREAM("application/octet-stream", "");
+    protected final String contentType;
+    protected final String fileExt;
 
-    private String contentType;
-    private String fileExt;
-
-    private ExportFormat(String contentType, String fileExt) {
+    public ExportFormat(String contentType, String fileExt) {
         this.contentType = contentType;
         this.fileExt = fileExt;
     }
 
     public static ExportFormat getByExtension(String extension) {
-        ExportFormat format = null;
-        ExportFormat[] formats = values();
-        int i = 0;
-        while ((i < formats.length) && (format == null)) {
-            if (formats[i].fileExt.equalsIgnoreCase(extension))
-                format = formats[i];
-            i++;
+        List<ExportFormat> formats = DEFAULT_FORMATS;
+        for (ExportFormat f : formats) {
+            if (f.getFileExt().equals(extension))
+                return f;
         }
-        return format;
+        return OCTET_STREAM;
     }
 
     public String getContentType() {
