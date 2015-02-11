43681a57ea8b1651ca85796f8c076854669bdd4a
Alena Kovalenko <kovalenko@haulmont.com>
2015-02-11 10:35:59 +0000

    Substitution mechanism support for screen history #PL-4870

--- modules/core/db/update/oracle/15/150205-addSubstituteUserInScreenHistory.sql
+++ modules/core/db/update/oracle/15/150205-addSubstituteUserInScreenHistory.sql
@@ -2,4 +2,4 @@
 -- Description:
 
 alter table sec_screen_history add substituted_user_id varchar2(32)^
-create index IDX_SEC_SCREEN_HISTORY_SUBSTITUTED_USER on SEC_SCREEN_HISTORY(SUBSTITUTED_USER_ID)^
+create index IDX_SEC_SCREEN_HIST_SUB_USER on SEC_SCREEN_HISTORY(SUBSTITUTED_USER_ID)^
--- modules/web/src/com/haulmont/cuba/web/app/ui/security/history/EntityOpenHistoryFrame.java
+++ modules/web/src/com/haulmont/cuba/web/app/ui/security/history/EntityOpenHistoryFrame.java
@@ -10,6 +10,7 @@ import com.haulmont.cuba.gui.components.AbstractFrame;
 import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.gui.components.Label;
 import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 import com.haulmont.cuba.security.entity.ScreenHistoryEntity;
 import com.haulmont.cuba.security.entity.User;
 
@@ -25,18 +26,22 @@ public class EntityOpenHistoryFrame extends AbstractFrame {
     @Inject
     protected Table historyTable;
 
+    @Inject
+    protected ComponentsFactory componentFactory;
+
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
         historyTable.addGeneratedColumn(getMessage("entityOpenHistoryFrame.user"), new Table.ColumnGenerator<ScreenHistoryEntity>() {
             @Override
             public Component generateCell(final ScreenHistoryEntity entity) {
-                Label user = AppConfig.getFactory().createComponent(Label.NAME);
+                Label user = componentFactory.createComponent(Label.NAME);
                 User substituteUser = entity.getSubstitutedUser();
                 if (substituteUser == null) {
                     user.setValue(entity.getUser().getCaption());
                 } else {
-                    user.setValue(String.format(getMessage("userMessage"),entity.getUser().getCaption(),substituteUser.getCaption()));
+                    user.setValue(String.format(getMessage("userMessage"), entity.getUser().getCaption(),
+                            substituteUser.getCaption()));
                 }
                 return user;
             }
