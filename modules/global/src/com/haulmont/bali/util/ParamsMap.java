b5615fed3c54f6d2bb9f91d3e3d18629040520c1
Yuriy Artamonov <artamonov@haulmont.com>
2015-02-20 11:54:35 +0000

    ParamsMap does not allow nulls in values #PL-4950

--- modules/global/src/com/haulmont/bali/util/ParamsMap.java
+++ modules/global/src/com/haulmont/bali/util/ParamsMap.java
@@ -10,7 +10,8 @@ import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 
 /**
- * Utility class for instantiation immutable Map&lt;String, Object&gt;
+ * Utility class for instantiation immutable Map&lt;String, Object&gt;. <br/>
+ * Null values will be ignored. Null keys are not permitted.
  *
  * @author artamonov
  * @version $Id$
@@ -20,27 +21,46 @@ public final class ParamsMap {
     private ParamsMap() {
     }
 
-    public static Map<String, Object> of(String paramName, Object value) {
-        return ImmutableMap.of(paramName, value);
+    private static void put(ImmutableMap.Builder<String, Object> builder, String key, Object value) {
+        if (value != null) {
+            builder.put(key, value);
+        }
+    }
+
+    public static Map<String, Object> of(String paramName, Object paramValue) {
+        ImmutableMap.Builder<String, Object> b = new ImmutableMap.Builder<>();
+        put(b, paramName, paramValue);
+        return b.build();
     }
 
     public static Map<String, Object> of(String paramName1, Object paramValue1,
                                          String paramName2, Object paramValue2) {
-        return ImmutableMap.of(paramName1, paramValue1, paramName2, paramValue2);
+        ImmutableMap.Builder<String, Object> b = new ImmutableMap.Builder<>();
+        put(b, paramName1, paramValue1);
+        put(b, paramName2, paramValue2);
+        return b.build();
     }
 
     public static Map<String, Object> of(String paramName1, Object paramValue1,
                                          String paramName2, Object paramValue2,
                                          String paramName3, Object paramValue3) {
-        return ImmutableMap.of(paramName1, paramValue1, paramName2, paramValue2, paramName3, paramValue3);
+        ImmutableMap.Builder<String, Object> b = new ImmutableMap.Builder<>();
+        put(b, paramName1, paramValue1);
+        put(b, paramName2, paramValue2);
+        put(b, paramName3, paramValue3);
+        return b.build();
     }
 
     public static Map<String, Object> of(String paramName1, Object paramValue1,
                                          String paramName2, Object paramValue2,
                                          String paramName3, Object paramValue3,
                                          String paramName4, Object paramValue4) {
-        return ImmutableMap.of(paramName1, paramValue1, paramName2,paramValue2,
-                paramName3, paramValue3, paramName4, paramValue4);
+        ImmutableMap.Builder<String, Object> b = new ImmutableMap.Builder<>();
+        put(b, paramName1, paramValue1);
+        put(b, paramName2, paramValue2);
+        put(b, paramName3, paramValue3);
+        put(b, paramName4, paramValue4);
+        return b.build();
     }
 
     public static Map<String, Object> of(String paramName1, Object paramValue1,
@@ -48,7 +68,28 @@ public final class ParamsMap {
                                          String paramName3, Object paramValue3,
                                          String paramName4, Object paramValue4,
                                          String paramName5, Object paramValue5) {
-        return ImmutableMap.of(paramName1, paramValue1, paramName2, paramValue2,
-                paramName3, paramValue3, paramName4, paramValue4, paramName5, paramValue5);
+        ImmutableMap.Builder<String, Object> b = new ImmutableMap.Builder<>();
+        put(b, paramName1, paramValue1);
+        put(b, paramName2, paramValue2);
+        put(b, paramName3, paramValue3);
+        put(b, paramName4, paramValue4);
+        put(b, paramName5, paramValue5);
+        return b.build();
+    }
+
+    public static Map<String, Object> of(String paramName1, Object paramValue1,
+                                         String paramName2, Object paramValue2,
+                                         String paramName3, Object paramValue3,
+                                         String paramName4, Object paramValue4,
+                                         String paramName5, Object paramValue5,
+                                         String paramName6, Object paramValue6) {
+        ImmutableMap.Builder<String, Object> b = new ImmutableMap.Builder<>();
+        put(b, paramName1, paramValue1);
+        put(b, paramName2, paramValue2);
+        put(b, paramName3, paramValue3);
+        put(b, paramName4, paramValue4);
+        put(b, paramName5, paramValue5);
+        put(b, paramName6, paramValue6);
+        return b.build();
     }
 }
