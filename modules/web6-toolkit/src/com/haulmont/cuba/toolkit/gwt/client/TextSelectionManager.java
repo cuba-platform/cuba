b28a319537a9cbb4720c2281c9cb8dba1932da22
Yuriy Artamonov <artamonov@haulmont.com>
2015-04-08 07:15:59 +0000

    Remove Vaadin 6 Support #PL-5203

--- modules/web6-toolkit/src/com/haulmont/cuba/toolkit/gwt/client/TextSelectionManager.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2008-2013 Haulmont. All rights reserved.
- * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
- */
-package com.haulmont.cuba.toolkit.gwt.client;
-
-public interface TextSelectionManager {
-    boolean allowTextSelection();
-}
