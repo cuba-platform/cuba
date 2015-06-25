5d36fcda8c846c03315c89af42acef337b5f0323
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2015-06-25 06:59:35 +0000

    PL-5393 Category attribute code should be unique for the certain entity type Fix scripts #PL-5393

--- modules/core/db/update/oracle/15/150514-categoryEntityTypeInAttribute.sql
+++ modules/core/db/update/oracle/15/150514-categoryEntityTypeInAttribute.sql
@@ -1,6 +1,6 @@
 -- $Id$
 alter table SYS_CATEGORY_ATTR add CATEGORY_ENTITY_TYPE varchar2(4000)^
-update SYS_CATEGORY_ATTR set CODE = NAME where CODE is null^
+update SYS_CATEGORY_ATTR set CODE = substr(NAME || CAST (CATEGORY_ID as varchar2(32)), 1, 100) where CODE is null^
 update SYS_CATEGORY_ATTR attr set CATEGORY_ENTITY_TYPE = (select cat.ENTITY_TYPE from SYS_CATEGORY cat where cat.ID = attr.CATEGORY_ID)^
 alter table SYS_CATEGORY_ATTR MODIFY  CODE varchar2(100) not null^
 create unique index IDX_CAT_ATTR_ENT_TYPE_AND_CODE on SYS_CATEGORY_ATTR (CATEGORY_ENTITY_TYPE, CODE, DELETE_TS)^
