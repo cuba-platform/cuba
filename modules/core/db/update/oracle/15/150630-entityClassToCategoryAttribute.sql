7b2386a86f41923169c1bea14a63a771a0515cc9
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2015-07-08 11:55:32 +0000

    Merged from release_5_6  [from revision 22173] PL-5497 Re-write CategoryAttribute editor, Runtime properties frame and all linked. Get rid of SetValueEntity fix script #PL-5497

--- modules/core/db/update/oracle/15/150630-entityClassToCategoryAttribute.sql
+++ modules/core/db/update/oracle/15/150630-entityClassToCategoryAttribute.sql
@@ -1,5 +1,5 @@
 -- $Id$
-alter table SYS_CATEGORY_ATTR add column ENTITY_CLASS varchar2(500)^
+alter table SYS_CATEGORY_ATTR add ENTITY_CLASS varchar2(500)^
 update SYS_CATEGORY_ATTR set ENTITY_CLASS = DATA_TYPE where IS_ENTITY = 1^
 update SYS_CATEGORY_ATTR set DATA_TYPE = 'ENTITY' where IS_ENTITY = 1^
 --alter table SYS_CATEGORY_ATTR drop column IS_ENTITY^
