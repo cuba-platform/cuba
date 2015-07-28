5e8cb7c9584a2f6d63a6c58b2600616bf92f9631
Gleb Gorelov <gorelov@haulmont.com>
2015-07-28 10:56:29 +0000

    Add marker for presentation which is used as default in presentations list #PL-4965

--- modules/gui/src/com/haulmont/cuba/gui/presentations/PresentationsChangeListener.java
+++ modules/gui/src/com/haulmont/cuba/gui/presentations/PresentationsChangeListener.java
@@ -3,7 +3,10 @@
  * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
  */
 package com.haulmont.cuba.gui.presentations;
-
+/**
+ * @author gorodnov
+ * @version $Id$
+ */
 public interface PresentationsChangeListener {
     
     void currentPresentationChanged(Presentations presentations, Object oldPresentationId);
