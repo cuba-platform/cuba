761f86cf374814334dd7a649f5b3c544d29c8d67
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-09-06 18:04:07 +0000

    EclipseLink ORM (improved logging). #PL-5412

--- modules/global/src/com/haulmont/cuba/core/sys/logging/EclipseLinkLog.java
+++ modules/global/src/com/haulmont/cuba/core/sys/logging/EclipseLinkLog.java
@@ -5,12 +5,15 @@
 
 package com.haulmont.cuba.core.sys.logging;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import org.eclipse.persistence.logging.AbstractSessionLog;
+import org.eclipse.persistence.logging.SessionLog;
 import org.eclipse.persistence.logging.SessionLogEntry;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,11 +25,25 @@ public class EclipseLinkLog extends AbstractSessionLog {
 
     protected Map<String, Logger> logsCache = new ConcurrentHashMap<>();
 
+    protected static Map<Integer, Level> levels = new HashMap<>();
+
+    static {
+        levels.put(SessionLog.OFF, Level.OFF);
+        levels.put(SessionLog.SEVERE, Level.ERROR);
+        levels.put(SessionLog.WARNING, Level.WARN);
+        levels.put(SessionLog.INFO, Level.INFO);
+        levels.put(SessionLog.CONFIG, Level.INFO);
+        levels.put(SessionLog.FINE, Level.DEBUG);
+        levels.put(SessionLog.FINER, Level.DEBUG);
+        levels.put(SessionLog.FINEST, Level.TRACE);
+        levels.put(SessionLog.ALL, Level.ALL);
+    }
+
     private Logger getLog(String category) {
         String logName = "eclipselink." + category;
         Logger log = logsCache.get(logName);
         if (log == null) {
-            log = LoggerFactory.getLogger(logName);
+            log = (Logger) LoggerFactory.getLogger(logName);
             logsCache.put(logName, log);
         }
         return log;
@@ -34,17 +51,39 @@ public class EclipseLinkLog extends AbstractSessionLog {
 
     @Override
     public void log(SessionLogEntry sessionLogEntry) {
+        Level logbackLevel = levels.get(sessionLogEntry.getLevel());
+        if (logbackLevel == null)
+            return;
         Logger log = getLog(sessionLogEntry.getNameSpace());
-        if (log.isDebugEnabled()) {
-            log.debug(sessionLogEntry.getMessage() +
-                    (sessionLogEntry.getParameters() == null ? "" :
-                            " " + Arrays.toString(sessionLogEntry.getParameters())));
+        if (log.isEnabledFor(logbackLevel)) {
+            String msg = sessionLogEntry.getMessage() +
+                    (sessionLogEntry.getParameters() == null ? "" : " " + Arrays.toString(sessionLogEntry.getParameters()));
+            switch (logbackLevel.toString()) {
+                case "ERROR":
+                    log.error(msg);
+                    break;
+                case "WARN":
+                    log.warn(msg);
+                    break;
+                case "INFO":
+                    log.info(msg);
+                    break;
+                case "DEBUG":
+                    log.debug(msg);
+                    break;
+                case "TRACE":
+                    log.trace(msg);
+                    break;
+            }
         }
     }
 
     @Override
     public boolean shouldLog(int level, String category) {
+        Level logbackLevel = levels.get(level);
+        if (logbackLevel == null)
+            return false;
         Logger log = getLog(category);
-        return log.isDebugEnabled();
+        return log.isEnabledFor(logbackLevel);
     }
 }
