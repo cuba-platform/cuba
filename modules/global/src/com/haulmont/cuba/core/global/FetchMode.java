c56c2818d2bcafc119dbd420d6c1d7ab68e9d7cf
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-10-16 12:03:51 +0000

    EclipseLink ORM (fetch mode JavaDocs). #PL-5412

--- modules/global/src/com/haulmont/cuba/core/global/FetchMode.java
+++ modules/global/src/com/haulmont/cuba/core/global/FetchMode.java
@@ -6,12 +6,29 @@
 package com.haulmont.cuba.core.global;
 
 /**
+ * Specifies how to fetch a referenced entity from the database.
+ *
  * @author Konstantin Krivopustov
  * @version $Id$
  */
 public enum FetchMode {
+    /**
+     * The platform will choose an optimal mode
+     */
     AUTO,
+
+    /**
+     * Fetching will be performed according to JPA rules, which effectively means loading by a separate select
+     */
     UNDEFINED,
+
+    /**
+     * Fetching in the same select by joining with referenced table
+     */
     JOIN,
+
+    /**
+     * Fetching by one separate select for all referenced entities
+     */
     BATCH
 }
