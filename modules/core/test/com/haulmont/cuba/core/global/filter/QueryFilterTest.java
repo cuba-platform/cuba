66d9a954d599cea0c0a1f0b5ac56eb8231019d5e
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2015-11-20 12:15:21 +0000

    PL-6010 Security constraints improvements fix test #PL-6010

--- modules/core/test/com/haulmont/cuba/core/global/filter/QueryFilterTest.java
+++ modules/core/test/com/haulmont/cuba/core/global/filter/QueryFilterTest.java
@@ -24,9 +24,6 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 public class QueryFilterTest {
-    @ClassRule
-    public static TestContainer cont = TestContainer.Common.INSTANCE;
-
     @Mocked
     GlobalConfig globalConfig;
 
