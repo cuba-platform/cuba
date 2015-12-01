519fe2b888eaa8b6a0436247bedd219bb9e8c046
Maxim Gorbunkov <gorbunkov@haulmont.com>
2015-12-01 12:06:20 +0000

    FTS filter creates new condition on every search #PL-6386

--- modules/gui/src/com/haulmont/cuba/gui/components/filter/FilterDelegateImpl.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/filter/FilterDelegateImpl.java
@@ -1379,6 +1379,7 @@ public class FilterDelegateImpl implements FilterDelegate {
             params.put("queryKey", queryKey);
 
             CustomCondition ftsCondition = ftsFilterHelper.createFtsCondition(queryKey);
+            conditions = new ConditionsTree();
             conditions.getRootNodes().add(new Node<>(ftsCondition));
 
             if ((applyTo != null) && (Table.class.isAssignableFrom(applyTo.getClass()))) {
