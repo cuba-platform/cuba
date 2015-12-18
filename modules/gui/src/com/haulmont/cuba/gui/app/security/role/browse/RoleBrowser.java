a798d4be1610677ce09094ccfa001715f1843659
Maxim Gorbunkov <gorbunkov@haulmont.com>
2015-12-18 07:00:21 +0000

    Roles import/export #PL-6487

--- modules/gui/src/com/haulmont/cuba/gui/app/security/role/browse/RoleBrowser.java
+++ modules/gui/src/com/haulmont/cuba/gui/app/security/role/browse/RoleBrowser.java
@@ -4,22 +4,29 @@
  */
 package com.haulmont.cuba.gui.app.security.role.browse;
 
+import com.haulmont.cuba.core.app.importexport.EntityImportExportService;
+import com.haulmont.cuba.core.app.importexport.EntityImportView;
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.*;
 import com.haulmont.cuba.gui.WindowManager;
 import com.haulmont.cuba.gui.WindowParams;
-import com.haulmont.cuba.gui.components.AbstractLookup;
-import com.haulmont.cuba.gui.components.Action;
-import com.haulmont.cuba.gui.components.Component;
-import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.components.actions.ItemTrackingAction;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
-import com.haulmont.cuba.security.entity.EntityOp;
-import com.haulmont.cuba.security.entity.Role;
-import com.haulmont.cuba.security.entity.User;
-import com.haulmont.cuba.security.entity.UserRole;
+import com.haulmont.cuba.gui.export.ByteArrayDataProvider;
+import com.haulmont.cuba.gui.export.ExportDisplay;
+import com.haulmont.cuba.gui.export.ExportFormat;
+import com.haulmont.cuba.gui.upload.FileUploadingAPI;
+import com.haulmont.cuba.security.entity.*;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -28,10 +35,12 @@ import java.util.*;
  */
 public class RoleBrowser extends AbstractLookup {
 
-    private static final String DEFAULT_ROLE_PROPERTY = "defaultRole";
+    protected static final String DEFAULT_ROLE_PROPERTY = "defaultRole";
+
+    protected static final Logger log = LoggerFactory.getLogger(RoleBrowser.class);
 
     @Inject
-    protected Table rolesTable;
+    protected Table<Role> rolesTable;
 
     @Inject
     protected Security security;
@@ -44,6 +53,21 @@ public class RoleBrowser extends AbstractLookup {
 
     @Inject
     protected CollectionDatasource<Role, UUID> rolesDs;
+
+    @Inject
+    protected ExportDisplay exportDisplay;
+
+    @Inject
+    protected EntityImportExportService entityImportExportService;
+
+    @Inject
+    protected ViewRepository viewRepository;
+
+    @Inject
+    protected FileUploadField importRolesUpload;
+
+    @Inject
+    protected FileUploadingAPI fileUploadingAPI;
 
     @Override
     public void init(Map<String, Object> params) {
@@ -123,5 +147,67 @@ public class RoleBrowser extends AbstractLookup {
                 rolesDs.commit();
             }
         });
+
+
+        rolesTable.addAction(new ExportAction());
+
+        importRolesUpload.addFileUploadSucceedListener(event -> {
+            File file = fileUploadingAPI.getFile(importRolesUpload.getFileId());
+            if (file == null) {
+                String errorMsg = String.format("Entities import upload error. File with id %s not found", importRolesUpload.getFileId());
+                throw new RuntimeException(errorMsg);
+            }
+            byte[] zipBytes;
+            try (InputStream is = new FileInputStream(file)) {
+                zipBytes = IOUtils.toByteArray(is);
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to import file", e);
+            }
+
+            try {
+                fileUploadingAPI.deleteFile(importRolesUpload.getFileId());
+            } catch (FileStorageException e) {
+                log.error("Unable to delete temp file", e);
+            }
+
+            Collection<Entity> importedEntities = entityImportExportService.importEntities(zipBytes, createRolesImportView());
+            showNotification(importedEntities.size() + " entities imported", NotificationType.HUMANIZED);
+            rolesDs.refresh();
+        });
     }
+
+    protected EntityImportView createRolesImportView() {
+        return new EntityImportView(Role.class)
+                .addLocalProperties()
+                .addProperty("permissions", new EntityImportView(Permission.class).addLocalProperties());
+    }
+
+    protected class ExportAction extends ItemTrackingAction {
+        public ExportAction() {
+            super("export");
+        }
+
+        @Override
+        public void actionPerform(Component component) {
+            Set<Role> selected = rolesTable.getSelected();
+            View view = viewRepository.getView(Role.class, "role.export");
+            if (view == null) {
+                throw new DevelopmentException("View 'role.export' for sec$Role was not found");
+            }
+            if (!selected.isEmpty()) {
+                try {
+                    exportDisplay.show(new ByteArrayDataProvider(entityImportExportService.exportEntities(selected, view)), "Roles", ExportFormat.ZIP);
+                } catch (Exception e) {
+                    showNotification(getMessage("exportFailed"), e.getMessage(), NotificationType.ERROR);
+                    log.error("Roles export failed", e);
+                }
+            }
+        }
+
+        @Override
+        public String getCaption() {
+            return getMessage("export");
+        }
+    }
+
 }
