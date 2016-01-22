d094d5500060c09f4fdd3bf485a8b46b892d84e8
Gleb Gorelov <gorelov@haulmont.com>
2016-01-22 11:03:13 +0400

    PL-6613 JavaDoc for FileUploadField.accept

--- modules/gui/src/com/haulmont/cuba/gui/components/FileUploadField.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/FileUploadField.java
@@ -98,6 +98,15 @@ public interface FileUploadField extends UploadComponentSupport {
     void addFileUploadSucceedListener(FileUploadSucceedListener listener);
     void removeFileUploadSucceedListener(FileUploadSucceedListener listener);
 
+    /**
+     * Returns comma separated types of files.
+     * @return comma separated types of files
+     */
     String getAccept();
+
+    /**
+     * Sets the types of files that the server accepts (that can be submitted through a file upload).
+     * @param accept comma separated types of files; possible options to specify the file types are {@code *.png, .png}
+     */
     void setAccept(String accept);
 }
