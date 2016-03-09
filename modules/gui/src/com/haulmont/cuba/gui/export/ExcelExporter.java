a0f9a6710e3dc6a3f4f37e4e2b0c8b6aa3286da2
Daniil Tsarev <tsarev@haulmont.com>
2016-03-09 17:22:02 +0400

    PL-6752 Check if column xml descriptor is null

--- modules/gui/src/com/haulmont/cuba/gui/export/ExcelExporter.java
+++ modules/gui/src/com/haulmont/cuba/gui/export/ExcelExporter.java
@@ -25,6 +25,7 @@ import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.dom4j.Element;
 
 import javax.annotation.Nullable;
 import javax.persistence.TemporalType;
@@ -310,8 +311,9 @@ public class ExcelExporter {
                 if (printable != null) {
                     cellValue = printable.getValue((Entity) instance);
                 } else {
-                    String captionProperty = column.getXmlDescriptor().attributeValue("captionProperty");
-                    if (StringUtils.isNotBlank(captionProperty)) {
+                    Element xmlDescriptor = column.getXmlDescriptor();
+                    if (xmlDescriptor != null && StringUtils.isNotEmpty(xmlDescriptor.attributeValue("captionProperty"))) {
+                        String captionProperty = xmlDescriptor.attributeValue("captionProperty");
                         cellValue = InstanceUtils.getValueEx(instance, captionProperty);
                     } else {
                         cellValue = InstanceUtils.getValueEx(instance, ((MetaPropertyPath) column.getId()).getPath());
