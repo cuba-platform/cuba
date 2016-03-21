37fb807f94db70015c29c1f5d38f3e97a2ab0e6c
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-03-21 14:10:29 +0400

    PL-6908 Refactor FtsConfig

--- modules/global/src/com/haulmont/cuba/core/global/FtsConfig.java
+++ modules/global/src/com/haulmont/cuba/core/global/FtsConfig.java
@@ -21,11 +21,6 @@ import com.haulmont.cuba.core.config.Property;
 import com.haulmont.cuba.core.config.Source;
 import com.haulmont.cuba.core.config.SourceType;
 import com.haulmont.cuba.core.config.defaults.DefaultBoolean;
-import com.haulmont.cuba.core.config.defaults.DefaultInt;
-import com.haulmont.cuba.core.config.type.Factory;
-import com.haulmont.cuba.core.config.type.StringListTypeFactory;
-
-import java.util.List;
 
 @Source(type = SourceType.DATABASE)
 public interface FtsConfig extends Config {
@@ -34,31 +29,4 @@ public interface FtsConfig extends Config {
     @DefaultBoolean(false)
     boolean getEnabled();
     void setEnabled(boolean enable);
-
-    @Property("fts.indexingBatchSize")
-    @DefaultInt(300)
-    int getIndexingBatchSize();
-
-    @Property("fts.indexDir")
-    String getIndexDir();
-
-    @Property("fts.maxSearchResults")
-    @DefaultInt(100)
-    int getMaxSearchResults();
-
-    @Property("fts.searchResultsBatchSize")
-    @DefaultInt(5)
-    int getSearchResultsBatchSize();
-
-    @Property("fts.storeContentInIndex")
-    @DefaultBoolean(true)
-    boolean getStoreContentInIndex();
-
-    @Property("fts.indexingHosts")
-    @Factory(factory = StringListTypeFactory.class)
-    List<String> getIndexingHosts();
-
-    @Property("fts.reindexBatchSize")
-    @DefaultInt(5000)
-    int getReindexBatchSize();
 }
