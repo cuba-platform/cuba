940fb2a61d1554055d3767e20d855b57812112ec
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-04-06 14:28:47 +0400

    PL-7003 UUIDs query params aren't converted to valid strings without dashes in JPQL queries for MySQL

--- modules/core/src/com/haulmont/cuba/core/sys/persistence/CubaMySQLPlatform.java
+++ modules/core/src/com/haulmont/cuba/core/sys/persistence/CubaMySQLPlatform.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 
 /**
  */
-public class CubaMySQLPlatform1 extends MySQLPlatform {
+public class CubaMySQLPlatform extends MySQLPlatform {
 
     @Override
     public Object convertObject(Object sourceObject, Class javaClass) throws ConversionException {
