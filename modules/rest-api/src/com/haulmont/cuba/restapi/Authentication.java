c6e22802f6ca2e12b7c7824f1b758da9a53dd348
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-05-05 15:28:35 +0400

    PL-6909 Add logging of authentication errors to com.haulmont.cuba.restapi.Authentication

--- modules/rest-api/src/com/haulmont/cuba/restapi/Authentication.java
+++ modules/rest-api/src/com/haulmont/cuba/restapi/Authentication.java
@@ -22,12 +22,28 @@ import com.haulmont.cuba.core.sys.AppContext;
 import com.haulmont.cuba.core.sys.SecurityContext;
 import com.haulmont.cuba.security.app.LoginService;
 import com.haulmont.cuba.security.global.UserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import java.util.UUID;
 
 /**
+ * Provides authentication for controllers code.
+ * <p>
+ * Example usage:
+ * <pre>
+ *     if (!authentication.begin(sessionId)) {
+ *        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+ *        return;
+ *     }
+ *     try {
+ *         // valid current thread's user session presents here
+ *     } finally {
+ *         authentication.end();
+ *     }
+ * </pre>
  */
 @Component(Authentication.NAME)
 public class Authentication {
@@ -36,28 +52,43 @@ public class Authentication {
 
     public static final String PERMISSION_NAME = "cuba.restApi.enabled";
 
+    private Logger log = LoggerFactory.getLogger(Authentication.class);
+
     @Inject
     protected LoginService loginService;
 
+    /**
+     * Begin authenticated block of code.
+     * @param sessionId {@link UserSession} id
+     * @return  true if the given session id is valid and authentication is successful
+     */
     public boolean begin(String sessionId) {
         UUID uuid;
         try {
             uuid = UuidProvider.fromString(sessionId);
         } catch (Exception e) {
+            log.warn("Invalid user session ID: " + e.toString());
             return false;
         }
 
         UserSession session = loginService.getSession(uuid);
-        if (session == null)
+        if (session == null) {
+            log.warn("User session " + uuid + " does not exist");
             return false;
+        }
 
-        if (!session.isSpecificPermitted(PERMISSION_NAME))
+        if (!session.isSpecificPermitted(PERMISSION_NAME)) {
+            log.warn(PERMISSION_NAME + " is not permitted for user " + session.getUser().getLogin());
             return false;
+        }
 
         AppContext.setSecurityContext(new SecurityContext(session));
         return true;
     }
 
+    /**
+     * End authenticated block of code, i.e. remove user session from the execution thread.
+     */
     public void end() {
         AppContext.setSecurityContext(null);
     }
