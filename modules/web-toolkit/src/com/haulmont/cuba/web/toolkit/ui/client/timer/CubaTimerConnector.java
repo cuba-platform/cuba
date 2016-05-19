a7dbaa855ee31880ec9f3018113fa66870643751
Yuriy Artamonov <artamonov@haulmont.com>
2016-05-19 16:08:20 +0400

    PL-7234 BackgroundTasks timer does not send events to server in BackgroundWorkProgressWindow

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/timer/CubaTimerConnector.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/timer/CubaTimerConnector.java
@@ -20,6 +20,7 @@ import com.google.gwt.user.client.Timer;
 import com.haulmont.cuba.web.toolkit.ui.CubaTimer;
 import com.vaadin.client.ServerConnector;
 import com.vaadin.client.communication.RpcProxy;
+import com.vaadin.client.communication.ServerRpcQueue;
 import com.vaadin.client.communication.StateChangeEvent;
 import com.vaadin.client.extensions.AbstractExtensionConnector;
 import com.vaadin.shared.ui.Connect;
@@ -66,7 +67,9 @@ public class CubaTimerConnector extends AbstractExtensionConnector {
     public void onTimer() {
         if (running) {
             if (getState().listeners) {
-                if (getConnection().getServerRpcQueue().isEmpty()) {
+                ServerRpcQueue rpcQueue = getConnection().getServerRpcQueue();
+
+                if (rpcQueue.isEmpty() || !rpcQueue.isFlushPending()) {
                     // if application stopped we will not schedule new timer event
                     if (getConnection().isApplicationRunning()) {
                         rpc.onTimer();
