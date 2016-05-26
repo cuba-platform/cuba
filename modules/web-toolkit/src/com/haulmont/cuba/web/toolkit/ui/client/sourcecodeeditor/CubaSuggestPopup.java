3740d305a6fa84b3f5e98fd0c2b1bf35e4975be2
Daniil Tsarev <tsarev@haulmont.com>
2016-05-26 11:35:19 +0400

    PL-7231 Incorrect progress icon in CodeEditor

---
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/sourcecodeeditor/CubaSuggestPopup.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2008-2016 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.cuba.web.toolkit.ui.client.sourcecodeeditor;
+
+import com.google.gwt.dom.client.Style;
+import org.vaadin.aceeditor.client.SuggestPopup;
+
+public class CubaSuggestPopup extends SuggestPopup {
+
+    public CubaSuggestPopup() {
+        Style style = loadingImage.getElement().getStyle();
+
+        style.clearWidth();
+        style.clearHeight();
+        style.setDisplay(Style.Display.BLOCK);
+    }
+}
---
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/sourcecodeeditor/CubaSuggesterConnector.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2008-2016 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.cuba.web.toolkit.ui.client.sourcecodeeditor;
+
+import com.vaadin.shared.ui.Connect;
+import org.vaadin.aceeditor.SuggestionExtension;
+import org.vaadin.aceeditor.client.SuggestPopup;
+import org.vaadin.aceeditor.client.SuggesterConnector;
+
+@SuppressWarnings("serial")
+@Connect(SuggestionExtension.class)
+public class CubaSuggesterConnector extends SuggesterConnector {
+
+    @Override
+    protected SuggestPopup createSuggestionPopup() {
+        SuggestPopup sp = new CubaSuggestPopup();
+        sp.setOwner(widget);
+        updatePopupPosition(sp);
+        sp.setSuggestionSelectedListener(this);
+        sp.show();
+        return sp;
+    }
+}
