073459537a2d13de69619c7e64ba4b400a18183b
Andrey Subbotin <subbotin@haulmont.com>
2016-06-20 16:33:21 +0400

    PL-7376 UnsupportedOperationException while using RuntimePropertiesFrame

--- modules/global/src/com/haulmont/cuba/core/app/dynamicattributes/DynamicAttributesMetaClass.java
+++ modules/global/src/com/haulmont/cuba/core/app/dynamicattributes/DynamicAttributesMetaClass.java
@@ -63,7 +63,7 @@ public class DynamicAttributesMetaClass extends MetadataObjectImpl implements Me
 
     @Override
     public Class getJavaClass() {
-        throw new UnsupportedOperationException();
+        return getClass();
     }
 
     @Override
