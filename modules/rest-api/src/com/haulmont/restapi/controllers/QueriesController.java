556ab63708dde4af73fd65d1213dc272d64a00e7
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-06-28 14:10:11 +0400

    PL-6730 QueriesController and RestQueriesManager

---
+++ modules/rest-api/src/com/haulmont/restapi/controllers/QueriesController.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2008-2016 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.restapi.controllers;
+
+import com.haulmont.chile.core.datatypes.Datatypes;
+import com.haulmont.chile.core.datatypes.impl.*;
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.cuba.core.app.serialization.EntitySerializationAPI;
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.core.global.Security;
+import com.haulmont.cuba.security.entity.EntityOp;
+import com.haulmont.restapi.exception.RestAPIException;
+import com.haulmont.restapi.query.RestQueriesManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.*;
+
+
+/**
+ * Controller that is used for operations with predefined JPQL queries
+ */
+@RestController
+@RequestMapping("/queries")
+public class QueriesController {
+
+    @Inject
+    protected RestQueriesManager restQueriesManager;
+
+    @Inject
+    protected DataManager dataManager;
+
+    @Inject
+    protected Metadata metadata;
+
+    @Inject
+    protected EntitySerializationAPI entitySerializationAPI;
+
+    @Inject
+    protected Security security;
+
+    @RequestMapping(value = "/{entityName}/{queryName}", method = RequestMethod.GET)
+    public String executeQuery(@PathVariable String entityName,
+                               @PathVariable String queryName,
+                               @RequestParam Map<String, String> params) throws ClassNotFoundException, ParseException {
+
+        MetaClass metaClass = getMetaClass(entityName);
+        checkCanReadEntity(metaClass);
+
+        RestQueriesManager.QueryInfo queryInfo = restQueriesManager.getQuery(entityName, queryName);
+        if (queryInfo == null) {
+            throw new RestAPIException("Query not found",
+                    String.format("Query with name %s for entity %s not found", queryName, entityName),
+                    HttpStatus.NOT_FOUND);
+        }
+
+        LoadContext<Entity> ctx = new LoadContext<>(metaClass);
+        LoadContext.Query query = new LoadContext.Query(queryInfo.getJpql());
+
+        for (RestQueriesManager.QueryParamInfo paramInfo : queryInfo.getParams()) {
+            String paramName = paramInfo.getName();
+            String requestParamValue = params.get(paramName);
+            if (requestParamValue == null) {
+                throw new RestAPIException("Query parameter not found",
+                        String.format("Query parameter %s not found", paramName),
+                        HttpStatus.BAD_REQUEST);
+            }
+
+            Class<?> clazz = ClassUtils.forName(paramInfo.getType(), getClass().getClassLoader());
+            Object objectParamValue = toObject(clazz, requestParamValue);
+            query.setParameter(paramName, objectParamValue);
+        }
+
+        ctx.setQuery(query);
+        ctx.setView(queryInfo.getViewName());
+        List<Entity> entities = dataManager.loadList(ctx);
+        return entitySerializationAPI.toJson(entities);
+    }
+
+    @RequestMapping(value = "/{entityName}", method = RequestMethod.GET)
+    public List<RestQueriesManager.QueryInfo> loadQueriesList(@PathVariable String entityName) {
+        MetaClass metaClass = getMetaClass(entityName);
+        checkCanReadEntity(metaClass);
+        return restQueriesManager.getQueries(entityName);
+    }
+
+
+    //TODO extract to the helper class
+    protected MetaClass getMetaClass(String entityName) {
+        MetaClass metaClass = metadata.getClass(entityName);
+        if (metaClass == null) {
+            throw new RestAPIException("MetaClass not found",
+                    String.format("MetaClass %s not found", entityName),
+                    HttpStatus.NOT_FOUND);
+        }
+        return metaClass;
+    }
+
+    protected void checkCanReadEntity(MetaClass metaClass) {
+        if (!security.isEntityOpPermitted(metaClass, EntityOp.READ)) {
+            throw new RestAPIException("Reading forbidden",
+                    String.format("Reading of the %s is forbidden", metaClass.getName()),
+                    HttpStatus.FORBIDDEN);
+        }
+    }
+
+
+    protected Object toObject(Class clazz, String value) throws ParseException {
+        if (String.class == clazz) return value;
+        if (Integer.class == clazz || Integer.TYPE == clazz
+                || Byte.class == clazz || Byte.TYPE == clazz
+                || Short.class == clazz || Short.TYPE == clazz) return Datatypes.get(IntegerDatatype.NAME).parse(value);
+        if (Date.class == clazz) {
+            try {
+                return Datatypes.get(DateTimeDatatype.NAME).parse(value);
+            } catch (ParseException e) {
+                try {
+                    return Datatypes.get(DateDatatype.NAME).parse(value);
+                } catch (ParseException e1) {
+                    return Datatypes.get(TimeDatatype.NAME).parse(value);
+                }
+            }
+        }
+        if (BigDecimal.class == clazz) return Datatypes.get(BigDecimalDatatype.NAME).parse(value);
+        if (Boolean.class == clazz || Boolean.TYPE == clazz) return Datatypes.get(BooleanDatatype.NAME).parse(value);
+        if (Long.class == clazz || Long.TYPE == clazz) return Datatypes.get(LongDatatype.NAME).parse(value);
+        if (Double.class == clazz || Double.TYPE == clazz
+                || Float.class == clazz || Float.TYPE == clazz) return Datatypes.get(DoubleDatatype.NAME).parse(value);
+        if (UUID.class == clazz) return UUID.fromString(value);
+        throw new IllegalArgumentException("Parameters of type " + clazz.getName() + " are not supported");
+    }
+}
---
+++ modules/rest-api/src/com/haulmont/restapi/query/RestQueriesManager.java
@@ -0,0 +1,248 @@
+/*
+ * Copyright (c) 2008-2016 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.restapi.query;
+
+import com.haulmont.bali.util.Dom4j;
+import com.haulmont.cuba.core.global.Resources;
+import com.haulmont.cuba.core.sys.AppContext;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.text.StrTokenizer;
+import org.dom4j.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
+
+
+/**
+ * Class is used for loading and storing of predefined JPQL queries that are used by the REST API.
+ * Queries are loaded from configuration files defined by the {@code cuba.rest.queriesConfig} application property.
+ */
+@Component("cuba_RestQueriesManager")
+public class RestQueriesManager {
+
+    protected final String CUBA_REST_QUERIES_CONFIG_PROP_NAME = "cuba.rest.queriesConfig";
+
+    protected volatile boolean initialized;
+
+    protected ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    @Inject
+    protected Resources resources;
+
+    protected static Logger log = LoggerFactory.getLogger(RestQueriesManager.class);
+
+    protected List<QueryInfo> queries = new ArrayList<>();
+
+    /**
+     * Returns a query description with the given name for the given entity.
+     * @param entityName entity name
+     * @param queryName query name
+     * @return query description
+     */
+    @Nullable
+    public QueryInfo getQuery(String entityName, String queryName) {
+        lock.readLock().lock();
+        try {
+            checkInitialized();
+            for (QueryInfo query : queries) {
+                if (queryName.equals(query.getName()) && entityName.equals(query.getEntityName())) {
+                    return query;
+                }
+            }
+            return null;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public List<QueryInfo> getQueries() {
+        lock.readLock().lock();
+        try {
+            checkInitialized();
+            return queries;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public List<QueryInfo> getQueries(String entityName) {
+        lock.readLock().lock();
+        try {
+            checkInitialized();
+            return queries.stream()
+                    .filter(queryInfo -> entityName.equals(queryInfo.getEntityName()))
+                    .collect(Collectors.toList());
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    protected void checkInitialized() {
+        if (!initialized) {
+            lock.readLock().unlock();
+            lock.writeLock().lock();
+            try {
+                if (!initialized) {
+                    init();
+                    initialized = true;
+                }
+            } finally {
+                lock.readLock().lock();
+                lock.writeLock().unlock();
+            }
+        }
+    }
+
+    protected void init() {
+        String configName = AppContext.getProperty(CUBA_REST_QUERIES_CONFIG_PROP_NAME);
+        StrTokenizer tokenizer = new StrTokenizer(configName);
+        for (String location : tokenizer.getTokenArray()) {
+            Resource resource = resources.getResource(location);
+            if (resource.exists()) {
+                InputStream stream = null;
+                try {
+                    stream = resource.getInputStream();
+                    loadConfig(Dom4j.readDocument(stream).getRootElement());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    IOUtils.closeQuietly(stream);
+                }
+            } else {
+                log.warn("Resource " + location + " not found, ignore it");
+            }
+        }
+    }
+
+    protected void loadConfig(Element rootElem) {
+        for (Element queryElem : Dom4j.elements(rootElem, "query")) {
+            String queryName = queryElem.attributeValue("name");
+            String entityName = queryElem.attributeValue("entity");
+            String viewName = queryElem.attributeValue("view");
+            String jpql = queryElem.elementText("jpql");
+
+            QueryInfo queryInfo = new QueryInfo();
+            queryInfo.setName(queryName);
+            queryInfo.setEntityName(entityName);
+            queryInfo.setViewName(viewName);
+            queryInfo.setJpql(jpql);
+
+            Element paramsEl = queryElem.element("params");
+            for (Element paramElem : Dom4j.elements(paramsEl, "param")) {
+                String paramName = paramElem.attributeValue("name");
+                String paramType = paramElem.attributeValue("type");
+                QueryParamInfo param = new QueryParamInfo(paramName, paramType);
+                queryInfo.getParams().add(param);
+            }
+
+            queries.add(queryInfo);
+        }
+    }
+
+
+    /**
+     * Class stores an information about the predefined JPQL query
+     */
+    public static class QueryInfo {
+
+        protected String name;
+        protected String jpql;
+        protected String entityName;
+        protected String viewName;
+        protected List<QueryParamInfo> params = new ArrayList<>();
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getJpql() {
+            return jpql;
+        }
+
+        public void setJpql(String jpql) {
+            this.jpql = jpql;
+        }
+
+        public String getEntityName() {
+            return entityName;
+        }
+
+        public void setEntityName(String entityName) {
+            this.entityName = entityName;
+        }
+
+        public String getViewName() {
+            return viewName;
+        }
+
+        public void setViewName(String viewName) {
+            this.viewName = viewName;
+        }
+
+        public List<QueryParamInfo> getParams() {
+            return params;
+        }
+
+        public void setParams(List<QueryParamInfo> params) {
+            this.params = params;
+        }
+    }
+
+    /**
+     * Class stores an information about the predefined JPQL query parameter
+     */
+    public static class QueryParamInfo {
+        protected String name;
+        protected String type;
+
+        public QueryParamInfo(String name, String type) {
+            this.name = name;
+            this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+    }
+}
