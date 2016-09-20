32d82fced430a567066ee70b72410186c6ac5197
Nikita Petunin <petunin@haulmont.com>
2016-09-20 12:05:22 +0400

    PL-7821 'Administrators' role is assigned to the 'Administrator' user only under HSQLDB

--- modules/core/db/init/mysql/create-db.sql
+++ modules/core/db/init/mysql/create-db.sql
@@ -868,6 +868,9 @@ values ('a405db59e6744f638afe269dda788fe8', now(), 0, 'anonymous', 'anonymous', 
 insert into SEC_ROLE (ID, CREATE_TS, VERSION, NAME, ROLE_TYPE)
 values ('0c018061b26f4de2a5bedff348347f93', current_timestamp, 0, 'Administrators', 10)^
 
+insert into SEC_USER_ROLE (ID, CREATE_TS, VERSION, USER_ID, ROLE_ID)
+values ('c838be0a96d04ef4a7c0dff348347f93', current_timestamp, 0, '608859871b61424794c7dff348347f93', '0c018061b26f4de2a5bedff348347f93')^
+
 insert into SEC_FILTER (ID,CREATE_TS,CREATED_BY,VERSION,UPDATE_TS,UPDATED_BY,DELETE_TS,DELETED_BY,COMPONENT,NAME,XML,USER_ID)
 values ('b61d18cbe79a46f3b16deaf4aebb10dd',{ts '2010-03-01 11:14:06.830'},'admin',2,{ts '2010-03-01 11:52:53.170'},'admin',null,null,'[sec$User.browse].genericFilter','Search by role',
 '<?xml version="1.0" encoding="UTF-8"?>
