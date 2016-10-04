1852b75411ce0ac8fb1ea5d4a33a9a2240c5b606
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-10-04 16:08:49 +0400

    PL-7961 Enable invoking services from inside middleware

--- modules/core/test/com/haulmont/cuba/core/sys/ServiceInterceptorTest.java
+++ modules/core/test/com/haulmont/cuba/core/sys/ServiceInterceptorTest.java
@@ -19,20 +19,25 @@ package com.haulmont.cuba.core.sys;
 
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 import com.haulmont.cuba.core.Transaction;
 import com.haulmont.cuba.core.app.TestingService;
 import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.RemoteException;
+import com.haulmont.cuba.security.app.UserSessions;
+import com.haulmont.cuba.security.entity.User;
+import com.haulmont.cuba.security.global.UserSession;
 import com.haulmont.cuba.testsupport.TestAppender;
 import com.haulmont.cuba.testsupport.TestContainer;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ServiceInterceptorTest {
 
@@ -54,33 +59,101 @@ public class ServiceInterceptorTest {
     @Test
     public void testOpenTransaction() throws Exception {
         TestingService service = AppBeans.get(TestingService.class);
-        int size;
-
         appender.getMessages().clear();
 
         // programmatic tx without proper completion
         Object tx = service.leaveOpenTransaction();
         ((Transaction) tx).commit();
-
-        size = Iterables.size(Iterables.filter(appender.getMessages(), new Predicate<String>() {
-            @Override
-            public boolean apply(@Nullable String input) {
-                return input != null && input.contains("Open transaction");
-            }
-        }));
-        assertEquals(1, size);
+        assertEquals(1, appender.getMessages().stream().filter(s -> s.contains("Open transaction")).count());
 
         appender.getMessages().clear();
 
         // declarative tx
         service.declarativeTransaction();
+        assertEquals(0, appender.getMessages().stream().filter(s -> s.contains("Open transaction")).count());
+    }
 
-        size = Iterables.size(Iterables.filter(appender.getMessages(), new Predicate<String>() {
-            @Override
-            public boolean apply(@Nullable String input) {
-                return input != null && input.contains("Open transaction");
+    @Test
+    public void testLogMessage() throws Exception {
+        TestingService service1 = AppBeans.get(TestingService.class);
+        ServiceInterceptorTestService service2 = AppBeans.get(ServiceInterceptorTestService.class);
+
+        // normally no log messages for internal invocations
+        appender.getMessages().clear();
+        service1.declarativeTransaction();
+        assertEquals(0, appender.getMessages().stream().filter(s -> s.contains("from another service")).count());
+
+        appender.getMessages().clear();
+        service2.declarativeTransaction();
+        assertEquals(0, appender.getMessages().stream().filter(s -> s.contains("from another service")).count());
+
+        // old behaviour
+        ServiceInterceptor serviceInterceptor = AppBeans.get(ServiceInterceptor.class);
+        serviceInterceptor.logInternalServiceInvocation = true;
+        try {
+            appender.getMessages().clear();
+            service1.declarativeTransaction();
+            assertEquals(0, appender.getMessages().stream().filter(s -> s.contains("from another service")).count());
+
+            appender.getMessages().clear();
+            service2.declarativeTransaction();
+            assertEquals(1, appender.getMessages().stream().filter(s -> s.contains("from another service")).count());
+        } finally {
+            serviceInterceptor.logInternalServiceInvocation = false;
+        }
+    }
+
+    @Test
+    public void testExceptionHandling() throws Exception {
+        TestingService service1 = AppBeans.get(TestingService.class);
+        ServiceInterceptorTestService service2 = AppBeans.get(ServiceInterceptorTestService.class);
+
+        appender.getMessages().clear();
+        try {
+            service1.executeWithException();
+        } catch (Exception e) {
+            assertTrue(e instanceof RemoteException
+                    && ((RemoteException) e).getFirstCauseException() instanceof TestingService.TestException);
+        }
+
+        appender.getMessages().clear();
+        try {
+            service2.executeWithException();
+        } catch (Exception e) {
+            assertTrue(e instanceof RemoteException
+                    && ((RemoteException) e).getFirstCauseException() instanceof TestingService.TestException);
+        }
+    }
+
+    @Test
+    public void testNewThread() throws Exception {
+        ServiceInterceptorTestService service = AppBeans.get(ServiceInterceptorTestService.class);
+        UserSessions userSessions = AppBeans.get(UserSessions.class);
+
+        // workaround for test security setup
+        Field startedField = AppContext.class.getDeclaredField("started");
+        startedField.setAccessible(true);
+        startedField.set(null, true);
+        AppContext.setSecurityContext(AppContext.NO_USER_CONTEXT);
+        UserSession userSession = new UserSession(AppContext.NO_USER_CONTEXT.getSessionId(), new User(), Collections.emptyList(), Locale.ENGLISH, true);
+        userSessions.add(userSession);
+
+        try {
+            appender.getMessages().clear();
+            service.declarativeTransactionNewThread();
+            assertEquals(0, appender.getMessages().stream().filter(s -> s.contains("from another service")).count());
+
+            appender.getMessages().clear();
+            try {
+                service.executeWithExceptionNewThread();
+            } catch (Exception e) {
+                assertTrue(e instanceof RemoteException
+                        && ((RemoteException) e).getFirstCauseException() instanceof TestingService.TestException);
             }
-        }));
-        assertEquals(0, size);
+
+        } finally {
+            userSessions.remove(userSession);
+            startedField.set(null, false);
+        }
     }
 }
