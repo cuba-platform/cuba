215b6a45c04746396f6b63a7814e08b991d3c8b7
Nikita Petunin <petunin@haulmont.com>
2016-10-11 16:40:17 +0400

    PL-7872 ColumnControlVisible cuts some part of last table column caption

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/table/CubaScrollTableWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/table/CubaScrollTableWidget.java
@@ -472,6 +472,15 @@ public class CubaScrollTableWidget extends VScrollTable implements ShortcutActio
                 rowRequestHandler.run(); // run immediately
             }
         }
+
+        @Override
+        public void resizeCaptionContainer(int rightSpacing) {
+            if (!td.getClassName().contains("-asc") && !td.getClassName().contains("-desc")) {
+                ComputedStyle computedStyle = new ComputedStyle(sortIndicator);
+                rightSpacing += (int) Math.ceil(computedStyle.getWidth());
+            }
+            super.resizeCaptionContainer(rightSpacing);
+        }
     }
 
     protected class CubaScrollTableBody extends VScrollTableBody {
