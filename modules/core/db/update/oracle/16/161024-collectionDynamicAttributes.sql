131f7f4f9d1e9b88f8b188662de1fccee8f3d1bc
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-10-26 10:35:12 +0400

    PL-8057 Dynamic attributes collection - fix db update tests

--- modules/core/db/update/oracle/16/161024-collectionDynamicAttributes.sql
+++ modules/core/db/update/oracle/16/161024-collectionDynamicAttributes.sql
@@ -1,2 +1,2 @@
-alter table SYS_CATEGORY_ATTR add column IS_COLLECTION char(1);
-alter table SYS_ATTR_VALUE add column PARENT_ID varchar2(32);
+alter table SYS_CATEGORY_ATTR add IS_COLLECTION char(1);
+alter table SYS_ATTR_VALUE add PARENT_ID varchar2(32);
