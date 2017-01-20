f6b1b681922c26027f9df97dda81ae7a4588bba7
Ilia Kuchmin <kuchmin@haulmont.com>
2017-01-20 17:10:21 +0400

    PL-8661 LoadContext.Query must have correct equals and hashCode implementations

---
+++ modules/global/test/com/haulmont/cuba/core/global/LoadContextQueryTest.java
@@ -0,0 +1,51 @@
+package com.haulmont.cuba.core.global;
+
+import com.haulmont.cuba.core.global.LoadContext.Query;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class LoadContextQueryTest {
+
+    @Test
+    public void twoQueryShouldBeEqualsIfOneOfCopyFirst() throws Exception {
+        Query expected = LoadContext.createQuery("select obj from Object obj where obj.param = :param")
+                                    .setParameter("param", "value")
+                                    .setFirstResult(100)
+                                    .setMaxResults(100);
+
+        assertEquals(expected, expected.copy());
+    }
+
+    @Test
+    public void twoQueriesShouldBeEqualsIfTheyHaveDifferentValueCacheableField() throws Exception {
+        Query expected = LoadContext.createQuery("select obj from Object obj where obj.param = :param")
+                                    .setParameter("param", "value")
+                                    .setFirstResult(100)
+                                    .setMaxResults(100)
+                                    .setCacheable(true);
+
+        Query actual = expected.copy().setCacheable(false);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inOtherCasesQueriesAreNotEquals() throws Exception {
+        Query expected = LoadContext.createQuery("select obj from Object obj where obj.param = :param")
+                                    .setParameter("param", "value")
+                                    .setFirstResult(100)
+                                    .setMaxResults(100)
+                                    .setCacheable(true);
+
+        assertNotEquals(expected, expected.copy().setQueryString("other query"));
+
+        assertNotEquals(expected, expected.copy().setParameter("param", "newValue"));
+
+        assertNotEquals(expected, expected.copy().setFirstResult(0));
+
+        assertNotEquals(expected, expected.copy().setMaxResults(10));
+
+    }
+}
