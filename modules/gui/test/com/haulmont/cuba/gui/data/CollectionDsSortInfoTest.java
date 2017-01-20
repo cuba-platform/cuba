8b1ed62f50ee3592247260778ce3a91f002c89ef
Ilia Kuchmin <kuchmin@haulmont.com>
2017-01-20 18:34:10 +0400

    PL-8660 SortInfo must have correct equals and hashCode implementations

---
+++ modules/gui/test/com/haulmont/cuba/gui/data/CollectionDsSortInfoTest.java
@@ -0,0 +1,42 @@
+package com.haulmont.cuba.gui.data;
+
+import com.haulmont.cuba.gui.data.CollectionDatasource.Sortable.SortInfo;
+import org.junit.Test;
+
+import static com.haulmont.cuba.gui.data.CollectionDatasource.Sortable.Order.ASC;
+import static com.haulmont.cuba.gui.data.CollectionDatasource.Sortable.Order.DESC;
+import static org.junit.Assert.*;
+
+public class CollectionDsSortInfoTest {
+
+    @Test
+    public void twoSortInfoShouldBeEqualsIfThemFieldsTheSame() throws Exception {
+        SortInfo<Object> expected = new SortInfo<>();
+        expected.setPropertyPath(1);
+        expected.setOrder(ASC);
+
+        SortInfo<Object> actual = new SortInfo<>();
+        actual.setPropertyPath(1);
+        actual.setOrder(ASC);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inOtherCasesSortInfoShouldBeDifferent() throws Exception {
+        SortInfo<Object> expected = new SortInfo<>();
+        expected.setPropertyPath(1);
+        expected.setOrder(ASC);
+
+        SortInfo<Object> actual = new SortInfo<>();
+        actual.setPropertyPath(1);
+        actual.setOrder(DESC);
+
+        assertNotEquals(expected, actual);
+
+        actual.setPropertyPath(2);
+        actual.setOrder(ASC);
+
+        assertNotEquals(expected, actual);
+    }
+}
