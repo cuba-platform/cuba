4652e600480b5f41870dc81a35504e88791143f9
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-02-28 12:16:53 +0400

    PL-8699 Extending an entity with an integer ID using the JOINED strategy twice causes PK violations

--- modules/core/test/com/haulmont/cuba/testmodel/create-db.sql
+++ modules/core/test/com/haulmont/cuba/testmodel/create-db.sql
@@ -296,6 +296,8 @@ create table TEST_ORDER (
     constraint TEST_ORDER_USER foreign key (USER_ID) references SEC_USER(ID)
 )^
 
+----------------------------------------------------------------------------------------------------------------
+
 create table TEST_LINK_ENTITY (
     ID bigint,
     --
@@ -318,4 +320,27 @@ create table TEST_MULTI_LINK_ENTITY (
     constraint FK_TEST_LINK_ENTITY_1 foreign key (A_ID) references TEST_LINK_ENTITY(ID),
     constraint FK_TEST_LINK_ENTITY_2 foreign key (B_ID) references TEST_LINK_ENTITY(ID),
     constraint FK_TEST_LINK_ENTITY_3 foreign key (C_ID) references TEST_LINK_ENTITY(ID)
+)^
+
+----------------------------------------------------------------------------------------------------------------
+
+create table TEST_JOINED_LONGID_BASE (
+    ID bigint not null,
+    DTYPE varchar(100),
+    NAME varchar(255),
+    primary key (ID)
+)^
+
+create table TEST_JOINED_LONGID_FOO (
+    ID bigint not null,
+    FOO varchar(255),
+    primary key (ID),
+    constraint FK_TEST_JOINED_LONGID_FOO foreign key (ID) references TEST_JOINED_LONGID_BASE(ID)
+)^
+
+create table TEST_JOINED_LONGID_BAR (
+    ID bigint not null,
+    BAR varchar(255),
+    primary key (ID),
+    constraint FK_TEST_JOINED_LONGID_BAR foreign key (ID) references TEST_JOINED_LONGID_BASE(ID)
 )^
