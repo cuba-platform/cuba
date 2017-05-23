0492579e633064140d48861337d41f94a9ec98ef
Maxim Gorbunkov <gorbunkov@haulmont.com>
2017-05-23 15:48:57 +0400

    PL-9131 EntityStates bean

--- modules/global/src/com/haulmont/cuba/core/global/PersistenceHelper.java
+++ modules/global/src/com/haulmont/cuba/core/global/PersistenceHelper.java
@@ -16,15 +16,7 @@
  */
 package com.haulmont.cuba.core.global;
 
-import com.haulmont.bali.util.Preconditions;
-import com.haulmont.cuba.core.entity.AbstractNotPersistentEntity;
-import com.haulmont.cuba.core.entity.BaseEntityInternalAccess;
 import com.haulmont.cuba.core.entity.BaseGenericIdEntity;
-import com.haulmont.cuba.core.entity.SoftDelete;
-import org.apache.commons.lang.StringUtils;
-
-import java.lang.annotation.Annotation;
-import java.util.Objects;
 
 /**
  * Utility class providing some information about persistent entities.
@@ -33,175 +25,72 @@ import java.util.Objects;
 public class PersistenceHelper {
 
     /**
-     * Determines whether the instance is <em>New</em>, i.e. just created and not stored in database yet.
-     *
-     * @param entity entity instance
-     * @return  - true if the instance is a new persistent entity, or if it is actually in Managed state
-     *            but newly-persisted in this transaction <br>
-     *          - true if the instance is a new non-persistent entity never returned from DataManager <br>
-     *          - false otherwise
-     * @throws IllegalArgumentException if entity instance is null
+     * @see EntityStates#isNew(Object)
      */
     public static boolean isNew(Object entity) {
-        Preconditions.checkNotNullArgument(entity, "entity is null");
-        if (entity instanceof BaseGenericIdEntity) {
-            return BaseEntityInternalAccess.isNew((BaseGenericIdEntity) entity);
-        } else if (entity instanceof AbstractNotPersistentEntity) {
-            return BaseEntityInternalAccess.isNew((AbstractNotPersistentEntity) entity);
-        }
-        return false;
+        return AppBeans.get(EntityStates.class).isNew(entity);
     }
 
     /**
-     * Determines whether the instance is <em>Managed</em>, i.e. attached to a persistence context.
-     *
-     * @param entity entity instance
-     * @return - true if the instance is managed,<br>
-     *         - false if it is New (and not yet persisted) or Detached, or if it is not a persistent entity
-     * @throws IllegalArgumentException if entity instance is null
+     * @see EntityStates#isManaged(Object)
      */
     public static boolean isManaged(Object entity) {
-        Preconditions.checkNotNullArgument(entity, "entity is null");
-        if (entity instanceof BaseGenericIdEntity) {
-            return BaseEntityInternalAccess.isManaged((BaseGenericIdEntity) entity);
-        }
-        return false;
+        return AppBeans.get(EntityStates.class).isManaged(entity);
     }
 
     /**
-     * Determines whether the instance is <em>Detached</em>, i.e. stored in database but not attached to a persistence
-     * context at the moment.
-     *
-     * @param entity entity instance
-     * @return - true if the instance is detached,<br>
-     *         - false if it is New or Managed, or if it is not a persistent entity
-     * @throws IllegalArgumentException if entity instance is null
+     * @see EntityStates#isDeleted(Object)
      */
     public static boolean isDetached(Object entity) {
-        Preconditions.checkNotNullArgument(entity, "entity is null");
-        if (entity instanceof BaseGenericIdEntity && BaseEntityInternalAccess.isDetached((BaseGenericIdEntity) entity)) {
-            return true;
-        }
-        return false;
+        return AppBeans.get(EntityStates.class).isDetached(entity);
     }
 
     /**
-     * @param entityClass entity class
-     * @return entity name as defined in {@link javax.persistence.Entity} annotation
+     * @see EntityStates#getEntityName(Class)
      */
     public static String getEntityName(Class<?> entityClass) {
-        Annotation annotation = entityClass.getAnnotation(javax.persistence.Entity.class);
-        if (annotation == null)
-            throw new IllegalArgumentException("Class " + entityClass + " is not a persistent entity");
-        String name = ((javax.persistence.Entity) annotation).name();
-        if (!StringUtils.isEmpty(name))
-            return name;
-        else
-            return entityClass.getSimpleName();
+        return AppBeans.get(EntityStates.class).getEntityName(entityClass);
     }
 
     /**
-     * Determines whether the entity supports <em>Soft Deletion</em>.
-     *
-     * @param entityClass entity class
-     * @return {@code true} if the entity implements {@link SoftDelete}
+     * @see EntityStates#isSoftDeleted(Class)
      */
     public static boolean isSoftDeleted(Class entityClass) {
-        return SoftDelete.class.isAssignableFrom(entityClass);
+        return AppBeans.get(EntityStates.class).isSoftDeleted(entityClass);
     }
 
     /**
-     * Checks if the property is loaded from DB.
-     * <p>Non-persistent attributes are considered loaded if they do not have related properties, or all related
-     * properties are loaded.
-     *
-     * @param entity   entity
-     * @param property name of the property. Only immediate attributes of the entity are supported.
-     * @return true if loaded
+     * @see EntityStates#isLoaded(Object, String)
      */
     public static boolean isLoaded(Object entity, String property) {
-        PersistentAttributesLoadChecker checker = AppBeans.get(PersistentAttributesLoadChecker.NAME);
-        return checker.isLoaded(entity, property);
+        return AppBeans.get(EntityStates.class).isLoaded(entity, property);
     }
 
     /**
-     * Check that entity has all specified properties loaded from DB.
-     * Throw exception if property is not loaded.
-     *
-     * @param entity entity
-     * @param properties property names
-     * @throws IllegalArgumentException if at least one of properties is not loaded
+     * @see EntityStates#checkLoaded(Object, String...)
      */
     public static void checkLoaded(Object entity, String... properties) {
-        Objects.requireNonNull(entity);
-
-        for (String property: properties) {
-            if (!isLoaded(entity, property)) {
-                String errorMessage = String.format("%s.%s is not loaded", entity.getClass().getSimpleName(), property);
-                throw new IllegalArgumentException(errorMessage);
-            }
-        }
+        AppBeans.get(EntityStates.class).checkLoaded(entity, properties);
     }
 
     /**
-     * Determines whether the entity instance was <em>deleted</em>.
-     *
-     * @param entity entity instance
-     * @return - true if the instance was deleted
-     *         - false otherwise
-     * @throws IllegalArgumentException if entity instance is null
+     * @see EntityStates#isDeleted(Object)
      */
     public static boolean isDeleted(Object entity) {
-        Preconditions.checkNotNullArgument(entity, "entity is null");
-        if (entity instanceof SoftDelete && ((SoftDelete) entity).isDeleted())
-            return true;
-        if (entity instanceof BaseGenericIdEntity && BaseEntityInternalAccess.isRemoved((BaseGenericIdEntity) entity)) {
-            return true;
-        }
-        return false;
+        return AppBeans.get(EntityStates.class).isDetached(entity);
     }
 
     /**
-     * Makes a newly constructed object detached. The detached object can be passed to {@code DataManager.commit()} or
-     * to {@code EntityManager.merge()} to save its state to the database.
-     * <p>If an object with such ID does not exist in the database, a new object will be inserted.
-     * <p>If the entity is {@code Versioned}, the version attribute should be equal to the latest version existing in
-     * the database, or null for a new object.
-     *
-     * @param entity    entity in the New state
-     * @throws IllegalStateException if the entity is Managed
-     * @see #isDetached(Object)
-     * @see #makePatch(BaseGenericIdEntity)
+     * @see EntityStates#makeDetached(BaseGenericIdEntity)
      */
     public static void makeDetached(BaseGenericIdEntity entity) {
-        Preconditions.checkNotNullArgument(entity, "entity is null");
-        if (BaseEntityInternalAccess.isManaged(entity))
-            throw new IllegalStateException("entity is managed");
-
-        BaseEntityInternalAccess.setNew(entity, false);
-        BaseEntityInternalAccess.setDetached(entity, true);
+        AppBeans.get(EntityStates.class).makeDetached(entity);
     }
 
     /**
-     * Makes a newly constructed object a patch object. The patch object is {@code !isNew() && !isDetached() && !isManaged()}.
-     * The patch object can be passed to {@code DataManager.commit()} or
-     * to {@code EntityManager.merge()} to save its state to the database. Only <b>non-null values</b> of attributes are
-     * updated.
-     * <p>If an object with such ID does not exist in the database, a new object will be inserted.
-     * <p>If the entity is {@code Versioned}, the version attribute should be null or equal to the latest version existing in
-     * the database.
-     *
-     * @param entity    entity in the New or Detached state
-     * @throws IllegalStateException if the entity is Managed
-     * @see #isDetached(Object)
-     * @see #makeDetached(BaseGenericIdEntity)
+     * @see EntityStates#makePatch(BaseGenericIdEntity)
      */
     public static void makePatch(BaseGenericIdEntity entity) {
-        Preconditions.checkNotNullArgument(entity, "entity is null");
-        if (BaseEntityInternalAccess.isManaged(entity))
-            throw new IllegalStateException("entity is managed");
-
-        BaseEntityInternalAccess.setNew(entity, false);
-        BaseEntityInternalAccess.setDetached(entity, false);
+        AppBeans.get(EntityStates.class).makePatch(entity);
     }
 }
