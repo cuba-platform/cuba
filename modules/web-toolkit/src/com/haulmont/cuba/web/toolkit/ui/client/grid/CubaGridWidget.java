1f75d3e868fafbb5f1e6c02d67fcc4ba20b5a17e
Gleb Gorelov <gorelov@haulmont.com>
2017-05-25 16:27:14 +0400

    PL 9144 ItemClickEvent is not thrown in DataGrid

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/grid/CubaGridWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/grid/CubaGridWidget.java
@@ -17,8 +17,11 @@
 package com.haulmont.cuba.web.toolkit.ui.client.grid;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Widget;
+import com.haulmont.cuba.web.toolkit.ui.client.grid.events.*;
 import com.vaadin.client.WidgetUtil;
+import com.vaadin.client.widget.grid.events.*;
 import com.vaadin.client.widgets.Grid;
 import elemental.json.JsonObject;
 
@@ -61,5 +64,80 @@ public class CubaGridWidget extends Grid<JsonObject> {
         Widget widget = WidgetUtil.findWidget(e, null);
         return widget instanceof HasClickSettings &&
                 ((HasClickSettings) widget).isClickThroughEnabled();
+    }
+
+    @Override
+    public HandlerRegistration addBodyKeyDownHandler(BodyKeyDownHandler handler) {
+        return addHandler(handler, CubaGridKeyDownEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addBodyKeyUpHandler(BodyKeyUpHandler handler) {
+        return addHandler(handler, CubaGridKeyUpEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addBodyKeyPressHandler(BodyKeyPressHandler handler) {
+        return addHandler(handler, CubaGridKeyPressEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addHeaderKeyDownHandler(HeaderKeyDownHandler handler) {
+        return addHandler(handler, CubaGridKeyDownEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addHeaderKeyUpHandler(HeaderKeyUpHandler handler) {
+        return addHandler(handler, CubaGridKeyUpEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addHeaderKeyPressHandler(HeaderKeyPressHandler handler) {
+        return addHandler(handler, CubaGridKeyPressEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addFooterKeyDownHandler(FooterKeyDownHandler handler) {
+        return addHandler(handler, CubaGridKeyDownEvent.EVENT_TYPE);
+}
+
+    @Override
+    public HandlerRegistration addFooterKeyUpHandler(FooterKeyUpHandler handler) {
+        return addHandler(handler, CubaGridKeyUpEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addFooterKeyPressHandler(FooterKeyPressHandler handler) {
+        return addHandler(handler, CubaGridKeyPressEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addBodyClickHandler(BodyClickHandler handler) {
+        return addHandler(handler, CubaGridClickEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addHeaderClickHandler(HeaderClickHandler handler) {
+        return addHandler(handler, CubaGridClickEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addFooterClickHandler(FooterClickHandler handler) {
+        return addHandler(handler, CubaGridClickEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addBodyDoubleClickHandler(BodyDoubleClickHandler handler) {
+        return addHandler(handler, CubaGridDoubleClickEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addHeaderDoubleClickHandler(HeaderDoubleClickHandler handler) {
+        return addHandler(handler, CubaGridDoubleClickEvent.EVENT_TYPE);
+    }
+
+    @Override
+    public HandlerRegistration addFooterDoubleClickHandler(FooterDoubleClickHandler handler) {
+        return addHandler(handler, CubaGridDoubleClickEvent.EVENT_TYPE);
     }
 }
