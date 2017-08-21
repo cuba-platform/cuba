0eface724ab6cb0914ae55f19d4ab3c71a3e67d9
Roman Pinyazhin <pinyazhin@haulmont.com>
2017-08-21 16:12:46 +0400

    PL-9261 'Enumeration' field should be required for the dynamic attributes with the 'Enumeration' type

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebListEditor.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebListEditor.java
@@ -22,6 +22,7 @@ import com.haulmont.cuba.gui.components.ListEditor;
 import com.haulmont.cuba.gui.components.listeditor.ListEditorDelegate;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomField;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.List;
@@ -146,6 +147,7 @@ public class WebListEditor extends WebAbstractField<WebListEditor.CubaListEditor
         if (newValue != null && !(newValue instanceof List)) {
             throw new IllegalArgumentException("Value type must be List");
         }
+
         super.setValue(newValue);
         delegate.setValue((List) newValue);
 
@@ -188,6 +190,11 @@ public class WebListEditor extends WebAbstractField<WebListEditor.CubaListEditor
         @Override
         public Class<List> getType() {
             return List.class;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return super.isEmpty() || CollectionUtils.isEmpty(getValue());
         }
     }
 
