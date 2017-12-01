071c869cb4abbf247c902e4454c32cec4f9e0dc9
Maxim Gorbunkov <gorbunkov@haulmont.com>
2017-12-01 09:53:21 +0400

    PL-9880 "Default by default" filters - update script fix

--- modules/core/db/update/mssql/17/171116-addGlobalDefaultToFilterEntity.sql
+++ modules/core/db/update/mssql/17/171116-addGlobalDefaultToFilterEntity.sql
@@ -1,2 +1,2 @@
-alter table SEC_FILTER add GLOBAL_DEFAULT tinyint;
-update SEC_FILTER set GLOBAL_DEFAULT = 0;
+alter table SEC_FILTER add GLOBAL_DEFAULT tinyint^
+update SEC_FILTER set GLOBAL_DEFAULT = 0^
--- modules/core/db/update/oracle/17/171116-addGlobalDefaultToFilterEntity.sql
+++ modules/core/db/update/oracle/17/171116-addGlobalDefaultToFilterEntity.sql
@@ -1,2 +1,2 @@
-alter table SEC_FILTER add GLOBAL_DEFAULT char(1);
-update SEC_FILTER set GLOBAL_DEFAULT = 0;
+alter table SEC_FILTER add GLOBAL_DEFAULT char(1)^
+update SEC_FILTER set GLOBAL_DEFAULT = 0^
