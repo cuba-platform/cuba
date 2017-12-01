9f9a875bd58615793c0664218e9506c9978fc34e
Daniil Tsarev <tsarev@haulmont.com>
2017-12-01 13:44:16 +0400

    PL-10017 Email sending failed when using Amazon SES and file attached for an e-mail

--- modules/core/src/com/haulmont/cuba/core/app/EmailSender.java
+++ modules/core/src/com/haulmont/cuba/core/app/EmailSender.java
@@ -31,10 +31,10 @@ import org.perf4j.log4j.Log4JStopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.mail.Message;
@@ -173,7 +173,7 @@ public class EmailSender implements EmailSenderAPI {
         String disposition = attachment.getDisposition() != null ? attachment.getDisposition() : Part.INLINE;
         String charset = MimeUtility.mimeCharset(attachment.getEncoding() != null ?
                 attachment.getEncoding() : StandardCharsets.UTF_8.name());
-        String contentTypeValue = String.format("%s; charset=%s; name=%s", mimeType, charset, encodedFileName);
+        String contentTypeValue = String.format("%s; charset=%s; name=\"%s\"", mimeType, charset, encodedFileName);
 
         MimeBodyPart attachmentPart = new MimeBodyPart();
         attachmentPart.setDataHandler(new DataHandler(source));
