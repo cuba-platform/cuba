7b72225021d5fc10149fee24b97496a4c8e505f2
budarov <budarov@haulmont.com>
2017-12-25 13:14:40 +0400

    Add Table.PlainTextCell to list of inline generated components for desktop table #PL-10197

--- modules/gui/src/com/haulmont/cuba/gui/app/core/entitylog/EntityLogBrowser.java
+++ modules/gui/src/com/haulmont/cuba/gui/app/core/entitylog/EntityLogBrowser.java
@@ -274,7 +274,7 @@ public class EntityLogBrowser extends AbstractWindow {
                 return new Table.PlainTextCell(entity.getObjectEntityId().toString());
             }
             return null;
-        });
+        }, Table.PlainTextCell.class);
     }
 
     public TreeMap<String, Object> getEntityMetaClasses() {
