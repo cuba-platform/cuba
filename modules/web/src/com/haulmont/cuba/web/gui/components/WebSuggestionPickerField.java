72a4eca695629fb344f7c47e2162b2869413079d
Gleb Gorelov <gorelov@haulmont.com>
2018-02-16 17:08:12 +0400

    PL-10400 No help icon is displayed if the ContextHelpIconClickHandler added to some components

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebSuggestionPickerField.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebSuggestionPickerField.java
@@ -27,6 +27,7 @@ import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class WebSuggestionPickerField extends WebSuggestionField implements SuggestionPickerField {
 
@@ -85,6 +86,16 @@ public class WebSuggestionPickerField extends WebSuggestionField implements Sugg
     @Override
     public void setContextHelpTextHtmlEnabled(boolean enabled) {
         pickerField.setContextHelpTextHtmlEnabled(enabled);
+    }
+
+    @Override
+    public Consumer<ContextHelpIconClickEvent> getContextHelpIconClickHandler() {
+        return pickerField.getContextHelpIconClickHandler();
+    }
+
+    @Override
+    public void setContextHelpIconClickHandler(Consumer<ContextHelpIconClickEvent> handler) {
+        pickerField.setContextHelpIconClickHandler(handler);
     }
 
     protected void initValueSync(WebPickerField.Picker picker) {
