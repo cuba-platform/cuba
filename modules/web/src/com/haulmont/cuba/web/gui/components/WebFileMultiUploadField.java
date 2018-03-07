a72245c753cbd5c49c801d191e9e1e968e84534b
Yuriy Artamonov <artamonov@haulmont.com>
2018-03-07 19:15:35 +0400

    Get rid of SWF file upload #444

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebFileMultiUploadField.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebFileMultiUploadField.java
@@ -14,6 +14,7 @@
  * limitations under the License.
  *
  */
+
 package com.haulmont.cuba.web.gui.components;
 
 import com.haulmont.cuba.client.ClientConfig;
@@ -21,35 +22,27 @@ import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.Configuration;
 import com.haulmont.cuba.core.global.FileStorageException;
 import com.haulmont.cuba.core.global.Messages;
-import com.haulmont.cuba.gui.AppConfig;
 import com.haulmont.cuba.gui.components.FileMultiUploadField;
 import com.haulmont.cuba.gui.components.Frame;
 import com.haulmont.cuba.gui.components.Window;
 import com.haulmont.cuba.gui.components.compatibility.MultiUploadFieldListenerWrapper;
-import com.haulmont.cuba.gui.theme.ThemeConstants;
 import com.haulmont.cuba.gui.upload.FileUploadingAPI;
 import com.haulmont.cuba.web.App;
 import com.haulmont.cuba.web.WebConfig;
 import com.haulmont.cuba.web.WebWindowManager;
 import com.haulmont.cuba.web.gui.icons.IconResolver;
 import com.haulmont.cuba.web.gui.FileUploadTypesHelper;
-import com.haulmont.cuba.web.widgets.CubaAbstractUploadComponent;
 import com.haulmont.cuba.web.widgets.CubaFileUpload;
-import com.haulmont.cuba.web.widgets.CubaMultiUpload;
-import com.haulmont.cuba.web.toolkit.VersionedThemeResource;
-import com.vaadin.server.Page;
-import com.vaadin.server.WebBrowser;
 import com.vaadin.ui.Component;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbstractUploadComponent>
+public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaFileUpload>
         implements FileMultiUploadField {
 
     protected final Map<UUID, String> files = new LinkedHashMap<>();
@@ -63,141 +56,7 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
     public WebFileMultiUploadField() {
         fileUploading = AppBeans.get(FileUploadingAPI.NAME);
 
-        WebBrowser webBrowser = Page.getCurrent().getWebBrowser();
-        if ((webBrowser.isIE() && !webBrowser.isEdge()) && webBrowser.getBrowserMajorVersion() < 10) {
-            initOldComponent();
-        } else {
-            initComponent();
-        }
-    }
-
-    protected void initOldComponent() {
-        CubaMultiUpload impl = createOldComponent();
-
-        ThemeConstants theme = App.getInstance().getThemeConstants();
-
-        String width = theme.get("cuba.web.WebFileMultiUploadField.upload.width");
-        String height = theme.get("cuba.web.WebFileMultiUploadField.upload.height");
-
-        impl.setWidth(width);
-        impl.setHeight(height);
-
-        int buttonTextLeft = theme.getInt("cuba.web.WebFileMultiUploadField.buttonText.left");
-        int buttonTextTop = theme.getInt("cuba.web.WebFileMultiUploadField.buttonText.top");
-
-        impl.setButtonTextLeft(buttonTextLeft);
-        impl.setButtonTextTop(buttonTextTop);
-
-        impl.setButtonWidth(Integer.parseInt(width.replace("px", "")));
-        impl.setButtonHeight(Integer.parseInt(height.replace("px", "")));
-
-        Messages messages = AppBeans.get(Messages.NAME);
-        impl.setCaption(messages.getMessage(AppConfig.getMessagesPack(), "multiupload.submit"));
-
-        Configuration configuration = AppBeans.get(Configuration.NAME);
-        impl.setFileSizeLimitMB(configuration.getConfig(ClientConfig.class).getMaxUploadSizeMb());
-
-        WebConfig webConfig = configuration.getConfig(WebConfig.class);
-        if (!webConfig.getUseFontIcons()) {
-            impl.setButtonImage(new VersionedThemeResource("components/multiupload/images/multiupload-button.png"));
-        } else {
-            impl.setButtonImage(new VersionedThemeResource("components/multiupload/images/multiupload-button-font-icon.png"));
-        }
-
-        impl.setButtonStyles(theme.get("cuba.web.WebFileMultiUploadField.button.style"));
-        impl.setButtonDisabledStyles(theme.get("cuba.web.WebFileMultiUploadField.button.disabled.style"));
-
-        impl.setBootstrapFailureHandler(new CubaMultiUpload.BootstrapFailureHandler() {
-            @Override
-            public void loadWebResourcesFailed() {
-                Messages messages = AppBeans.get(Messages.NAME);
-                String resourcesLoadFailed = messages.getMessage(WebFileMultiUploadField.class, "multiupload.resources.notLoaded");
-                WebWindowManager wm = App.getInstance().getWindowManager();
-                wm.showNotification(resourcesLoadFailed, Frame.NotificationType.ERROR);
-            }
-
-            @Override
-            public void flashNotInstalled() {
-                Messages messages = AppBeans.get(Messages.NAME);
-                String swfNotSupported = messages.getMessage(WebFileMultiUploadField.class, "multiupload.resources.swfNotSupported");
-                WebWindowManager wm = App.getInstance().getWindowManager();
-                wm.showNotification(swfNotSupported, Frame.NotificationType.ERROR);
-            }
-        });
-
-        impl.setReceiver((filename, mimeType) -> {
-            FileOutputStream outputStream;
-            try {
-                FileUploadingAPI.FileInfo fileInfo = fileUploading.createFile();
-                tempFileId = fileInfo.getId();
-                File tmpFile = fileInfo.getFile();
-                outputStream = new FileOutputStream(tmpFile);
-            } catch (Exception e) {
-                throw new RuntimeException("Unable to open stream for file uploading", e);
-            }
-            return outputStream;
-        });
-
-        impl.addUploadListener(new CubaMultiUpload.UploadListener() {
-            @Override
-            public void fileUploadStart(String fileName, long contentLength) {
-                fireFileUploadStart(fileName, contentLength);
-            }
-
-            @Override
-            public void fileUploaded(String fileName, long contentLength) {
-                files.put(tempFileId, fileName);
-
-                fireFileUploadFinish(fileName, contentLength);
-            }
-
-            @Override
-            public void queueUploadComplete() {
-                fireQueueUploadComplete();
-            }
-
-            @Override
-            public void errorNotify(String fileName, String message, CubaMultiUpload.UploadErrorType errorCode, long contentLength) {
-                LoggerFactory.getLogger(WebFileMultiUploadField.class)
-                        .warn("Error while uploading file '{}' with code '{}': {}", fileName, errorCode.getId(), message);
-
-                Messages messages = AppBeans.get(Messages.NAME);
-                WebWindowManager wm = App.getInstance().getWindowManager();
-                switch (errorCode) {
-                    case QUEUE_LIMIT_EXCEEDED:
-                        wm.showNotification(messages.getMessage(WebFileMultiUploadField.class, "multiupload.queueLimitExceed"),
-                                Frame.NotificationType.WARNING);
-                        break;
-                    case INVALID_FILETYPE:
-                        String invalidFiletypeMsg = messages.formatMainMessage("upload.fileIncorrectExtension.message", fileName);
-                        wm.showNotification(invalidFiletypeMsg, Frame.NotificationType.WARNING);
-                        break;
-                    case FILE_EXCEEDS_SIZE_LIMIT:
-                        String warningMsg = messages.formatMessage(WebFileMultiUploadField.class, "multiupload.filesizeLimitExceed", fileName, getFileSizeLimitString());
-                        wm.showNotification(warningMsg, Frame.NotificationType.WARNING);
-                        break;
-                    case SECURITY_ERROR:
-                        wm.showNotification(messages.getMessage(WebFileMultiUploadField.class, "multiupload.securityError"),
-                                Frame.NotificationType.WARNING);
-                        break;
-                    case ZERO_BYTE_FILE:
-                        wm.showNotification(messages.formatMessage(WebFileMultiUploadField.class, "multiupload.zerobyteFile", fileName),
-                                Frame.NotificationType.WARNING);
-                        break;
-                    default:
-                        String uploadError = messages.formatMessage(WebFileMultiUploadField.class, "multiupload.uploadError", fileName);
-                        wm.showNotification(uploadError, Frame.NotificationType.ERROR);
-
-                        fireFileUploadError(fileName, contentLength, new IOException("Upload error " + errorCode.name()));
-
-                        break;
-
-                }
-            }
-        });
-        impl.setDescription(null);
-
-        component = impl;
+        initComponent();
     }
 
     protected void initComponent() {
@@ -263,7 +122,7 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
             String warningMsg = messages.formatMessage(WebFileMultiUploadField.class, "multiupload.filesizeLimitExceed", e.getFileName(), getFileSizeLimitString());
             getFrame().showNotification(warningMsg, Frame.NotificationType.WARNING);
         });
-        impl.addFileExtensionNotAllowedListener(e ->{
+        impl.addFileExtensionNotAllowedListener(e -> {
             String warningMsg = messages.formatMainMessage("upload.fileIncorrectExtension.message", e.getFileName());
             getFrame().showNotification(warningMsg, Frame.NotificationType.WARNING);
         });
@@ -273,80 +132,6 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
 
     protected CubaFileUpload createComponent() {
         return new CubaFileUpload();
-    }
-
-    protected CubaMultiUpload createOldComponent() {
-        return new CubaMultiUpload();
-    }
-
-    @Deprecated
-    public int getButtonWidth() {
-        return ((CubaMultiUpload) component).getButtonWidth();
-    }
-
-    @Deprecated
-    public void setButtonWidth(int buttonWidth) {
-        ((CubaMultiUpload) component).setButtonWidth(buttonWidth);
-    }
-
-    @Deprecated
-    public int getButtonHeight() {
-        return ((CubaMultiUpload) component).getButtonHeight();
-    }
-
-    @Deprecated
-    public void setButtonHeight(int buttonHeight) {
-        ((CubaMultiUpload) component).setButtonHeight(buttonHeight);
-    }
-
-    @Deprecated
-    public String getFileTypesMask() {
-        return ((CubaMultiUpload) component).getFileTypesMask();
-    }
-
-    @Deprecated
-    public void setFileTypesMask(String fileTypesMask) {
-        ((CubaMultiUpload) component).setFileTypesMask(fileTypesMask);
-    }
-
-    @Deprecated
-    public String getFileTypesDescription() {
-        return ((CubaMultiUpload) component).getFileTypesDescription();
-    }
-
-    @Deprecated
-    public void setFileTypesDescription(String fileTypesDescription) {
-        ((CubaMultiUpload) component).setFileTypesDescription(fileTypesDescription);
-    }
-
-    @Deprecated
-    public double getFileSizeLimitMB() {
-        return ((CubaMultiUpload) component).getFileSizeLimitMB();
-    }
-
-    @Deprecated
-    public void setFileSizeLimitMB(double filesizeLimit) {
-        ((CubaMultiUpload) component).setFileSizeLimitMB(filesizeLimit);
-    }
-
-    @Deprecated
-    public double getQueueUploadLimitMB() {
-        return ((CubaMultiUpload) component).getQueueUploadLimitMB();
-    }
-
-    @Deprecated
-    public void setQueueUploadLimitMB(double queueUploadLimit) {
-        ((CubaMultiUpload) component).setQueueUploadLimitMB(queueUploadLimit);
-    }
-
-    @Deprecated
-    public int getQueueSizeLimit() {
-        return ((CubaMultiUpload) component).getQueueSizeLimit();
-    }
-
-    @Deprecated
-    public void setQueueSizeLimit(int queueSizeLimit) {
-        ((CubaMultiUpload) component).setQueueSizeLimit(queueSizeLimit);
     }
 
     @Override
@@ -406,9 +191,7 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
     public void setAccept(String accept) {
         if (!Objects.equals(accept, getAccept())) {
             this.accept = accept;
-            component.setAccept(component instanceof CubaMultiUpload
-                    ? FileUploadTypesHelper.convertSeparator(accept, ";")
-                    : FileUploadTypesHelper.convertToMIME(accept));
+            component.setAccept(FileUploadTypesHelper.convertToMIME(accept));
         }
     }
 
@@ -416,18 +199,16 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
     public void setDropZone(DropZone dropZone) {
         super.setDropZone(dropZone);
 
-        if (component instanceof CubaFileUpload) {
-            if (dropZone == null) {
-                ((CubaFileUpload) component).setDropZone(null);
-            } else {
-                com.haulmont.cuba.gui.components.Component target = dropZone.getTarget();
-                if (target instanceof Window.Wrapper) {
-                    target = ((Window.Wrapper) target).getWrappedWindow();
-                }
-
-                Component vComponent = target.unwrapComposition(Component.class);
-                ((CubaFileUpload) this.component).setDropZone(vComponent);
+        if (dropZone == null) {
+            component.setDropZone(null);
+        } else {
+            com.haulmont.cuba.gui.components.Component target = dropZone.getTarget();
+            if (target instanceof Window.Wrapper) {
+                target = ((Window.Wrapper) target).getWrappedWindow();
             }
+
+            Component vComponent = target.unwrapComposition(Component.class);
+            this.component.setDropZone(vComponent);
         }
     }
 
@@ -435,13 +216,11 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
     public void setPasteZone(Container pasteZone) {
         super.setPasteZone(pasteZone);
 
-        if (component instanceof CubaFileUpload) {
-            if (pasteZone == null) {
-                ((CubaFileUpload) component).setPasteZone(null);
-            } else {
-                Component vComponent = pasteZone.unwrapComposition(Component.class);
-                ((CubaFileUpload) component).setPasteZone(vComponent);
-            }
+        if (pasteZone == null) {
+            component.setPasteZone(null);
+        } else {
+            Component vComponent = pasteZone.unwrapComposition(Component.class);
+            component.setPasteZone(vComponent);
         }
     }
 
@@ -449,9 +228,7 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
     public void setDropZonePrompt(String dropZonePrompt) {
         super.setDropZonePrompt(dropZonePrompt);
 
-        if (component instanceof CubaFileUpload) {
-            ((CubaFileUpload) component).setDropZonePrompt(dropZonePrompt);
-        }
+        component.setDropZonePrompt(dropZonePrompt);
     }
 
     protected void fireFileUploadStart(String fileName, long contentLength) {
@@ -527,11 +304,9 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
     @Override
     public void setFileSizeLimit(long fileSizeLimit) {
         this.fileSizeLimit = fileSizeLimit;
-        if (this.component instanceof CubaFileUpload){
-            ((CubaFileUpload) this.component).setFileSizeLimit(fileSizeLimit);
-        } else if (this.component instanceof CubaMultiUpload) {
-            ((CubaMultiUpload) this.component).setFileSizeLimitMB((double) fileSizeLimit/BYTES_IN_MEGABYTE);
-        }
+
+        this.component.setFileSizeLimit(fileSizeLimit);
+
     }
 
     @Override
@@ -542,12 +317,7 @@ public class WebFileMultiUploadField extends WebAbstractUploadComponent<CubaAbst
             this.permittedExtensions = null;
         }
 
-        if (this.component instanceof CubaFileUpload){
-            ((CubaFileUpload) this.component).setPermittedExtensions(this.permittedExtensions);
-        } else if (this.component instanceof CubaMultiUpload) {
-            ((CubaMultiUpload) this.component).setPermittedExtensions(this.permittedExtensions);
-            ((CubaMultiUpload) this.component).setFileTypesDescription("");
-        }
+        this.component.setPermittedExtensions(this.permittedExtensions);
     }
 
     @Override
