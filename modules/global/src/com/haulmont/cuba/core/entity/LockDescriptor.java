cf026f8a90f3730cc1b100cce815ac2f3f844856
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-05-25 10:28:30 +0400

    Some platform entities have lost their createTs, createdBy attributes #769

--- modules/global/src/com/haulmont/cuba/core/entity/LockDescriptor.java
+++ modules/global/src/com/haulmont/cuba/core/entity/LockDescriptor.java
@@ -20,19 +20,46 @@ import com.haulmont.cuba.core.entity.annotation.SystemLevel;
 
 import javax.persistence.Column;
 import javax.persistence.Table;
+import java.util.Date;
 
 @javax.persistence.Entity(name = "sys$LockDescriptor")
 @Table(name = "SYS_LOCK_CONFIG")
 @SystemLevel
-public class LockDescriptor extends BaseUuidEntity {
+public class LockDescriptor extends BaseUuidEntity implements Creatable {
 
     private static final long serialVersionUID = -5798715368435824090L;
+
+    @Column(name = "CREATE_TS")
+    private Date createTs;
+
+    @Column(name = "CREATED_BY", length = 50)
+    private String createdBy;
 
     @Column(name = "NAME", length = 100, nullable = false)
     private String name;
 
     @Column(name = "TIMEOUT_SEC", nullable = false)
     private Integer timeoutSec;
+
+    @Override
+    public Date getCreateTs() {
+        return createTs;
+    }
+
+    @Override
+    public void setCreateTs(Date createTs) {
+        this.createTs = createTs;
+    }
+
+    @Override
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    @Override
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 
     public String getName() {
         return name;
