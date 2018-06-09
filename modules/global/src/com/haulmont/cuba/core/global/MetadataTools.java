80769059961e0c86f59240ed201b8dd2d9824888
Andrey Subbotin <subbotin@haulmont.com>
2018-06-09 10:03:49 +0400

    Handle EclipseLink validation exception while copy entity #855

--- modules/global/src/com/haulmont/cuba/core/global/MetadataTools.java
+++ modules/global/src/com/haulmont/cuba/core/global/MetadataTools.java
@@ -931,8 +931,9 @@ public class MetadataTools {
                     if (cause == null)
                         cause = e;
                     // ignore exception on copy for not loaded fields
-                    if (!(cause instanceof IllegalStateException))
+                    if (!isNotLoadedAttributeException(cause)) {
                         throw e;
+                    }
                 }
             }
         }
@@ -1124,6 +1125,12 @@ public class MetadataTools {
             ((BaseGenericIdEntity) entity).setId(id);
         }
         return entity;
+    }
+
+    private boolean isNotLoadedAttributeException(Throwable e) {
+        return e instanceof IllegalStateException
+                || e instanceof org.eclipse.persistence.exceptions.ValidationException && e.getMessage() != null
+                && e.getMessage().contains("An attempt was made to traverse a relationship using indirection that had a null Session");
     }
 
     /**
