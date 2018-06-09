6f71beddfb64eb345e8faa43b7ea680557d4cd84
Alexander Shustanov <shustanov@haulmont.com>
2018-06-09 10:36:16 +0400

    Change the login of anonymous user to system #633

---
+++ modules/core/db/update/oracle/18/180609-renameAnonymousToSystem.sql
@@ -0,0 +1,7 @@
+update SEC_USER
+set LOGIN='system', LOGIN_LC='system', NAME='System'
+where id = 'a405db59-e674-4f63-8afe-269dda788fe8'^
+
+update SEC_GROUP
+set NAME='System'
+where id='cd541dd4-eeb7-cd5b-847e-d32236552fa9'^
--- modules/core/src/com/haulmont/cuba/core/app/ServerConfig.java
+++ modules/core/src/com/haulmont/cuba/core/app/ServerConfig.java
@@ -245,7 +245,7 @@ public interface ServerConfig extends Config {
      */
     @Property("cuba.anonymousLogin")
     @Source(type = SourceType.DATABASE)
-    @Default("anonymous")
+    @Default("system")
     String getAnonymousLogin();
 
     /**
