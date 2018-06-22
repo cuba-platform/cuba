5a15699e046de8a16f29bfb71408df7e9cf08b2c
Yuriy Artamonov <artamonov@haulmont.com>
2018-06-22 19:10:45 +0400

    If window is opened as THIS_TAB WindowManager does not register it in windows map #996

--- modules/gui/src/com/haulmont/cuba/gui/WindowManager.java
+++ modules/gui/src/com/haulmont/cuba/gui/WindowManager.java
@@ -771,6 +771,11 @@ public abstract class WindowManager {
     }
 
     protected boolean isOpenAsNewTab(OpenType openType) {
+        if (getOpenWindows().isEmpty()
+                && openType.getOpenMode() == OpenMode.THIS_TAB) {
+            return true;
+        }
+
         return openType.getOpenMode() == OpenMode.NEW_TAB;
     }
 
