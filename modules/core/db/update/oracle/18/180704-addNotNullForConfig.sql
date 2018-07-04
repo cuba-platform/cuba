f5d9b919746f2e42d29fe4be755a69354540e854
Yuriy Artamonov <artamonov@haulmont.com>
2018-07-04 20:03:02 +0400

    NPE occurs when some db-stored application property has null value #1016
    Fix Oracle update scripts, handle exception if already not null

--- modules/core/db/update/oracle/18/180704-addNotNullForConfig.sql
+++ modules/core/db/update/oracle/18/180704-addNotNullForConfig.sql
@@ -1,12 +1,9 @@
 -- add not null constraints for SYS_CONFIG
 
-DECLARE
-   allready_null EXCEPTION;
-   PRAGMA EXCEPTION_INIT(allready_null, -1451);
-BEGIN
+begin
    execute immediate 'alter table SYS_CONFIG modify NAME varchar2(255) not null';
    execute immediate 'alter table SYS_CONFIG modify VALUE_ clob not null';
-EXCEPTION
-   WHEN allready_null THEN
-      null; -- handle the error
-END;^
+exception
+end;
+/
+^
