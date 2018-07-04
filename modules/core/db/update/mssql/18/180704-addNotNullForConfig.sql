2946e9abd50f41711383c0ae89c035fd48c88321
Yuriy Artamonov <artamonov@haulmont.com>
2018-07-04 20:33:29 +0400

    NPE occurs when some db-stored application property has null value #1016
    Fix MS SQL scripts

--- modules/core/db/update/mssql/18/180704-addNotNullForConfig.sql
+++ modules/core/db/update/mssql/18/180704-addNotNullForConfig.sql
@@ -2,3 +2,6 @@
 
 alter table SYS_CONFIG alter column NAME varchar(255) not null;
 alter table SYS_CONFIG alter column VALUE_ varchar(max) not null;
+
+drop constraint IDX_SYS_CONFIG_UNIQ_NAME;
+alter table SYS_CONFIG add constraint IDX_SYS_CONFIG_UNIQ_NAME unique (NAME);
