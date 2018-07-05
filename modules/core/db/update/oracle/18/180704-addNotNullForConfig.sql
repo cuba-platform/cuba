9b38549bf0dc63f7de43aa122724a3d13b293f5b
Yuriy Artamonov <artamonov@haulmont.com>
2018-07-05 12:41:37 +0400

    NPE occurs when some db-stored application property has null value #1016
    Fix Oracle scripts

--- modules/core/db/update/oracle/18/180704-addNotNullForConfig.sql
+++ modules/core/db/update/oracle/18/180704-addNotNullForConfig.sql
@@ -1,9 +1,15 @@
 -- add not null constraints for SYS_CONFIG
 
+drop index IDX_SYS_CONFIG_UNIQ_NAME^
+
 begin
-   execute immediate 'alter table SYS_CONFIG modify NAME varchar2(255) not null';
-   execute immediate 'alter table SYS_CONFIG modify VALUE_ clob not null';
+   execute immediate 'alter table SYS_CONFIG modify NAME not null';
+   execute immediate 'alter table SYS_CONFIG modify VALUE_ not null';
 exception
+    when others then
+        null;
 end;
 /
 ^
+
+create index IDX_SYS_CONFIG_UNIQ_NAME on SYS_CONFIG (NAME)^
