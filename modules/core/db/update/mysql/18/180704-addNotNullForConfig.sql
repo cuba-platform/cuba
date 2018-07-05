a100ef513f95c3e4e038c9efeeec7c45ad8c3883
Yuriy Artamonov <artamonov@haulmont.com>
2018-07-05 13:52:53 +0400

    NPE occurs when some db-stored application property has null value #1016
    Fix MySQL scripts

--- modules/core/db/update/mysql/18/180704-addNotNullForConfig.sql
+++ modules/core/db/update/mysql/18/180704-addNotNullForConfig.sql
@@ -1,4 +1,4 @@
 -- add not null constraints for SYS_CONFIG
 
-alter table SYS_CONFIG alter column NAME(190) not null;
-alter table SYS_CONFIG alter column VALUE_(text) not null;
+alter table SYS_CONFIG MODIFY NAME varchar(190) not null;
+alter table SYS_CONFIG MODIFY VALUE_ text not null;
