846d143f8c3f1601ff3bcd61f1cc9693c63c5bc8
Daniil Tsarev <tsarev@haulmont.com>
2018-08-06 14:44:57 +0400

    GroupTable columns are misaligned in case of aggregatable table #684

--- modules/web-toolkit/src/com/haulmont/cuba/web/widgets/client/tableshared/TableWidgetDelegate.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/widgets/client/tableshared/TableWidgetDelegate.java
@@ -140,13 +140,24 @@ public class TableWidgetDelegate {
                 if (realColWidth > 0) {
                     if (realColWidth > minWidth) {
                         Style hStyle = hcell.getElement().getStyle();
-
                         hStyle.setProperty("width", realColWidth + "px");
                         hStyle.setProperty("minWidth", realColWidth + "px");
                         hStyle.setProperty("maxWidth", realColWidth + "px");
                     }
-
                     break;
+                }
+            }
+        }
+
+        if (aggregationRow != null && aggregationRow.isInitialized()) {
+            Widget twRow = aggregationRow.getTableWidget().getRenderedRows().get(0);
+            VScrollTable.VScrollTableBody.VScrollTableRow vRow = (VScrollTable.VScrollTableBody.VScrollTableRow) twRow;
+
+            double realColWidth = vRow.getRealCellWidth(colIndex);
+            if (realColWidth > 0) {
+                if (realColWidth > minWidth) {
+                    Style hStyle = hcell.getElement().getStyle();
+                    hStyle.setProperty("width", realColWidth + "px");
                 }
             }
         }
