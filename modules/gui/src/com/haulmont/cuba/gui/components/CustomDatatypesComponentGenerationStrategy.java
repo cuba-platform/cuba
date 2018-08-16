995e9f6f9500895f36c62cacd9f7c4000004da6e
Gleb Gorelov <gorelov@haulmont.com>
2018-08-16 11:50:52 +0400

    Implement an independent CustomDatatypesComponentGenerationStrategy #1154

---
+++ modules/gui/src/com/haulmont/cuba/gui/components/CustomDatatypesComponentGenerationStrategy.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2008-2018 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.cuba.gui.components;
+
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.chile.core.model.MetaPropertyPath;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
+import org.springframework.core.Ordered;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+/**
+ * This generation strategy is intended for generating a default field corresponding to a datatype for which other
+ * generation strategies didn't create a field. For instance, when a datatype is custom and doesn't match any type
+ * for which {@link DefaultComponentGenerationStrategy} creates fields.
+ */
+@org.springframework.stereotype.Component(CustomDatatypesComponentGenerationStrategy.NAME)
+public class CustomDatatypesComponentGenerationStrategy extends AbstractComponentGenerationStrategy implements Ordered {
+
+    public static final String NAME = "cuba_CustomDatatypesComponentGenerationStrategy";
+
+    @Inject
+    public CustomDatatypesComponentGenerationStrategy(Messages messages) {
+        super(messages);
+    }
+
+    @Inject
+    public void setComponentsFactory(ComponentsFactory componentsFactory) {
+        this.componentsFactory = componentsFactory;
+    }
+
+    @Nullable
+    @Override
+    public Component createComponent(ComponentGenerationContext context) {
+        MetaClass metaClass = context.getMetaClass();
+        MetaPropertyPath mpp = resolveMetaPropertyPath(metaClass, context.getProperty());
+
+        if (mpp != null
+                && mpp.getRange().isDatatype()) {
+            return createCustomDatatypeField(context, mpp);
+        }
+
+        return null;
+    }
+
+    protected Component createCustomDatatypeField(ComponentGenerationContext context, MetaPropertyPath mpp) {
+        return createStringField(context, mpp);
+    }
+
+    @Override
+    public int getOrder() {
+        return Integer.MAX_VALUE;
+    }
+}
