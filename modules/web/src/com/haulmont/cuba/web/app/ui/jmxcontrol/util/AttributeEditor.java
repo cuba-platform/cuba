f62be82f15af961c8e4abc2e7dffe239f566202e
Yuriy Artamonov <artamonov@haulmont.com>
2018-09-18 12:11:21 +0400

    JMX AttributeEditor uses concrete UI component classes #1278

--- modules/web/src/com/haulmont/cuba/web/app/ui/jmxcontrol/util/AttributeEditor.java
+++ modules/web/src/com/haulmont/cuba/web/app/ui/jmxcontrol/util/AttributeEditor.java
@@ -19,12 +19,12 @@ package com.haulmont.cuba.web.app.ui.jmxcontrol.util;
 
 import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.Messages;
-import com.haulmont.cuba.gui.AppConfig;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.BaseAction;
+import com.haulmont.cuba.gui.icons.CubaIcon;
 import com.haulmont.cuba.gui.theme.ThemeConstants;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 import com.haulmont.cuba.web.App;
-import com.haulmont.cuba.web.gui.components.*;
 import com.haulmont.cuba.web.jmx.entity.AttributeHelper;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
@@ -50,7 +50,7 @@ public class AttributeEditor {
     public AttributeEditor(Frame frame, String type, Object value, boolean requestFocus, boolean isFixedSize) {
         this.type = type;
         if (AttributeHelper.isBoolean(type)) {
-            checkBox = componentsFactory.createComponent(WebCheckBox.class);
+            checkBox = componentsFactory.createComponent(CheckBox.class);
             checkBox.setFrame(frame);
             if (requestFocus) {
                 checkBox.requestFocus();
@@ -62,14 +62,14 @@ public class AttributeEditor {
         } else if (AttributeHelper.isArrayOrCollection(type)) {
             initArrayLayout(value, isFixedSize, AttributeHelper.isObjectArrayOrCollection(type));
         } else if (AttributeHelper.isDate(type)) {
-            dateField = AppConfig.getFactory().createComponent(DateField.class);
+            dateField = componentsFactory.createComponent(DateField.class);
             dateField.setWidth("500px");
             dateField.setFrame(frame);
             if (value != null) {
                 dateField.setValue(value.toString());
             }
         } else {
-            textField = componentsFactory.createComponent(WebTextField.class);
+            textField = componentsFactory.createComponent(TextField.class);
 
             ThemeConstants theme = App.getInstance().getThemeConstants();
             textField.setWidth(theme.get("cuba.web.jmx.AttributeEditor.textField.width"));
@@ -86,7 +86,7 @@ public class AttributeEditor {
 
     protected void initArrayLayout(Object value, boolean isFixedSize, boolean isReadOnly) {
 
-        layout = componentsFactory.createComponent(WebVBoxLayout.class);
+        layout = componentsFactory.createComponent(VBoxLayout.class);
         layout.setSpacing(true);
         ThemeConstants theme = App.getInstance().getThemeConstants();
         layout.setWidth(theme.get("cuba.web.jmx.AttributeEditor.arrayLayout.width"));
@@ -94,33 +94,26 @@ public class AttributeEditor {
             layout.setHeight(theme.get("cuba.web.jmx.AttributeEditor.arrayLayout.height"));
         }
 
-        Button btnAdd = componentsFactory.createComponent(WebButton.class);
-        btnAdd.setIcon("icons/plus-btn.png");
+        Button btnAdd = componentsFactory.createComponent(Button.class);
+        btnAdd.setIconFromSet(CubaIcon.PLUS_CIRCLE);
         btnAdd.setDescription(messages.getMessage(getClass(), "editAttribute.array.btnAdd"));
         layout.add(btnAdd);
 
-        ScrollBoxLayout scrollBoxLayout = componentsFactory.createComponent(WebScrollBoxLayout.class);
-        scrollBoxLayout.setWidth("100%");
+        ScrollBoxLayout scrollBoxLayout = componentsFactory.createComponent(ScrollBoxLayout.class);
+        scrollBoxLayout.setWidthFull();
         scrollBoxLayout.setSpacing(true);
+
         layout.add(scrollBoxLayout);
         layout.expand(scrollBoxLayout);
 
-        btnAdd.setAction(new AbstractAction("addRow") {
-            @Override
-            public void actionPerform(Component component) {
-                addRow(null, scrollBoxLayout, false);
-            }
+        AbstractAction addRowAction = new BaseAction("addRow")
+                .withCaption("")
+                .withHandler(actionPerformedEvent ->
+                        addRow(null, scrollBoxLayout, false)
+                );
 
-            @Override
-            public String getCaption() {
-                return "";
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return !isReadOnly;
-            }
-        });
+        addRowAction.setEnabled(!isReadOnly);
+        btnAdd.setAction(addRowAction);
 
         if (value != null) {
             List values = objectToStringArray(value);
@@ -131,35 +124,29 @@ public class AttributeEditor {
     }
 
     protected void addRow(Object value, Component.Container parent, boolean isReadOnly) {
-        BoxLayout row = componentsFactory.createComponent(WebHBoxLayout.class);
+        BoxLayout row = componentsFactory.createComponent(HBoxLayout.class);
         row.setSpacing(true);
-        row.setWidth("100%");
+        row.setWidthFull();
 
-        TextField valueField = componentsFactory.createComponent(WebTextField.class);
+        TextField valueField = componentsFactory.createComponent(TextField.class);
         valueField.setValue(value);
         valueField.setEditable(!isReadOnly);
         row.add(valueField);
         row.expand(valueField);
 
-        Button btnRemove = componentsFactory.createComponent(WebButton.class);
-        btnRemove.setIcon("icons/remove.png");
+        Button btnRemove = componentsFactory.createComponent(Button.class);
+        btnRemove.setIconFromSet(CubaIcon.TIMES);
         btnRemove.setDescription(messages.getMessage(getClass(), "editAttribute.array.btnRemove"));
-        btnRemove.setAction(new AbstractAction("removeRow") {
-            @Override
-            public void actionPerform(Component component) {
-                parent.remove(row);
-            }
 
-            @Override
-            public String getCaption() {
-                return "";
-            }
+        Action removeRowAction = new BaseAction("removeRow")
+                .withCaption("")
+                .withHandler(actionPerformedEvent ->
+                        parent.remove(row)
+                );
 
-            @Override
-            public boolean isEnabled() {
-                return !isReadOnly;
-            }
-        });
+        removeRowAction.setEnabled(!isReadOnly);
+
+        btnRemove.setAction(removeRowAction);
         row.add(btnRemove);
 
         parent.add(row);
