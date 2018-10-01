b60e1e0370051aa868ada93aa94208d10c9f8a37
Yuriy Artamonov <artamonov@haulmont.com>
2018-10-01 20:11:49 +0400

    Accordion shows Tab caption inside of lazy tab content #1281

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebAccordion.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebAccordion.java
@@ -543,6 +543,13 @@ public class WebAccordion extends WebAbstractComponent<CubaAccordion> implements
 
                 loader.loadComponent();
 
+                // do not show icon/caption in layout, it is used in Tab only
+                WebAbstractComponent contentComponent = (WebAbstractComponent) lazyContent;
+
+                contentComponent.setIcon(null);
+                contentComponent.setCaption(null);
+                contentComponent.setDescription(null);
+
                 Window window = ComponentsHelper.getWindow(WebAccordion.this);
                 if (window != null) {
                     walkComponents(tabContent, (settingsComponent, name) -> {
