906646e2e29bebce6de710bfe1327f80d99dd1c2
Roman Pinyazhin <pinyazhin@haulmont.com>
2018-10-12 14:11:19 +0400

    BulkEditor is always disabled #1393

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebBulkEditor.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebBulkEditor.java
@@ -201,13 +201,4 @@ public class WebBulkEditor extends WebButton implements BulkEditor {
     public boolean getUseConfirmDialog() {
         return useConfirmDialog;
     }
-
-    @Override
-    public void setEnabled(boolean enabled) {
-        super.setEnabled(enabled);
-
-        if (bulkEditAction != null) {
-            bulkEditAction.setEnabled(enabled);
-        }
-    }
 }
