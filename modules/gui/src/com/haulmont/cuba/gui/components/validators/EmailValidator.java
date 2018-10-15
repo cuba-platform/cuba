8f07d26973678e59241d1ceb8d8c20a4bf838a85
Daniil Tsarev <tsarev@haulmont.com>
2018-10-15 11:26:11 +0400

    Email local part validation problems #1231

--- modules/gui/src/com/haulmont/cuba/gui/components/validators/EmailValidator.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/validators/EmailValidator.java
@@ -16,31 +16,120 @@
  */
 package com.haulmont.cuba.gui.components.validators;
 
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.BeanValidation;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.components.Field;
 import com.haulmont.cuba.gui.components.ValidationException;
 import org.dom4j.Element;
+import org.hibernate.validator.constraints.Email;
 
-public class EmailValidator extends PatternValidator {
+import javax.validation.Validator;
+import java.util.ArrayList;
+import java.util.List;
 
-    private static String EMAIL_NAME = "([a-zA-Z[0-9]][a-zA-Z[0-9]\u005F\u002E\u002D]*[a-z||A-Z||0-9]|[a-zA-Z[0-9]])";
-    private static String EMAIL_DOMAIN = "((?:[a-zA-Z0-9а-яА-Я](?>\u002D[a-zA-Z0-9а-яА-Я]|[a-zA-Z0-9а-яА-Я])*+\\.?)+)";
-    private static String EMAIL_DOMAIN_ZONE = "([a-zA-Z[0-9]]{2,8})";
+public class EmailValidator implements Field.Validator {
 
-    public static final String EMAIL_PATTERN = EMAIL_NAME + "@" + EMAIL_DOMAIN + "\\." + EMAIL_DOMAIN_ZONE;
-    public static final String MULTI_EMAIL_PATTERN = EMAIL_PATTERN + "([;,]+[\\s]*" + EMAIL_PATTERN + ")*";
+    protected String message;
+    protected String messagesPack;
+    protected Messages messages;
+
+    protected Validator validator;
+
+    public EmailValidator() {
+        messages = AppBeans.get(Messages.class);
+        validator = AppBeans.get(BeanValidation.class)
+                .getValidator();
+    }
 
     public EmailValidator(Element element, String messagesPack) {
-        super(MULTI_EMAIL_PATTERN);
+        this();
         message = element.attributeValue("message");
         this.messagesPack = messagesPack;
     }
 
-    public EmailValidator() {
-        super(MULTI_EMAIL_PATTERN);
+    /**
+     * INTERNAL. Used in tests.
+     */
+    protected EmailValidator(Messages messages, Validator validator) {
+        this.messages = messages;
+        this.validator = validator;
     }
 
     @Override
     public void validate(Object value) throws ValidationException {
-        if (value != null)
-            super.validate(value);
+        if (value == null) {
+            return;
+        }
+
+        List<String> emails = collectEmails((String) value);
+        if (emails.isEmpty()) {
+            return;
+        }
+
+        for (String email : emails) {
+            boolean valid = validator.validateValue(EmailValidationPojo.class, "email", email).isEmpty();
+            if (!valid) {
+                String msg = message != null ?
+                        messages.getTools().loadString(messagesPack, message)
+                        : null;
+
+                if (msg == null) {
+                    msg = messages.getMainMessage("validation.invalidEmail");
+                }
+
+                throw new ValidationException(String.format(msg, value));
+            }
+        }
+    }
+
+    protected List<String> collectEmails(String emailString) {
+        List<String> emails = new ArrayList<>();
+
+        int sepIdx = getSepIdx(emailString);
+        while (sepIdx > 0) {
+            String email = emailString.substring(0, sepIdx).trim();
+            emails.add(preventEmpty(email));
+
+            emailString = emailString.substring(sepIdx + 1);
+            sepIdx = getSepIdx(emailString);
+        }
+
+        emails.add(emailString.trim());
+
+        return emails;
+    }
+
+    protected String preventEmpty(String s) {
+        // make validator fall on blank emails for rejecting trailing separators
+        return s.isEmpty() ? " " : s;
+    }
+
+    protected int getSepIdx(String emailString) {
+        int semicolonIdx = emailString.indexOf(';');
+        int commaIdx = emailString.indexOf(',');
+
+        if (semicolonIdx < 0 && commaIdx < 0) {
+            return -1;
+        }
+        if (semicolonIdx >= 0 && commaIdx >= 0) {
+            return Math.min(semicolonIdx, commaIdx);
+        }
+
+        return semicolonIdx != -1 ? semicolonIdx : commaIdx;
+    }
+
+    protected static class EmailValidationPojo {
+
+        @Email
+        private final String email;
+
+        public EmailValidationPojo(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
     }
 }
