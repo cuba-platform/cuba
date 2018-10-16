010002895d5795644d5d23fb8cba563e690cb9ed
Rushan <rushan.developer@gmail.com>
2018-10-16 10:12:51 +0400

    Error when merging new entity #1405
    Fixed issue that occurred while merging new entity referenced to new entity that is instance of subclass of original attribute class

--- modules/core/src/com/haulmont/cuba/core/sys/EntityManagerImpl.java
+++ modules/core/src/com/haulmont/cuba/core/sys/EntityManagerImpl.java
@@ -376,7 +376,7 @@ public class EntityManagerImpl implements EntityManager {
                     if (!equal) {
                         dstCollection.clear();
                         for (Entity srcRef : srcCollection) {
-                            Entity reloadedRef = findOrCreate(refClass, srcRef.getId());
+                            Entity reloadedRef = findOrCreate(srcRef.getClass(), srcRef.getId());
                             dstCollection.add(reloadedRef);
                             deepCopyIgnoringNulls(srcRef, reloadedRef, visited);
                         }
@@ -387,7 +387,7 @@ public class EntityManagerImpl implements EntityManager {
                     if (srcRef.equals(destRef)) {
                         deepCopyIgnoringNulls(srcRef, destRef, visited);
                     } else {
-                        Entity reloadedRef = findOrCreate(refClass, srcRef.getId());
+                        Entity reloadedRef = findOrCreate(srcRef.getClass(), srcRef.getId());
                         dest.setValue(name, reloadedRef);
                         deepCopyIgnoringNulls(srcRef, reloadedRef, visited);
                     }
