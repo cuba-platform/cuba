fd7f1db64495fae513bc70a5cc5b727719176f09
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-12-27 17:07:16 +0400

    Tests on entity listeners which demonstrate entity states

--- modules/core/test/com/haulmont/cuba/core/listener/TestUserEntityListener.java
+++ modules/core/test/com/haulmont/cuba/core/listener/TestUserEntityListener.java
@@ -21,7 +21,10 @@ import com.haulmont.cuba.security.entity.User;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class TestUserEntityListener implements
         BeforeInsertEntityListener<User>,
@@ -33,33 +36,53 @@ public class TestUserEntityListener implements
 
     public static final List<String> events = new ArrayList<>();
 
+    public static Map<String, Consumer<User>> consumers = new HashMap<>();
+
     @Override
     public void onBeforeInsert(User entity, EntityManager entityManager) {
         events.add("onBeforeInsert: " + entity.getId());
+        if (consumers.get("BeforeInsert") != null) {
+            consumers.get("BeforeInsert").accept(entity);
+        }
     }
 
     @Override
     public void onBeforeUpdate(User entity, EntityManager entityManager) {
         events.add("onBeforeUpdate: " + entity.getId());
+        if (consumers.get("BeforeUpdate") != null) {
+            consumers.get("BeforeUpdate").accept(entity);
+        }
     }
 
     @Override
     public void onBeforeDelete(User entity, EntityManager entityManager) {
         events.add("onBeforeDelete: " + entity.getId());
+        if (consumers.get("BeforeDelete") != null) {
+            consumers.get("BeforeDelete").accept(entity);
+        }
     }
 
     @Override
     public void onAfterInsert(User entity, Connection connection) {
         events.add("onAfterInsert: " + entity.getId());
+        if (consumers.get("AfterInsert") != null) {
+            consumers.get("AfterInsert").accept(entity);
+        }
     }
 
     @Override
     public void onAfterUpdate(User entity, Connection connection) {
         events.add("onAfterUpdate: " + entity.getId());
+        if (consumers.get("AfterUpdate") != null) {
+            consumers.get("AfterUpdate").accept(entity);
+        }
     }
 
     @Override
     public void onAfterDelete(User entity, Connection connection) {
         events.add("onAfterDelete: " + entity.getId());
+        if (consumers.get("AfterDelete") != null) {
+            consumers.get("AfterDelete").accept(entity);
+        }
     }
 }
