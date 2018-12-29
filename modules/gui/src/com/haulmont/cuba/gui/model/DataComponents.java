0a6add267b46ecbc91dcabda43510632e5cb752f
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-12-29 15:09:50 +0400

    PropertyContainer should be empty if the property is unavailable by security constraints #1732

--- modules/gui/src/com/haulmont/cuba/gui/model/DataComponents.java
+++ modules/gui/src/com/haulmont/cuba/gui/model/DataComponents.java
@@ -18,7 +18,9 @@ package com.haulmont.cuba.gui.model;
 
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.core.global.Security;
 import com.haulmont.cuba.gui.model.impl.*;
+import com.haulmont.cuba.security.entity.EntityOp;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -36,6 +38,9 @@ public class DataComponents implements ApplicationContextAware {
 
     @Inject
     protected Metadata metadata;
+
+    @Inject
+    protected Security security;
 
     private ApplicationContext applicationContext;
 
@@ -68,16 +73,19 @@ public class DataComponents implements ApplicationContextAware {
         InstancePropertyContainerImpl<E> container = new InstancePropertyContainerImpl<>(
                 metadata.getClassNN(entityClass), masterContainer, property);
 
-        masterContainer.addItemChangeListener(e -> {
-            Entity item = masterContainer.getItemOrNull();
-            container.setItem(item != null ? item.getValue(property) : null);
-        });
+        if (security.isEntityAttrReadPermitted(masterContainer.getEntityMetaClass(), property)
+                && security.isEntityOpPermitted(entityClass, EntityOp.READ)) {
+            masterContainer.addItemChangeListener(e -> {
+                Entity item = masterContainer.getItemOrNull();
+                container.setItem(item != null ? item.getValue(property) : null);
+            });
 
-        masterContainer.addItemPropertyChangeListener(e -> {
-            if (e.getProperty().equals(property)) {
-                container.setItem((E) e.getValue());
-            }
-        });
+            masterContainer.addItemPropertyChangeListener(e -> {
+                if (e.getProperty().equals(property)) {
+                    container.setItem((E) e.getValue());
+                }
+            });
+        }
 
         return container;
     }
@@ -99,16 +107,19 @@ public class DataComponents implements ApplicationContextAware {
         CollectionPropertyContainerImpl<E> container = new CollectionPropertyContainerImpl<>(
                 metadata.getClassNN(entityClass), masterContainer, property);
 
-        masterContainer.addItemChangeListener(e -> {
-            Entity item = masterContainer.getItemOrNull();
-            container.setItems(item != null ? item.getValue(property) : null);
-        });
+        if (security.isEntityAttrReadPermitted(masterContainer.getEntityMetaClass(), property)
+                && security.isEntityOpPermitted(entityClass, EntityOp.READ)) {
+            masterContainer.addItemChangeListener(e -> {
+                Entity item = masterContainer.getItemOrNull();
+                container.setItems(item != null ? item.getValue(property) : null);
+            });
 
-        masterContainer.addItemPropertyChangeListener(e -> {
-            if (e.getProperty().equals(property)) {
-                container.setDisconnectedItems((Collection<E>) e.getValue());
-            }
-        });
+            masterContainer.addItemPropertyChangeListener(e -> {
+                if (e.getProperty().equals(property)) {
+                    container.setDisconnectedItems((Collection<E>) e.getValue());
+                }
+            });
+        }
 
         return container;
     }
