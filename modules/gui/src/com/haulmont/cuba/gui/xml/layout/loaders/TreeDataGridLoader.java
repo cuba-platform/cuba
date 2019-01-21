01f74d245ddd54da897e096f0b418685eb05a94e
Gleb Gorelov <gorelov@haulmont.com>
2019-01-21 14:11:17 +0400

    Add TreeDataGrid 'hierarchyColumn' property to XSD #1792

--- modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/TreeDataGridLoader.java
+++ modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/TreeDataGridLoader.java
@@ -2,16 +2,32 @@ package com.haulmont.cuba.gui.xml.layout.loaders;
 
 import com.google.common.base.Strings;
 import com.haulmont.cuba.gui.GuiDevelopmentException;
+import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.TreeDataGrid;
 import com.haulmont.cuba.gui.components.data.DataGridItems;
 import com.haulmont.cuba.gui.components.data.datagrid.ContainerTreeDataGridItems;
 import com.haulmont.cuba.gui.model.CollectionContainer;
+import org.dom4j.Element;
 
 public class TreeDataGridLoader extends AbstractDataGridLoader<TreeDataGrid> {
 
     @Override
     protected TreeDataGrid createComponentInternal() {
         return factory.create(TreeDataGrid.NAME);
+    }
+
+    @Override
+    public void loadComponent() {
+        super.loadComponent();
+
+        loadHierarchyColumn(resultComponent, element);
+    }
+
+    protected void loadHierarchyColumn(TreeDataGrid component, Element element) {
+        String hierarchyColumn = element.attributeValue("hierarchyColumn");
+        if (!Strings.isNullOrEmpty(hierarchyColumn)) {
+            component.setHierarchyColumn(hierarchyColumn);
+        }
     }
 
     @SuppressWarnings("unchecked")
