0b3dedcba1978a0f88eb1cf66779fc208b2f0d91
Andrey Subbotin <subbotin@haulmont.com>
2019-01-30 17:04:24 +0400

    Unable to use attribute name "set" in entity #754

---
+++ modules/core/test/com/haulmont/cuba/core/global/QueryParserAstKeywordsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2008-2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.cuba.core.global;
+
+import com.haulmont.cuba.core.sys.jpql.DomainModel;
+import com.haulmont.cuba.core.sys.jpql.model.EntityBuilder;
+import com.haulmont.cuba.core.sys.jpql.model.JpqlEntityModel;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QueryParserAstKeywordsTest {
+    @Test
+    public void testSetKeywordEntity() {
+        DomainModel model = prepareDomainModel();
+        QueryParserAstBased parser = new QueryParserAstBased(model,
+                "select e from test_Entity e where e.set = :par"
+        );
+        Assert.assertEquals("test_Entity", parser.getEntityName());
+    }
+
+    private DomainModel prepareDomainModel() {
+        EntityBuilder builder = new EntityBuilder();
+        builder.startNewEntity("test_Entity");
+        builder.addStringAttribute("set");
+        builder.addStringAttribute("createdBy");
+        JpqlEntityModel testEntity = builder.produce();
+
+        return new DomainModel(testEntity);
+    }
+}
