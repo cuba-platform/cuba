097f2a4c5b5445078264fb785c279606606ba217
Yuriy Artamonov <artamonov@haulmont.com>
2019-03-28 18:18:04 +0400

    Permanent XSS attack through GroupBoxLayout caption #1993

--- modules/web-toolkit/src/com/haulmont/cuba/web/widgets/client/groupbox/CubaGroupBoxConnector.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/widgets/client/groupbox/CubaGroupBoxConnector.java
@@ -73,6 +73,11 @@ public class CubaGroupBoxConnector extends PanelConnector {
 
     @Override
     public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+        if (isRealUpdate(uidl)) {
+            // set captionAsHtml before caption update in super.updateFromUIDL(uidl, client);
+            getWidget().setCaptionAsHtml(getState().captionAsHtml);
+        }
+
         super.updateFromUIDL(uidl, client);
 
         if (!getState().showAsPanel) {
