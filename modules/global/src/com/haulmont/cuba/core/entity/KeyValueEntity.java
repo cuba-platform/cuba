6c36a05e3e305b7baad623795df66ed85359087c
Artem Chizhikov <chihzikov@haulmont.com>
2019-07-30 17:44:12 +0300

    KeyValueCollectionContainer enum support #2297

--- modules/global/src/com/haulmont/cuba/core/entity/KeyValueEntity.java
+++ modules/global/src/com/haulmont/cuba/core/entity/KeyValueEntity.java
@@ -17,13 +17,16 @@
 
 package com.haulmont.cuba.core.entity;
 
+import com.haulmont.chile.core.datatypes.Enumeration;
 import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.chile.core.model.MetaProperty;
 import com.haulmont.chile.core.model.impl.AbstractInstance;
 import com.haulmont.chile.core.model.utils.InstanceUtils;
 import com.haulmont.cuba.core.entity.annotation.SystemLevel;
 import com.haulmont.cuba.core.global.UuidProvider;
 import com.haulmont.cuba.core.sys.CubaEnhancingDisabled;
 
+import java.text.ParseException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -91,6 +94,21 @@ public class KeyValueEntity
 
     @Override
     public <T> T getValue(String name) {
+        if (metaClass != null) {
+            MetaProperty metaProperty = metaClass.getProperty(name);
+            if (metaProperty != null && metaProperty.getRange().isEnum()) {
+                Enumeration enumeration = metaProperty.getRange().asEnumeration();
+                Object enumValue = properties.get(name);
+                if (enumValue != null) {
+                    try {
+                        //noinspection unchecked
+                        return (T) enumeration.parse(enumValue.toString());
+                    } catch (ParseException e) {
+                        throw new RuntimeException("Can't parse enum value stored in the database", e);
+                    }
+                }
+            }
+        }
         //noinspection unchecked
         return (T) properties.get(name);
     }
