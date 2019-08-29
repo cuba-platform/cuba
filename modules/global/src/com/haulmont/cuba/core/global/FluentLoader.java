2249527a10804f753a9e99a26f1d3bf91c77e9fa
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-08-29 18:50:22 +0400

    ViewBuilder - fix Javadocs #2393

--- modules/global/src/com/haulmont/cuba/core/global/FluentLoader.java
+++ modules/global/src/com/haulmont/cuba/core/global/FluentLoader.java
@@ -137,7 +137,7 @@ public class FluentLoader<E extends Entity<K>, K> {
      * <pre>
      *     dataManager.load(Pet.class)
      *         .id(petId)
-     *         .view(viewBuilder -> viewBuilder.addAll(
+     *         .view(viewBuilder -&gt; viewBuilder.addAll(
      *                 "name",
      *                 "owner.name"))
      *         .one();
