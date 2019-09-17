f31ab1ebc6efa400329e43a59828941d35dc4d24
Gleb Durygin <durygin@haulmont.com>
2019-09-17 15:01:51 +0400

    Menu of suggestionField pop up when focus already transferred to another window #2430

--- modules/web-toolkit/src/com/haulmont/cuba/web/widgets/client/suggestionfield/CubaSuggestionFieldWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/widgets/client/suggestionfield/CubaSuggestionFieldWidget.java
@@ -76,6 +76,8 @@ public class CubaSuggestionFieldWidget extends Composite implements HasEnabled, 
     protected String popupStylename = "";
     protected String popupWidth;
 
+    protected boolean focused = false;
+
     public CubaSuggestionFieldWidget() {
         textField = GWT.create(VTextField.class);
         initTextField();
@@ -94,6 +96,7 @@ public class CubaSuggestionFieldWidget extends Composite implements HasEnabled, 
         textField.addKeyUpHandler(events);
         textField.addValueChangeHandler(events);
         textField.addBlurHandler(events);
+        textField.addFocusHandler(events);
 
         disableAutocompletion();
     }
@@ -151,7 +154,7 @@ public class CubaSuggestionFieldWidget extends Composite implements HasEnabled, 
             suggestionsPopup.removeAutoHidePartner(getElement());
             suggestionsPopup.addAutoHidePartner(getElement());
 
-            if (!suggestionsPopup.isAttached() || !suggestionsPopup.isVisible()) {
+            if (isFocused() && (!suggestionsPopup.isAttached() || !suggestionsPopup.isVisible())) {
                 suggestionsPopup.showPopup();
             }
 
@@ -207,6 +210,10 @@ public class CubaSuggestionFieldWidget extends Composite implements HasEnabled, 
         if (query.length() >= minSearchStringLength) {
             scheduleQuery(query);
         }
+    }
+
+    public boolean isFocused() {
+        return focused;
     }
 
     public int getTabIndex() {
@@ -530,7 +537,8 @@ public class CubaSuggestionFieldWidget extends Composite implements HasEnabled, 
         return s.getMarginWidth() + s.getBorderWidth() + s.getPaddingWidth();
     }
 
-    protected class CubaTextFieldEvents implements KeyDownHandler, KeyUpHandler, ValueChangeHandler<String>, BlurHandler {
+    protected class CubaTextFieldEvents implements KeyDownHandler, KeyUpHandler, ValueChangeHandler<String>, BlurHandler,
+                                                    FocusHandler {
         @Override
         public void onKeyDown(KeyDownEvent event) {
             switch (event.getNativeKeyCode()) {
@@ -556,11 +564,17 @@ public class CubaSuggestionFieldWidget extends Composite implements HasEnabled, 
         @Override
         public void onBlur(BlurEvent event) {
             handleOnBlur(event);
+            focused = false;
         }
 
         @Override
         public void onKeyUp(KeyUpEvent event) {
             searchSuggestions(textField.getText());
+        }
+
+        @Override
+        public void onFocus(FocusEvent event) {
+            focused = true;
         }
     }
 
