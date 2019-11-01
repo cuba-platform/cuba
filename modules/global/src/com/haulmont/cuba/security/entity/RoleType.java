2cdeba29fc685689c1bfaaa276ebd49e2493f639
subbotin <subbotin@haulmont.com>
2019-11-01 17:24:35 +0400

    Introduce strict denying role #2493

--- modules/global/src/com/haulmont/cuba/security/entity/RoleType.java
+++ modules/global/src/com/haulmont/cuba/security/entity/RoleType.java
@@ -17,9 +17,16 @@
 
 package com.haulmont.cuba.security.entity;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.haulmont.chile.core.datatypes.impl.EnumClass;
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Metadata;
 
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * Role type.
@@ -71,6 +78,50 @@ public enum RoleType implements EnumClass<Integer> {
                 default:
                     return null;
             }
+        }
+    },
+    STRICT_DENYING(40) {
+
+        protected final Set<String> systemAttributes = ImmutableSet.of("deleteTs", "version");
+
+        @Override
+        public Integer permissionValue(PermissionType type, String target) {
+            switch (type) {
+                case ENTITY_ATTR:
+                    return entityAttributePermission(target);
+                case SCREEN:
+                case SPECIFIC:
+                case ENTITY_OP:
+                    return 0;
+                default:
+                    return null;
+            }
+        }
+
+        protected Integer entityAttributePermission(String target) {
+            Iterable<String> paths = Splitter.on(Permission.TARGET_PATH_DELIMETER)
+                    .omitEmptyStrings()
+                    .trimResults()
+                    .split(target);
+            if (Iterables.size(paths) == 2) {
+                String entityName = Iterables.get(paths, 0);
+                String attribute = Iterables.get(paths, 1);
+
+                if (systemAttributes.contains(attribute)) {
+                    return 1;
+                }
+
+                Metadata metadata = AppBeans.get(Metadata.class);
+                MetaClass metaClass = metadata.getClass(entityName);
+
+                if (metaClass != null) {
+                    String identifier = metadata.getTools().getPrimaryKeyName(metaClass);
+                    if (Objects.equals(identifier, attribute)) {
+                        return 1;
+                    }
+                }
+            }
+            return 0;
         }
     };
 
