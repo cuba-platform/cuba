cacbe7e2987016ec137f5c450e983960ec2e4168
Gleb Durygin <durygin@haulmont.com>
2019-12-03 20:02:34 +0400

    The Buttons panel is hiding if the attributes caption are set both in the ButtonsPanel and the Tables component #1269

--- modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/AbstractDataGridLoader.java
+++ modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/AbstractDataGridLoader.java
@@ -47,6 +47,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.dom4j.DocumentFactory;
 import org.dom4j.Element;
 import org.dom4j.datatype.DatatypeElementFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Constructor;
@@ -57,6 +59,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class AbstractDataGridLoader<T extends DataGrid> extends ActionsHolderLoader<T> {
+
+    private static final Logger log = LoggerFactory.getLogger(AbstractDataGridLoader.class);
 
     protected ComponentLoader buttonsPanelLoader;
     protected Element panelElement;
@@ -351,6 +355,11 @@ public abstract class AbstractDataGridLoader<T extends DataGrid> extends Actions
             String alwaysVisible = panelElement.attributeValue("alwaysVisible");
             if (alwaysVisible != null) {
                 panel.setAlwaysVisible(Boolean.parseBoolean(alwaysVisible));
+            }
+
+            if (panel.getCaption() != null) {
+                log.debug("The caption '{}' of ButtonsPanel inside of DataGrid will be ignored",
+                        panel.getCaption());
             }
         }
     }
--- modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/AbstractTableLoader.java
+++ modules/gui/src/com/haulmont/cuba/gui/xml/layout/loaders/AbstractTableLoader.java
@@ -65,6 +65,8 @@ import java.util.stream.Stream;
 
 public abstract class AbstractTableLoader<T extends Table> extends ActionsHolderLoader<T> {
 
+    private static final Logger log = LoggerFactory.getLogger(AbstractTableLoader.class);
+
     protected ComponentLoader buttonsPanelLoader;
     protected Element panelElement;
 
@@ -122,7 +124,6 @@ public abstract class AbstractTableLoader<T extends Table> extends ActionsHolder
             if (StringUtils.isBlank(rowHeaderMode)) {
                 rowHeaderMode = rowsElement.attributeValue("headerMode");
                 if (StringUtils.isNotBlank(rowHeaderMode)) {
-                    Logger log = LoggerFactory.getLogger(AbstractTableLoader.class);
                     log.warn("Attribute headerMode is deprecated. Use rowHeaderMode.");
                 }
             }
@@ -492,6 +493,11 @@ public abstract class AbstractTableLoader<T extends Table> extends ActionsHolder
             String alwaysVisible = panelElement.attributeValue("alwaysVisible");
             if (alwaysVisible != null) {
                 panel.setAlwaysVisible(Boolean.parseBoolean(alwaysVisible));
+            }
+
+            if (panel.getCaption() != null) {
+                log.debug("The caption '{}' of ButtonsPanel inside of Table will be ignored",
+                        panel.getCaption());
             }
         }
     }
