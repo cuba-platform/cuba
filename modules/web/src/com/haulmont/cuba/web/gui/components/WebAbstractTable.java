5b49411d4a0e0af4c254336ddb349845da50dc0b
Gleb Durygin <durygin@haulmont.com>
2019-12-20 14:13:06 +0400

    Replace ImmutableSet with Collections.unmodifiableSet #2604

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebAbstractTable.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebAbstractTable.java
@@ -334,7 +334,7 @@ public abstract class WebAbstractTable<T extends com.vaadin.v7.ui.Table & CubaEn
                     res.add(item);
                 }
             }
-            return res;
+            return Collections.unmodifiableSet(res);
         } else {
             return Collections.emptySet();
         }
