dadb2d452856726f90311bedfd4ca1c7102a6c08
Maxim Gorbunkov <gorbunkov@haulmont.com>
2019-12-23 12:21:59 +0400

    FTS filter doesn't use custom parameters used for datasource refresh #2609

--- modules/gui/src/com/haulmont/cuba/gui/components/filter/FilterDelegateImpl.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/filter/FilterDelegateImpl.java
@@ -1721,6 +1721,9 @@ public class FilterDelegateImpl implements FilterDelegate {
         for (String paramName : ftsLastDatasourceRefreshParamsNames) {
             lastRefreshParameters.remove(paramName);
         }
+        lastRefreshParameters.remove(FtsFilterHelper.SESSION_ID_PARAM_NAME);
+        lastRefreshParameters.remove(FtsFilterHelper.QUERY_KEY_PARAM_NAME);
+
         List<FtsCondition> ftsConditions = conditions.toConditionsList().stream()
                 .filter(abstractCondition -> abstractCondition instanceof FtsCondition)
                 .map(abstractCondition -> (FtsCondition) abstractCondition)
@@ -1761,7 +1764,12 @@ public class FilterDelegateImpl implements FilterDelegate {
             return;
         }
 
-        Map<String, Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>(adapter.getLastRefreshParameters());
+        for (String paramName : ftsLastDatasourceRefreshParamsNames) {
+            params.remove(paramName);
+        }
+        params.remove(FtsFilterHelper.SESSION_ID_PARAM_NAME);
+        params.remove(FtsFilterHelper.QUERY_KEY_PARAM_NAME);
 
         conditions = new ConditionsTree();
         if (!Strings.isNullOrEmpty(searchTerm)) {
