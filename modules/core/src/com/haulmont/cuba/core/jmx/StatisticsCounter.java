a1da601b415c35b903e9896fcca95b71e674f426
subbotin <subbotin@haulmont.com>
2019-12-26 11:41:40 +0400

    Use own database connection pool #2315 Fix warning log message

--- modules/core/src/com/haulmont/cuba/core/jmx/StatisticsCounter.java
+++ modules/core/src/com/haulmont/cuba/core/jmx/StatisticsCounter.java
@@ -136,7 +136,7 @@ public class StatisticsCounter implements StatisticsCounterMBean {
         }
 
         if (connectionPoolInfo.getRegisteredMBeanName() == null) {
-            log.warn("No one connection pool was found for statistics counting!");
+            log.warn("Сonnection pool not found for statistics counting!");
             dbConnPoolNotFound = true;
             return null;
         }
