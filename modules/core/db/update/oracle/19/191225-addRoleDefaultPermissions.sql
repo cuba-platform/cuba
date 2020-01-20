d1d14990ccbd84e5d5e4df9360bc96062a28c835
subbotin <subbotin@haulmont.com>
2020-01-20 19:08:40 +0400

    Default role permissions#2637
    Fix ORACLE scripts

--- modules/core/db/update/oracle/19/191225-addRoleDefaultPermissions.sql
+++ modules/core/db/update/oracle/19/191225-addRoleDefaultPermissions.sql
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-alter table SEC_ROLE add DEFAULT_SCREEN_ACCESS integer;
-alter table SEC_ROLE add DEFAULT_ENTITY_CREATE_ACCESS integer;
-alter table SEC_ROLE add DEFAULT_ENTITY_READ_ACCESS integer;
-alter table SEC_ROLE add DEFAULT_ENTITY_UPDATE_ACCESS integer;
-alter table SEC_ROLE add DEFAULT_ENTITY_DELETE_ACCESS integer;
-alter table SEC_ROLE add DEFAULT_ENTITY_ATTR_ACCESS integer;
-alter table SEC_ROLE add DEFAULT_SPECIFIC_ACCESS integer;
+alter table SEC_ROLE add DEFAULT_SCREEN_ACCESS integer^
+alter table SEC_ROLE add DEFAULT_ENTITY_CREATE_ACCESS integer^
+alter table SEC_ROLE add DEFAULT_ENTITY_READ_ACCESS integer^
+alter table SEC_ROLE add DEFAULT_ENTITY_UPDATE_ACCESS integer^
+alter table SEC_ROLE add DEFAULT_ENTITY_DELETE_ACCESS integer^
+alter table SEC_ROLE add DEFAULT_ENTITY_ATTR_ACCESS integer^
+alter table SEC_ROLE add DEFAULT_SPECIFIC_ACCESS integer^
