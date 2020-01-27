07117609fa5d5f06999cf334f70e8475661156fd
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-01-27 13:38:46 +0400

    Fine-looking annotation or attribute to disable DDL generation in Studio #2385

---
+++ modules/global/src/com/haulmont/cuba/core/global/DdlGeneration.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2008-2016 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.haulmont.cuba.core.global;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates whether a development tool should generate DDL for this entity.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface DdlGeneration {
+
+    /**
+     * Whether a development tool should generate DDL.
+     */
+    boolean enabled();
+}
