17add91c332a07533ac3b6cf453372c924d23bcb
bav <bav@cs-consult.ru>
2020-08-20 22:55:45 +0700

    Add a new sendSupportEmail method with 3 arguments.

--- modules/web/src/com/haulmont/cuba/web/exception/ExceptionDialog.java
+++ modules/web/src/com/haulmont/cuba/web/exception/ExceptionDialog.java
@@ -197,7 +197,7 @@ public class ExceptionDialog extends CubaWindow {
 
                 Button reportButton = new CubaButton(messages.getMainMessage("exceptionDialog.reportBtn"));
                 reportButton.addClickListener(event -> {
-                    sendSupportEmail(text, stackTrace, userMessageTextArea.getValue() );
+                    sendSupportEmail(text, stackTrace, userMessageTextArea.getValue());
                     reportButton.setEnabled(false);
                 });
                 buttonsLayout.addComponent(reportButton);
@@ -362,6 +362,32 @@ public class ExceptionDialog extends CubaWindow {
         }
     }
 
+    public void sendSupportEmail(String message, String stackTrace) {
+        try {
+            User user = userSessionSource.getUserSession().getUser();
+            String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(timeSource.currentTimestamp());
+
+            Map<String, Object> binding = new HashMap<>();
+            binding.put("timestamp", date);
+            binding.put("errorMessage", message);
+            binding.put("stacktrace", stackTrace);
+            binding.put("systemId", clientConfig.getSystemID());
+            binding.put("userLogin", user.getLogin());
+            binding.put("userMessage", "");
+
+            if (MapUtils.isNotEmpty(additionalExceptionReportBinding)) {
+                binding.putAll(additionalExceptionReportBinding);
+            }
+
+            reportService.sendExceptionReport(clientConfig.getSupportEmail(), Collections.unmodifiableMap(binding));
+
+            Notification.show(messages.getMainMessage("exceptionDialog.emailSent"));
+        } catch (Throwable e) {
+            log.error("Error sending exception report", e);
+            Notification.show(messages.getMainMessage("exceptionDialog.emailSendingErr"));
+        }
+    }
+
     public void sendSupportEmail(String message, String stackTrace, String userMessage) {
         try {
             User user = userSessionSource.getUserSession().getUser();
@@ -423,11 +449,11 @@ public class ExceptionDialog extends CubaWindow {
         }
     }
 
-    public void setAdditionalExceptionReportBinding(Map<String, Object> binding) {
-        additionalExceptionReportBinding = binding;
-    }
-
     public Map<String, Object> getAdditionalExceptionReportBinding() {
         return additionalExceptionReportBinding;
+    }
+
+    public void setAdditionalExceptionReportBinding(Map<String, Object> binding) {
+        additionalExceptionReportBinding = binding;
     }
 }
