bc1fd2d3bdda5e3c9a284fd42c46fba8c27a6d45
Vladislav Plakhov <v.plakhov@haulmont.com>
2020-10-07 16:45:17 +0400

    Change StringBuilder to String #3019
    (cherry picked from commit e8b1cf82136c08c215b27e0361e386f3e733afda)

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebDateField.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebDateField.java
@@ -305,14 +305,14 @@ public class WebDateField<V extends Comparable<V>>
 
         dateTimeFormat = dateFormat;
 
-        StringBuilder date = new StringBuilder(dateFormat);
+        String date = dateFormat;
         Pair<Integer, Integer> timePosition = findTimePosition(dateFormat);
         if (timePosition.getFirst() >= 0 && timePosition.getSecond() >= 0) {
             StringBuilder time = new StringBuilder(dateFormat.substring(timePosition.getFirst(), timePosition.getSecond() + 1));
             timeField.setTimeFormat(StringUtils.trimToEmpty(time.toString()));
-            date = new StringBuilder(StringUtils.trimToEmpty(dateFormat.replaceAll(time.toString(), "")));
+            date = StringUtils.trimToEmpty(dateFormat.replaceAll(time.toString(), ""));
         }
-        dateField.setDateFormat(StringUtils.trimToEmpty(date.toString()));
+        dateField.setDateFormat(StringUtils.trimToEmpty(date));
 
         updateLayout();
     }
