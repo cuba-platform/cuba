ed567c0f125c3bda04f26a8b696c93949bcaa585
Dmitry Taimanov <taimanov@haulmont.com>
2020-12-25 17:25:40 +0400

    Query IN parameter accepting list of entities doesn't work on MySQL #3086
    (cherry picked from commit bfd0c092e4330d9df563400e2767e181e06ff8f5)

--- modules/global/src/com/haulmont/cuba/core/sys/persistence/CubaMySQLPlatform.java
+++ modules/global/src/com/haulmont/cuba/core/sys/persistence/CubaMySQLPlatform.java
@@ -17,17 +17,37 @@
 package com.haulmont.cuba.core.sys.persistence;
 
 import org.eclipse.persistence.exceptions.ConversionException;
+import org.eclipse.persistence.internal.sessions.AbstractSession;
 import org.eclipse.persistence.platform.database.MySQLPlatform;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class CubaMySQLPlatform extends MySQLPlatform {
 
     @Override
+    public void setParameterValueInDatabaseCall(Object parameter,
+                                                PreparedStatement statement,
+                                                int index,
+                                                AbstractSession session)
+            throws SQLException {
+
+        if (parameter instanceof UUID)
+            parameter = convertUUID((UUID) parameter);
+
+        super.setParameterValueInDatabaseCall(parameter, statement, index, session);
+    }
+
+    @Override
     public Object convertObject(Object sourceObject, Class javaClass) throws ConversionException {
         if (sourceObject instanceof UUID && javaClass == String.class) {
-            return sourceObject.toString().replace("-", "");
+            return convertUUID((UUID) sourceObject);
         }
         return super.convertObject(sourceObject, javaClass);
+    }
+
+    protected String convertUUID(UUID uuid) {
+        return uuid.toString().replace("-", "");
     }
 }
