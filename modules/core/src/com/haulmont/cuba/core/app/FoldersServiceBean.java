1d46a61fdf6c4b41d6d8ccbf784bfd68a47856d7
Ivan Gavrilov <i.gavrilov@haulmont.com>
2025-01-13 16:54:15 +0400

    Update dependencies #3307

--- modules/core/src/com/haulmont/cuba/core/app/FoldersServiceBean.java
+++ modules/core/src/com/haulmont/cuba/core/app/FoldersServiceBean.java
@@ -34,7 +34,6 @@ import com.haulmont.cuba.security.global.UserSession;
 import com.thoughtworks.xstream.XStream;
 import groovy.lang.Binding;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
@@ -247,7 +246,7 @@ public class FoldersServiceBean implements FoldersService {
         zipOutputStream.setEncoding(StandardCharsets.UTF_8.name());
         String xml = createXStream().toXML(folder);
         byte[] xmlBytes = xml.getBytes(StandardCharsets.UTF_8);
-        ArchiveEntry zipEntryDesign = newStoredEntry("folder.xml", xmlBytes);
+        ZipArchiveEntry zipEntryDesign = newStoredEntry("folder.xml", xmlBytes);
         zipOutputStream.putArchiveEntry(zipEntryDesign);
         zipOutputStream.write(xmlBytes);
         try {
@@ -369,7 +368,7 @@ public class FoldersServiceBean implements FoldersService {
         return xStream;
     }
 
-    protected ArchiveEntry newStoredEntry(String name, byte[] data) {
+    protected ZipArchiveEntry newStoredEntry(String name, byte[] data) {
         ZipArchiveEntry zipEntry = new ZipArchiveEntry(name);
         zipEntry.setSize(data.length);
         zipEntry.setCompressedSize(zipEntry.getSize());
